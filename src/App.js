c8c4ccd1eb8bec2eceec903d789cb71041b9b280
Jennifer Wen Xu <z5207930@unsw.edu.au>
2021-03-18 00:18:05 +1100

    Adding explanation

--- src/App.js
+++ src/App.js
@@ -5,15 +5,32 @@ import {appendCourse, deleteCourse} from './actions/updateCourses';
 function App() {
     // Note: You can access the state from any component since it is passed down from root! 
     const degree = useSelector(state => state.degree);
-    const dispatch = useDispatch();
+    const dispatch = useDispatch();  
     return ( 
       <div> 
         {degree == null ? (
         <div> Select your degree, replace with dropdown 
-          <button onClick={() => dispatch(update)}> UPDATE </button>
+          <button onClick={() => dispatch(updateDegree)}> UPDATE </button>
         </div> ) : (<Page/>)}
       </div>
     )
 }
+
+// How I imagine dispatch works 
+// dispatch(data) { 
+//   for (action in action) { 
+//     if data.type = action { 
+//       runReducer(data.params);
+//     }
+//   }
+// }
+
+// Click a button 
+// Run a function (Reducer)
+// pass in action with information -> action.type  (Action)
+// Run the function which has the same type as action.type ()
+// Update the store (global store) with the next data 
+// When any value within the store changes, components which use this value will re-render
+
 
 export default App;
