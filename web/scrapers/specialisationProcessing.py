8ad14fa89f641b4befa982f84572c0a0d15cec86
Adam Brieger <adam.brieger@gmail.com>
2021-06-04 15:37:10 +1000

    style: adds docstring setting out proposed data structure

---
+++ web/scrapers/specialisationProcessing.py
@@ -0,0 +1,49 @@
+"""
+Program processes scraped data for specialisations.
+
+Proposed data structure:
+
+"COMPA1": {
+
+    "programs": [ "3776", "3777", ... ], 
+
+    "name": "Computer Science (Artificial Intelligence)", 
+
+    "type": "honours",
+
+    "total_credits": 72,
+
+    "curriculum": {
+
+        "core_courses": {
+            
+            "credits": 24,
+
+            "courses": [ "COMP2511", "COMP1511", "MATH1081", ... ],
+        } ,
+
+        "prescribed_electives": {
+            
+            "credits": 6,
+
+            "courses": [ "COMP3331", "COMP3311", "COMP3141", ... ],
+        } ,
+
+        "discipline_electives": {
+            
+            "credits": ...,
+
+            "courses": [ ... ],
+        } ,
+
+        "computing_electives": {
+            
+            "credits": ...,
+
+            "courses": [ ... ],
+        } ,
+
+    }
+}
+
+"""
