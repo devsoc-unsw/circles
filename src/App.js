67ad73ae0e4a753ca5703b4e49f609989a3ba9eb
Sanojan Thiyagaraja <sanojan99@gmail.com>
2021-06-17 14:18:55 +1000

    set up ant design with custom theme

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
-import React from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import {updateDegree, resetDegree} from './actions/updateDegree'; 
-import {appendCourse, deleteCourse} from './actions/updateCourses';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { updateDegree, resetDegree } from "./actions/updateDegree";
+import { appendCourse, deleteCourse } from "./actions/updateCourses";
+import { Button } from "antd";
+import "./App.less";
+
 function App() {
-    // Note: You can access the state from any component since it is passed down from root! 
-    const degree = useSelector(state => {
-      return state.degree
-    });
-    const dispatch = useDispatch();  
-    return ( 
-      <div> 
-        {degree == null ? (
-        <div> Select your degree, replace with dropdown 
-          <button onClick={() => dispatch(updateDegree("some chosen degree"))}> UPDATE </button>
-        </div> ) : (<div>Hello! {degree}</div>)}
-      </div>
-    )
+  // Note: You can access the state from any component since it is passed down from root!
+  const degree = useSelector((state) => {
+    return state.degree;
+  });
+  const dispatch = useDispatch();
+  return (
+    <div>
+      {degree == null ? (
+        <div>
+          Select your degree, replace with dropdown
+          <Button
+            type="primary"
+            onClick={() => dispatch(updateDegree("some chosen degree"))}
+          >
+            UPDATE
+          </Button>
+        </div>
+      ) : (
+        <div>Hello! {degree}</div>
+      )}
+    </div>
+  );
 }
 
-// How I imagine dispatch works 
-// dispatch(data) { 
-//   for (action in action) { 
-//     if data.type = action { 
+// How I imagine dispatch works
+// dispatch(data) {
+//   for (action in action) {
+//     if data.type = action {
 //       runReducer(data.params);
 //     }
 //   }
 // }
 
-// Click a button 
+// Click a button
 // Run a function (Reducer)
 // pass in action with information -> action.type  (Action)
 // Run the function which has the same type as action.type ()
-// Update the store (global store) with the next data 
+// Update the store (global store) with the next data
 // When any value within the store changes, components which use this value will re-render
-
 
 export default App;
