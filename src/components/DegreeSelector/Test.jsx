dcb17b7bde93ef232baf19ac725f7fa554308e40
Sally Sun <sallyysun101@gmail.com>
2021-06-21 20:19:49 +1000

    I wrote a test file.

---
+++ src/components/DegreeSelector/Test.jsx
@@ -0,0 +1,12 @@
+import React from "react"; 
+const Test = (() => {
+    const state = 0 // context get state 
+    const onClick = (() => { 
+        // Call function from context
+    })
+    return (
+        <div> 
+            Show all the selected nodes 
+        </div>
+    ) 
+});
