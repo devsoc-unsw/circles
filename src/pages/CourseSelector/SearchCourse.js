b1d1af283c7cdd03100f277d6e33a5c53fb005b5
gabriellahartanto <gabriella.hartanto@icloud.com>
2021-07-04 18:55:48 +0700

    edited search bar

--- src/pages/CourseSelector/SearchCourse.js
+++ src/pages/CourseSelector/SearchCourse.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useMemo } from 'react';
 import { Select, Spin } from 'antd';
 import debounce from 'lodash/debounce';
 
-function DebounceSelect({ fetchOptions, debounceTimeout = 800, ...props }) {
-  const [fetching, setFetching] = React.useState(false);
-  const [options, setOptions] = React.useState([]);
-  const fetchRef = React.useRef(0);
-  const debounceFetcher = React.useMemo(() => {
+const DebounceSelect = ({ fetchOptions, debounceTimeout = 800, ...props }) => {
+  const [fetching, setFetching] = useState(false);
+  const [options, setOptions] = useState([]);
+  const fetchRef = useRef(0);
+  const debounceFetcher = useMemo(() => {
     const loadOptions = (value) => {
       fetchRef.current += 1;
       const fetchId = fetchRef.current;
