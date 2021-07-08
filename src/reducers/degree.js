bd4170b61da4f4fe871e94610214b9b66f8f96e4
Sally Sun <sallyysun101@gmail.com>
2021-07-09 00:36:43 +1000

    degree.js

--- src/reducers/degree.js
+++ src/reducers/degree.js
@@ -1,7 +1,7 @@
 const degreeReducer = (state=null, action) => { 
     switch (action.type) { 
         case 'SELECT': 
-            return state = action.payload; 
+            return state = "Undergraduate"; 
         default: 
             return state;    
     }
