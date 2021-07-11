9775c6eb1975d2439eaa347af75b1da97d70d4c0
Jennifer Wen Xu <z5207930@unsw.edu.au>
2021-07-11 20:05:25 +1000

    Fixed text colour issue in course selector menu

--- src/pages/CourseSelector/CourseMenu.js
+++ src/pages/CourseSelector/CourseMenu.js
@@ -59,9 +59,9 @@ export default function CourseMenu(props) {
       {
         courseId && core.length > 0 && electives.length > 0 && genEd.length > 0 &&
         <Menu
-          className={"text"}
+          className={'text'}
           onClick={handleClick}
-          style={{ width: '100%' }}
+          style={{ width: '100%', color: 'black'}}
           defaultSelectedKeys={[courseId !== '0' ? courseId : core[0]]}
           selectedKeys={[courseId]}
           defaultOpenKeys={['sub2', 'sub4', 'sub5']}
@@ -70,29 +70,29 @@ export default function CourseMenu(props) {
           <SubMenu className={"text"} key="sub1" /* icon={<MailOutlined />} */ title="Recently Viewed">
             <Menu.Item key={'0'} disabled>No courses here (ㆆ_ㆆ)</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub2" /* icon={<AppstoreOutlined />} */ title="Core">
+          <SubMenu  className={"text"} key="sub2" /* icon={<AppstoreOutlined />} */ title="Core">
             {
               core.map((course) => {
                 return (
-                  <Menu.Item key={ course } onClick={ () => goToCourse(course) }>{ course }</Menu.Item>
+                  <Menu.Item className={"text"}  key={ course } onClick={ () => goToCourse(course) }>{ course }</Menu.Item>
                 )
               })
             }
           </SubMenu>
-          <SubMenu key="sub4" /* icon={<SettingOutlined />} */ title="Electives">
+          <SubMenu className={"text"} key="sub4" /* icon={<SettingOutlined />} */ title="Electives">
             {
               electives.map((course) => {
                 return (
-                  <Menu.Item key={ course } onClick={ () => goToCourse(course) }>{ course }</Menu.Item>
+                  <Menu.Item className={"text"} key={ course } onClick={ () => goToCourse(course) }>{ course }</Menu.Item>
                 )
               })
             }
           </SubMenu>
-          <SubMenu key="sub5" /* icon={<SettingOutlined />} */ title="General Education">
+          <SubMenu  className={"text"} key="sub5" /* icon={<SettingOutlined />} */ title="General Education">
             {
               genEd.map((course) => {
                 return (
-                  <Menu.Item key={ course } onClick={ () => goToCourse(course) }>{ course }</Menu.Item>
+                  <Menu.Item  className={"text"} key={ course } onClick={ () => goToCourse(course) }>{ course }</Menu.Item>
                 )
               })
             }
--- src/pages/CourseSelector/main.js
+++ src/pages/CourseSelector/main.js
@@ -25,7 +25,6 @@ export default function CourseSelector() {
   }, []);
 
   useEffect(() => {
-    console.log('COURSE MENU');
     dispatch(getAllCourses());
   }, []);
 
