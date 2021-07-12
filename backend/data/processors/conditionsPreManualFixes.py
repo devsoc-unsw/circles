5c55d335b8075c413ae9d161d660e462d22c8a73
James Ji <jamesx.ji@gmail.com>
2021-07-12 23:11:59 +1000

    initialised new conditions files, will discuss later what they do

---
+++ backend/data/processors/conditionsPreManualFixes.py
@@ -0,0 +1,6 @@
+
+'''
+Applying light manual fixes to conditions before preprocessing the conditions.
+These involve things like manually adding in a missing bracket or manually
+fixing a small spelling mistake.
+'''
---
+++ backend/data/processors/conditionsPreprocessing.py
@@ -0,0 +1,8 @@
+'''
+Preprocessing the conditions and logging to enrolmentRules.json
+Some examples of preprocessing are:
+- Getting rid of "Prerequisite:" or similar at start of line
+- Fixing extra white space
+- Normalising AND/OR/and/or to && and ||
+- Converting specialisation and program names to corresponding codes
+'''
