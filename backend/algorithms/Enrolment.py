abdc8ae6ac967709d9166769b031b2b864d2c703
JamesyJi <49261993+JamesyJi@users.noreply.github.com>
2021-07-13 17:17:36 +1000

    Setup backend (#23)
    * CourseScrape
    
    First submission
    
    * courseScraper update
    
    output three separate files
    added processData to Courses.py
    added getERList to Courses.py
    
    * Update Enrolmentrules
    
    add cleanEnrolmentRules
    
    * Add basic enrolment rules
    
    Requirements classes (course only)
    parse function (course only)
    
    * Tiny changes to Enrolment
    
    * update
    
    * Give Courses.py
    
    * somasd
    
    * commenced on processing program data
    
    * Set up express skeleton with routes and an init file which writes to mongodb
    
    * routes for courses, specialisations and programs
    
    * restructuring files in line with new file structure
    
    * deleted some left over files in th wrong places
    
    * all scraping/formatting/processing filenames pluralised
    
    * initialised new conditions files, will discuss later what they do
    
    * moving enrolment.py to algorithms folder
    
    * pip requirements in txt
    
    * moved raw Conditions into scrapers
    
    Co-authored-by: IsaacYaner <47761372+IsaacYaner@users.noreply.github.com>
    Co-authored-by: Kevin <kevin.huang6541@gmail.com>

---
+++ backend/algorithms/Enrolment.py
@@ -0,0 +1,148 @@
+class Condition:
+    def __init__(self):
+        self.selected = []
+        self.Program = []
+        self.UoC = 0
+        self.WaM = 0
+        return
+    
+    def select(self, course):
+        self.selected += course
+
+class Requirement:
+    def __init__(self):
+        return
+
+    #default value is True
+    def validate(self, Condition):
+        return True
+    
+    def show(self, newLine = True):
+        print('empty!')
+
+class CourseRequirement(Requirement):
+    def __init__(self, courseCode = None):
+        self.components = courseCode
+
+    def addComponent(self, newComponent):
+        self.components = newComponent
+
+    def validate(self, Condition):
+        if self.components != None:
+            satisfied = self.components in Condition.selected
+        else:
+            satisfied = True
+        return satisfied
+    
+    def show(self, newLine = False):
+        print(self.components, end = '')
+        if newLine:
+            print()
+
+class CompositeRequirement(Requirement):
+    def __init__(self):
+        self.components = []
+        self.type = 'and'
+
+    def addComponent(self, newComponent):
+        self.components.append(newComponent)
+
+    # To or
+    def switchType(self):
+        self.type = 'or'
+
+    def validate(self, Condition):
+        if self.type == 'and':
+            satisfied = True
+            for c in self.components:
+                satisfied = satisfied and c.validate(Condition)
+        else:
+            satisfied = False
+            for c in self.components:
+                satisfied = satisfied or c.validate(Condition)
+        return satisfied
+    
+    def show(self, newLine = False):
+        print('(', end = '')
+        if self.type == 'and':
+            satisfied = True
+            for c in self.components[:-1]:
+                c.show()
+                print(' and', end = ' ')
+            self.components[-1].show() 
+        else:
+            satisfied = False
+            for c in self.components[:-1]:
+                c.show()
+                print(' or', end = ' ')     
+            self.components[-1].show() 
+        print(')', end = '')
+        if newLine:
+            print()
+
+def orLike(text):
+    result = False
+    if text.lower() == 'or':
+        result = True
+    if '|' in text:
+        result = True
+    return result
+
+def andLike(text):
+    result = False
+    if text.lower() == 'and':
+        result = True
+    if '&' in text:
+        result = True
+    return result
+
+bad = False
+
+#This function cannot work properly with more than one leading '('s
+#The correct method is to add white spaces on the both side of parentheses and treat them seperately
+#But the data source is simple and here's no such cases, so I remain the code unchanged
+#At least it works well now
+def parseRequirement(text, isCalled = False, tracedBack = False):
+    result = Requirement()
+    #If empty, always True
+    if len(text) == 0:
+        result = Requirement()
+    #If only one element, simply add a CourseRequirement
+    elif len(text) == 1:
+        result = CourseRequirement(text[0])
+    #Add a compositeRequirement instead
+    else:
+        result = CompositeRequirement()
+        for index, i in enumerate(text):
+
+            if tracedBack:
+                if len(i) >= 9 and i[8] == ')':
+                    tracedBack = False
+                continue
+
+            # Modify later to a more complicated condition
+            if len(i) == 8:
+                newCourse = CourseRequirement(i)
+                result.addComponent(newCourse)
+            elif len(i) >= 9:
+                if i[0] == '(':
+                    if isCalled:
+                        newCourse = CourseRequirement(i[1:])
+                        result.addComponent(newCourse)
+                        isCalled = False
+                    else:
+                        result.addComponent(parseRequirement(text[index:], True))
+                        tracedBack = True
+                elif i[8] == ')':
+                    newCourse = CourseRequirement(i[:8])
+                    result.addComponent(newCourse)
+                    return result
+            elif orLike(i):
+                result.switchType()
+            elif andLike(i):
+                pass
+            else:
+                bad = True
+                return
+                #print('Unknown type: ', i)
+    return result
---
+++ backend/data/processors/conditionsPostManualFixes.py
@@ -0,0 +1,6 @@
+'''
+Applying heavy manual fixes to the conditions after preprocessing the conditions.
+These manual fixes are drastic manual fixes (e.g. not just a missing bracket like
+in conditionsPreManualFixes.py but rather, when the whole condition is so unique
+that it needs to be completely altered for it to even parse)
+'''
