bed1783403bf1343a20be3de0cd4e4c7ba200887
James Ji <jamesx.ji@gmail.com>
2021-07-16 20:43:55 +1000

    try to install fastapi

---
+++ backend/runserver.py
@@ -0,0 +1,5 @@
+import sys
+from server import server
+
+if __name__ == "__main__":
+    server
---
+++ backend/server/server.py
@@ -0,0 +1,13 @@
+import sys
+from fastapi import FastAPI
+from backend.server.routers import specialisations
+
+app = FastAPI()
+
+# app.include_router(programs.router)
+app.include_router(specialisations.router)
+# app.include_router(courses.router)
+
+@app.get('/')
+def index():
+    return "Index of server"
