65a115425286e7c5e481ff2689d81d8030b87563
Kevin <kevin.huang6541@gmail.com>
2021-07-21 19:46:57 +1000

    finished program processed

--- backend/data/processors/programsProcessing.py
+++ backend/data/processors/programsProcessing.py
@@ -15,15 +15,14 @@ Step in the data's journey:
 
 import re 
 import sys
-sys.path.append('..')
 from typing import Container, List, Iterable, Union, Optional 
-from .. import dataHelpers
+from data.utility import dataHelpers
 
 
 TEST_PROGS = ["3778"]
 
 def process_data():
-    data = dataHelpers.read_data("programsFormattedRaw.json")
+    data = dataHelpers.read_data("data/scrapers/programsFormattedRaw.json")
     processedData = {}
 
     for program in TEST_PROGS:
@@ -35,7 +34,7 @@ def process_data():
 
         processedData[programData["code"]] = programData
         
-    dataHelpers.write_data(process_data, 'programsProcessedd.json')
+    dataHelpers.write_data(processedData, "data/finalData/programsProcessedd.json")
 
 def addComponentData(formatted, programData):
     components = {
@@ -62,7 +61,7 @@ def addComponentData(formatted, programData):
             addFEData(components, item)
 
         if item["vertical_grouping"]["value"] == "GE":
-            components["GE"]["credits_to_complete"] =  "credit_points"
+            components["GE"]["credits_to_complete"] =  int(item["credit_points"])
         
         if item["title"] == "Disciplinary Component":
             addDisciplineData(components, item)
@@ -70,32 +69,26 @@ def addComponentData(formatted, programData):
     programData["components"] = components   
                 
 def addDisciplineData(components, item):
-    components["disciplinary_component"]["credits_to_complete"] == item["credit_points"]
+    components["disciplinary_component"]["credits_to_complete"] = int(item["credit_points"])
     if "container" in item and item["container"] != []:
         for container in item["container"]:
             if container["vertical_grouping"]["value"] == "undergrad_major":
                 for major in container["relationship"]:
                     if major["academic_item_type"]["value"] == "major":
                         code = major["academic_item_code"] 
-                        Major = {
-                            "code" : code
-                        }
-                        components["disciplinary_component"]["Majors"][code] = Major
+                        components["disciplinary_component"]["Majors"][code] = 1
 
 
 
 def addFEData(components, item):
-    components["credits_to_complete"] == item["credit_points"]
+    components["FE"]["credits_to_complete"] = int(item["credit_points"])
     if "container" in item and item["container"] != []:
         for container in item["container"]:
             if container["vertical_grouping"]["value"] == "undergrad_minor":
                 for minor in container["relationship"]:
-                    if minor["academic_item_type"]["value"] == "minor":
+                    if minor["academic_item_type"] and minor["academic_item_type"]["value"] == "minor":
                         code = minor["academic_item_code"] 
-                        Minor = {
-                            "code" : code
-                        }
-                    components["FE"]["Minors"][code] = Minor
+                        components["FE"]["Minors"][code] = 1
 
 
 def initialise_program(program): 
@@ -105,8 +98,12 @@ def initialise_program(program):
     program_info = {}
     program_info["title"] = program["title"]
     program_info["code"] = program["code"]
-    program_info['duration'] = program['duration']
-    program_info["UOC"] = program["UOC"]
+
+    duration = re.search("(\d)", program['duration'])
+    duration = duration.group(1)
+
+    program_info['duration'] = int(duration)
+    program_info["UOC"] = int(program["UOC"])
     program_info["faculty"] = program["faculty"]
     program_info["components"] = {}
 
