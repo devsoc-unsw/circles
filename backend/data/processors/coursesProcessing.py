226a20d32e2bc5c8aac774541fea56a5de3c3ef0
Adam Brieger <adam.brieger@gmail.com>
2021-07-23 12:00:33 +1000

    style: fixes comments

--- backend/data/processors/coursesProcessing.py
+++ backend/data/processors/coursesProcessing.py
@@ -16,18 +16,7 @@ PROCESSED_COURSES = {}
 ABSENT_COURSES = {} # Courses that appear in enrolment rules but do not exist
                     # in the 2021 Undergraduate handbook, either because the
                     # code is a typo, it has been discontinued, or is
-                    # a postgraduate course #TODO: check with James
-
-# TODO: Query inclusion of ATTRIBUTES
-"""
-introductory_course = "This course is usually taken in the early part of a program and may be required as a pre-requisite before taking a more advanced course."
-
-plus_alliance_outgoing = "This course is part of PLuS Alliance offerings. UNSW students enrol in this course at the relevant PLuS Alliance partner university. Please visit <a title=\"PLuS Alliance website\" href=\"http://plusalliance.unsw.edu.au/\" target=\"_blank\" rel=\"noopener noreferrer nofollow\">PLuS Alliance website</a>\u00a0for more information."
-
-work_integrated_learning = "This course involves work learning experiences where students work directly in or with an industry or community organisation to gain real-world experience in preparation for a future career. Please visit <a title=\"WIL website\" href=\"https://www.wil.unsw.edu.au/\" target=\"_blank\" rel=\"noopener noreferrer nofollow\">WIL website</a> for more information."
-
-multi-term_course = "This course is taught across multiple terms or is a member of a group of courses taken across multiple terms. In either case a final grade is withheld until the final course is completed."
-"""
+                    # a postgraduate course
 
 def process_courses():
 
@@ -43,8 +32,8 @@ def process_courses():
 
         process_terms(processed, course)
         process_gen_ed(processed, course)
-        process_attributes(processed, course)
         process_enrolment_path(processed, course, data)
+        # process_attributes(processed, course)
 
         # Overwrite data file entry with the newly processed info
         data[code] = processed
@@ -64,6 +53,7 @@ def process_terms(processed: dict, formatted: dict) -> None:
         res = re.sub("Semester ", "S", formatted["terms"])
 
     res = re.sub("Summer Term", "ST", res)
+    res = re.sub("Summer Canberra", "SC", res)
     res = res.split(',')
     res = [item.strip(' ') for item in res] # Strip any remaining spaces
     processed["terms"] = res
@@ -75,20 +65,20 @@ def process_gen_ed(processed: dict, formatted: dict) -> None:
     else:
         processed["gen_ed"] = 1
 
-def process_attributes(processed: dict, formatted: dict) -> None:
-    """ Add any attributes to a list, excluding gen_ed since that is covered by 
-    the gen_ed key """
-    processed["attributes"] = []
-    for attribute in formatted["attributes"]:
-        if "general_education" in attribute["type"]:
-            continue
-        processed["attributes"].append(attribute["type"])
+# NOTE: ignoring attributes for now
+# def process_attributes(processed: dict, formatted: dict) -> None:
+#     """ Add any attributes to a list, excluding gen_ed since that is covered by 
+#     the gen_ed key """
+#     processed["attributes"] = []
+#     for attribute in formatted["attributes"]:
+#         if "general_education" in attribute["type"]:
+#             continue
+#         processed["attributes"].append(attribute["type"])
 
 def process_enrolment_path(processed: dict, formatted: dict, data: dict) -> None:
     """ Adds pre-requisites to 'path_from' field and for each prereq, adds this
-    processed course to their 'path_to' field in the main formatted 'data'
-    dict, in anticipation of looping over that course when it is processed. 
-    This allows for courses to be processed in one iteration through the data """
+    processed course to their 'path_to' field in the main 'data' dict. This 
+    allows for courses to be processed in one iteration through the data """
     global ABSENT_COURSES
 
     processed["path_from"] = dict()
