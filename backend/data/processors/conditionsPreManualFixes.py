cd68416e0b05f85042fdc5a0bfe1e2616df30f47
JamesyJi <49261993+JamesyJi@users.noreply.github.com>
2021-09-03 18:40:54 +1000

    Rename to conditions (#50)
    * added wam requirement
    
    * saving some minor changes to requirements before going to work on cloud database
    
    * renamed rules to conditions. Working on renaming requirements to conditions
    
    * finished renaming. Will now test
    
    * ran processing on the files which were impacted by the name changes and on inspection, everything seems fine

--- backend/data/processors/conditionsPreManualFixes.py
+++
@@ -1,6 +0,0 @@
-
-'''
-Applying light manual fixes to conditions before preprocessing the conditions.
-These involve things like manually adding in a missing bracket or manually
-fixing a small spelling mistake.
-'''
