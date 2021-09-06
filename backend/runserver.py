8d2a025217e7faca5afd195d0d6f73de9f0bfff4
JamesyJi <49261993+JamesyJi@users.noreply.github.com>
2021-09-07 01:45:06 +1000

    Dev (#55)
    * can parse logic for brackets which are attached to word without space
    
    * Update Enrolment.py
    
    * Update test.py for filtering
    
    Filtering unmatched requirements and store in a separate file.
    
    * Update Uoc functions
    
    complete isUoc and getUoc(ith)
    improve the filter for Uoc intest
    
    * Update filtering for in...courses clauses
    
    * Process exclusions in course enrolment rules (#40)
    
    * [ab/manual condition fixes] Applies manual fixes to COMP courses (#44)
    
    * updates coursesProcessing and conditionsPreprocessing for course exclusions
    
    * small fixes to remove extraneous phrasing
    
    * new function to remove HTML tags from descriptions
    
    * joining terms
    
    * comment out program code conversions
    
    * beginning manual fixes of COMP courses
    
    * finishes COMP course manual fixes
    
    * Updates exclusions after manual fixes
    
    * Minor style changes
    
    Co-authored-by: James Ji <jamesx.ji@gmail.com>
    
    * [ab/process spns] Expands specialisations processing to COMP minors (#45)
    
    * Expands specialisations to COMP minors
    
    * Removes html tags
    
    * adds study_level, school, faculty and campus to processed courses (#48)
    
    * starting to write more tests
    
    * set up very basic test template
    
    * began rewriting algorithms. Need to work on UOC
    
    * made a start on uoc requirements
    
    * got some rough uoc requirements wokring including "in" keyword
    
    * [ab/process conditions] Refactors condition processing code (#49)
    
    * phase 0 deletions complete
    
    * decouples functions in conditionsPreprocessing.py
    
    * drafts coreq conversion
    
    * finalises corequisite parsing and 'including' conversions
    
    * deleted old algorithms files
    
    * Rename to conditions (#50)
    
    * added wam requirement
    
    * saving some minor changes to requirements before going to work on cloud database
    
    * renamed rules to conditions. Working on renaming requirements to conditions
    
    * finished renaming. Will now test
    
    * ran processing on the files which were impacted by the name changes and on inspection, everything seems fine
    
    * deleted unnecessary old api files and implemented venv
    
    * overwriting pipeline
    
    * Setup backend (#54)
    
    * beginning to set up fastapi with vincent
    
    * try to install fastapi
    
    * successful import
    
    * runserver.py
    
    * vincent
    
    * continue setting up on sunday
    
    * continue on sunday
    
    * basic routes and runserver file
    
    * finalised runserver files
    
    * deleted old database api files
    
    * overwrite collection in runserver
    
    * uhfyku
    
    * added support for engineering program
    
    * Can return!
    
    * Changed the names
    
    * gitignore node modules
    
    * Added some extra apis for Specialisations
    
    * git ignore
    
    * overwrite courses on server start
    
    * Courses
    
    * Added one more api call
    
    * adding new program
    
    * Added programs
    
    I think I've been  trolling this entire time
    
    * Fixed the minor thing
    
    * Changed the return format
    
    * Missed one
    
    * Changed format of return to be dictionary
    
    * Changed caps
    
    * Changed format again
    
    * Fixed a UOC mistake
    
    * Changed to boolean
    
    * Changed return to None
    
    * Got a response model
    
    * I am a api demon
    
    * API sorted for courses
    
    * added commerce program
    
    * updating programs data
    
    * updated program data structure
    
    * Basic api calls are done
    
    * API calls done
    
    * Added biotech program
    
    * Added an optional api template
    
    * Added the CORS thing
    
    * ensuring the data is piped to programsProcessed.json
    
    Co-authored-by: Kevin <kevin.huang6541@gmail.com>
    Co-authored-by: VincentXiao-jpg <vincent.xiao3@gmail.com>
    
    * deleted redundant init.py
    
    * added pymongo[srv] module to requirements so we can push to cloud
    
    Co-authored-by: IsaaacYaner <47761372+IsaacYaner@users.noreply.github.com>
    Co-authored-by: Adam Brieger <77366819+abrieger@users.noreply.github.com>
    Co-authored-by: Kevin <kevin.huang6541@gmail.com>
    Co-authored-by: VincentXiao-jpg <vincent.xiao3@gmail.com>

--- backend/runserver.py
+++ backend/runserver.py
@@ -4,14 +4,26 @@ import sys
 import argparse
 
 import uvicorn
+import argparse
+
 from server import server
 from server.server import app
 from server.database import overwrite_collection
 
-
+parser = argparse.ArgumentParser()
+parser.add_argument('--overwrite', default=False, type=bool,
+                    help='True or False to overwrite the existing databases with up to date data when necessary')
 
 if __name__ == "__main__":
-    overwrite_collection('Programs')
-    overwrite_collection('Specialisations')
-    overwrite_collection('Courses')
+    try:
+        args = parser.parse_args()
+    except:
+        parser.print_help()
+        sys.exit(0)
+
+    if args.overwrite:
+        overwrite_collection("Courses")
+        overwrite_collection("Specialisations")
+        overwrite_collection("Programs")
+
     uvicorn.run(app)
