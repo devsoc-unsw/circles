79c29a81d432a76f821b6cd2c94491e345436362
James Ji <jamesx.ji@gmail.com>
2021-09-06 17:37:41 +1000

    added some more tests

--- backend/algorithms/logic_test.py
+++ backend/algorithms/logic_test.py
@@ -45,3 +45,33 @@ def test_course_condition():
         "COMP1511": (6, 80)
     })
     assert single_cond.validate(user) == True
+
+
+def test_composite_condition_simple():
+    '''AND/OR conditions with only course requirements'''
+    user = create_student_3707_COMPA1()
+
+    and_cond = create_condition(
+        ["(", "COMP1511", "&&", "COMP1521", "&&", "COMP1531", ")"])
+    assert and_cond.validate(user) == False
+
+    user.add_courses({
+        "COMP1511": (6, 80),
+        "COMP1531": (6, None),
+        "MATH1141": (6, None)
+    })
+    assert and_cond.validate(user) == False
+
+    user.add_courses({
+        "COMP1521": (6, None)
+    })
+    assert and_cond.validate(user) == True
+
+    or_cond = create_condition(
+        ["(", "MATH1081", "||", "MATH1151", "||", "MATH1241", ")"])
+    assert or_cond.validate(user) == False
+
+    user.add_courses({
+        "MATH1151": (6, 90),
+    })
+    assert or_cond.validate(user) == True
