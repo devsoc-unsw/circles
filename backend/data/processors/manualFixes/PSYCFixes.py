90559e94ca1451bdfa91afc22e7d4327eb2cc2b1
Sanojan Thiyagaraja <sanojan99@gmail.com>
2021-09-12 21:34:16 +1000

    psyc fixes in progress:

---
+++ backend/data/processors/manualFixes/PSYCFixes.py
@@ -0,0 +1,91 @@
+"""
+https://github.com/csesoc/Circles/wiki/Manual-Fixes-to-Course-Prerequisites
+
+Apply manual COMP fixes to processed conditions in conditionsProcessed.json so 
+that they can be fed into algorithms.
+
+If you make a mistake and need to regenerate conditionsProcessed.json, then you
+can run:
+    python3 -m data.processors.conditionsPreprocessing
+
+To then run this file:
+    python3 -m data.processors.manualFixes.COMPFixes
+"""
+
+from data.utility import dataHelpers
+
+CONDITIONS = dataHelpers.read_data("data/finalData/conditionsProcessed.json")
+PROCESSED = "processed"
+
+COURSES = dataHelpers.read_data("data/finalData/coursesProcessed.json")
+
+def fix_conditions():
+    """ Functions to apply manual fixes """
+
+    CONDITIONS["PSYC2061"][PROCESSED] = PSYC_2061_2081();
+    CONDITIONS["PSYC2071"][PROCESSED] = PSYC_2061_2081();
+    CONDITIONS["PSYC2081"][PROCESSED] = PSYC_2061_2081();
+    CONDITIONS["PSYC2001"][PROCESSED] = PSYC_2001();
+    CONDITIONS["PSYC2061"][PROCESSED] = PSYC_2061_2081();
+    CONDITIONS["PSYC2071"][PROCESSED] = PSYC_2061_2081();
+    CONDITIONS["PSYC2081"][PROCESSED] = PSYC_2061_2081();
+    # CONDITIONS["PSYC2101"][PROCESSED] = PSYC_2001();
+    # CONDITIONS["PSYC3001"][PROCESSED] = PSYC_3011();
+    CONDITIONS["PSYC3011"][PROCESSED] = PSYC3011();
+    CONDITIONS["PSYC3051"][PROCESSED] = PSYC_3051();
+    CONDITIONS["PSYC3121"][PROCESSED] = PSYC_3051();
+    CONDITIONS["PSYC3121"][PROCESSED] = PSYC_3051();
+    CONDITIONS["PSYC3202"][PROCESSED] = PSYC_3051();
+    CONDITIONS["PSYC3211"][PROCESSED] = PSYC_3051();
+    CONDITIONS["PSYC3221"][PROCESSED] = PSYC_3051();
+    CONDITIONS["PSYC3241"][PROCESSED] = PSYC_3241();
+    CONDITIONS["PSYC3301"][PROCESSED] = PSYC_3241();
+    CONDITIONS["PSYC3311"][PROCESSED] = PSYC_3241();
+    CONDITIONS["PSYC3331"][PROCESSED] = PSYC_3241();
+    CONDITIONS["PSYC3341"][PROCESSED] = PSYC_3241();
+    CONDITIONS["PSYC3361"][PROCESSED] = PSYC_3241();
+    CONDITIONS["PSYC3371"][PROCESSED] = PSYC_3241();
+    CONDITIONS["PSYC4072"][PROCESSED] = PSYC_3241();
+    CONDITIONS["PSYC4073"][PROCESSED] = PSYC_3241();
+    CONDITIONS["PSYC4093"][PROCESSED] = PSYC_3241();
+    CONDITIONS["PSYC4103"][PROCESSED] = PSYC_3241();
+
+    CONDITIONS["PSYC1021"][PROCESSED] = PSYC_3241();
+
+    dataHelpers.write_data(
+        CONDITIONS, "data/finalData/conditionsProcessed.json")
+    dataHelpers.write_data(COURSES, "data/finalData/coursesProcessed.json")
+
+
+def PSYC_2061_2081():
+	return "PSYC1001 && PSYC1011"
+
+def PSYC2001():
+    return "PSYC1001 && PSYC1011 && PSYC1111"
+
+def PSYC3011():
+    return "PSYC2001 &&  PSYC2061 && PSYC2071 && PSYC2081 && PSYC2101"
+
+def PSYC3051_PSYC3241():
+    return "PSYC2001 && PSYC2081"
+
+def PSYC3121_PSYC3301():
+    return "PSYC2001 && PSYC2061"
+
+def PSYC3211_PSYC3221_PSYC3311():
+    return "PSYC2001 && PSYC2071"
+
+def PSYC3331():
+    return "(PSYC2001 || PSYC2061 || PSYC2101) || (HESC3504 && 3871)"
+
+def PSYC3361():
+    return "72UOC in PSYC && 80WAM"
+
+def PSYC4072_4073_4093_4103():
+    return "PSYCAH"
+
+
+
+
+if __name__ == "__main__":
+    fix_conditions()
