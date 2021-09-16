ea29bab049fd28d88d6f81f59951975d6810e813
James Ji <jamesx.ji@gmail.com>
2021-09-16 21:06:42 +1000

    started on TABL

---
+++ backend/data/processors/manualFixes/TABLFixes.py
@@ -0,0 +1,42 @@
+"""
+https://github.com/csesoc/Circles/wiki/Manual-Fixes-to-Course-Prerequisites
+
+Copy this into a new file for the relevant faculty's fixes: 
+e.g. COMPFixes.py, ACCTFixes.py, PSYCFixes.py
+
+Apply manual TABL fixes to processed conditions in conditionsProcessed.json so 
+that they can be fed into algorithms.
+
+If you make a mistake and need to regenerate conditionsProcessed.json, then you
+can run:
+    python3 -m data.processors.conditionsPreprocessing
+
+To then run this file:
+    python3 -m data.processors.manualFixes.TABLFixes
+"""
+
+from data.utility import dataHelpers
+
+# Reads conditionsProcessed dictionary into 'CONDITIONS'
+CONDITIONS = dataHelpers.read_data("data/finalData/conditionsProcessed.json")
+PROCESSED = "processed"
+
+# Reads coursesProcessed dictionary into 'COURSES' (for updating exclusions)
+COURSES = dataHelpers.read_data("data/finalData/coursesProcessed.json")
+
+
+def fix_conditions():
+    """ Functions to apply manual fixes """
+
+    CONDITIONS["COMP1400"][PROCESSED] = COMP_1400_1911("COMP1400")
+
+    # Updates the files with the modified dictionaries
+    dataHelpers.write_data(
+        CONDITIONS, "data/finalData/conditionsProcessed.json")
+    dataHelpers.write_data(COURSES, "data/finalData/coursesProcessed.json")
+
+# TODO: implement your functions here
+
+
+if __name__ == "__main__":
+    fix_conditions()
