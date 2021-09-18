2e800742ca2f1ca6ff1a0ea5e8db0ede559d8925
JamesyJi <49261993+JamesyJi@users.noreply.github.com>
2021-09-19 00:46:04 +1000

    Archive data in Database (#82)
    * no major changes, exiting this branch
    
    * archived 2019 and 2020 data in json
    
    * added pipeline to update db archives which can be triggered in runserver.py

--- backend/server/config.py
+++ backend/server/config.py
@@ -1,3 +1,5 @@
 URI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
 FINAL_DATA_PATH = "./data/finalData/"
+
+ARCHIVED_DATA_PATH = "./data/finalData/archive/processed/"
