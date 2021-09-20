66e1811f403195e5cd628f12c9dd04ba14002f54
James Ji <jamesx.ji@gmail.com>
2021-09-20 20:55:03 +1000

    set up for Isaac

---
+++ backend/algorithms/log_broken.py
@@ -0,0 +1,5 @@
+"""Logs all broken conditions to backend/algorithms/errors.txt
+
+Run from the backend directory with python3 -m backend.algorithms.log_broken
+"""
+from backend.algorithms import conditions
