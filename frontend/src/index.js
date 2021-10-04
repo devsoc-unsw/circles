197b307ea9db609bf5d513824a5ea991bdd49277
JWen00 <jenniferwen00@gmail.com>
2021-10-04 22:18:05 +1100

    Reduce package.json imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import { createStore, applyMiddleware, compose }  from 'redux';
-import allReducers from "./reducers/index"; 
+import { createStore }  from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
+import allReducers from "./reducers/index"; 
+import App from './App';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   allReducers, 
-  composeEnhancers(applyMiddleware(thunk))
-  // Download redux devtools extension on chrome extensions 
-  // This is helpful for debugging the state inside ur browser
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ); 
 ReactDOM.render(
   <Provider store={store}>
