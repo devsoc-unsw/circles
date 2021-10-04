c53c839f708286c8020d358a7f809c69b3103ddb
JWen00 <jenniferwen00@gmail.com>
2021-10-04 15:39:11 +1100

    CLean up component unneceesary code

--- frontend/src/components/courseTag/CourseTag.jsx
+++ frontend/src/components/courseTag/CourseTag.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { courseTabActions } from '../../actions/courseTabActions';
 import { Tag } from 'antd';
 import './courseTag.less'
 
-// NOTE: Tooltip not working
 export const CourseTag = ({ name }) => {
   const dispatch = useDispatch();
   const handleClick = () => {
     dispatch(courseTabActions("ADD_TAB", name));
   }
 
-  // dark mode always has white text
-  const theme = useSelector((state) => state.theme);
   return (
       <Tag onClick={handleClick} className={'text tag'}>
         { name }
