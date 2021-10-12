efd264a1974487b69e4dd08b218c25269db96998
JamesyJi <49261993+JamesyJi@users.noreply.github.com>
2021-10-12 20:50:55 +1100

    User data (#116)
    * Update User class
    
    add load_json function
    
    * Changes to load_json
    
    parameter from path to data
    
    * wrote test for user data, fails on None WAM
    
    * wrote test for user data, fails on None WAM
    
    * Fix bugs about wam
    
    Co-authored-by: IsaaacYaner <47761372+IsaacYaner@users.noreply.github.com>

---
+++ backend/algorithms/test_user.py
@@ -0,0 +1,47 @@
+'''Testing the user class to ensure that the user data is accurately imported and updated'''
+
+from conditions import User
+from math import isclose
+import pytest
+import json
+
+PATH = "./exampleUsers.json"
+
+
+with open(PATH) as f:
+    USERS = json.load(f)
+f.close()
+
+
+def test_user1():
+    user = User(USERS["user1"])
+
+    assert user.in_program("3707")
+    assert user.in_specialisation("COMPA1")
+    assert user.in_specialisation("ACCTA2")
+    assert user.has_taken_course("COMP1511")
+    assert user.has_taken_course("COMP1521")
+    assert user.has_taken_course("MATH2400")
+    assert isclose(user.wam, 70.5)
+    assert user.uoc == 15
+    assert user.year == 3
+    
+def test_user2():
+    user = User(USERS["user2"])
+
+    assert user.in_program("3707")
+    assert user.in_specialisation("COMPA1")
+    assert user.has_taken_course("COMP1511")
+    assert user.has_taken_course("MATH1081")
+    assert user.wam == None
+    assert user.uoc == 12
+    assert user.year == 1
+
+def test_user3():
+    user = User(USERS["user3"])
+    
+    assert user.in_program("3707")
+    assert user.in_specialisation("COMPA1")
+    assert user.wam == None
+    assert user.uoc == 0
+    assert user.year == 1
