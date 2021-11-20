139961a42b5fb689467f0a7c0bdd6a9026727df3
Adam Brieger <adam.brieger@gmail.com>
2021-11-20 17:59:25 +1100

    Exp change

--- backend/algorithms/test_user.py
+++ backend/algorithms/test_user.py
@@ -16,7 +16,7 @@ f.close()
 def test_user1():
     user = User(USERS["user1"])
 
-    assert user.in_program("37057")
+    assert user.in_program("3705")
     assert user.in_specialisation("COMPA1")
     assert user.in_specialisation("ACCTA2")
     assert user.has_taken_course("COMP1511")
