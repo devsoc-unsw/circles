d3e08799c117936488c9d0724bc19e739cf810f1
James Ji <jamesx.ji@gmail.com>
2021-12-08 23:24:52 +1100

    fixed up econ manual fixes

--- backend/data/processors/manualFixes/ECONFixes.py
+++ backend/data/processors/manualFixes/ECONFixes.py
@@ -12,7 +12,7 @@ can run:
     python3 -m data.processors.conditionsPreprocessing
 
 To then run this file:
-    python3 -m data.processors.manualFixes.[CODE]Fixes
+    python3 -m data.processors.manualFixes.ECONFixes
 """
 
 from data.utility import dataHelpers
@@ -28,8 +28,8 @@ def fix_conditions():
     """ Functions to apply manual fixes """
 
     # TODO: call your functions here
-    CONDITIONS["ECON1101"][PROCESSED] = ECON_1101("ECON1101") # NOT SURE HOW THIS WORKS (FOR THE EXCEPT PART)
-    CONDITIONS["ECON1203"][PROCESSED] = ECON_1203("ECON1203") # NOT SURE HOW THIS WORKS (FOR THE EXCEPT PART)
+    CONDITIONS["ECON1101"] = ECON_1101(CONDITIONS["ECON1101"])
+    CONDITIONS["ECON1203"][PROCESSED] = ECON_1203("ECON1203", CONDITIONS["ECON1203"])
     CONDITIONS["ECON2206"][PROCESSED] = ECON_2206()
     CONDITIONS["ECON2403"][PROCESSED] = ECON_2403()
     CONDITIONS["ECON3000"][PROCESSED] = ECON_3000()
@@ -46,7 +46,7 @@ def fix_conditions():
     CONDITIONS["ECON4170"][PROCESSED] = ECON_4170_2()
     CONDITIONS["ECON4171"][PROCESSED] = ECON_4170_2()
     CONDITIONS["ECON4172"][PROCESSED] = ECON_4170_2()
-    CONDITIONS["ECON4201"][PROCESSED] = ECON_4201() # NOT SURE ABOUT ASSUMED KNOWLEDGE
+    CONDITIONS["ECON4201"] = ECON_4201(CONDITIONS["ECON4201"])
     CONDITIONS["ECON4202"][PROCESSED] = ECON_4100_3_6_4202_5_8_4301_2_7_9_11_50()
     CONDITIONS["ECON4205"][PROCESSED] = ECON_4100_3_6_4202_5_8_4301_2_7_9_11_50()
     CONDITIONS["ECON4208"][PROCESSED] = ECON_4100_3_6_4202_5_8_4301_2_7_9_11_50()
@@ -64,25 +64,28 @@ def fix_conditions():
         CONDITIONS, "data/finalData/conditionsProcessed.json")
     dataHelpers.write_data(COURSES, "data/finalData/coursesProcessed.json")
 
-# TODO: implement your functions here
-# NOT SURE HOW THIS WORKS (FOR THE EXCEPT PART)
-def ECON_1101(code):
+def ECON_1101(conditions):
     """
     "original": "Excluded: Students in BCom single or double-degree programs, except for 3155 Actl/Comm and 3521 Comm/Econ, are not allowed to enrol in this course. <br/><br/>"
     
     "processed": ""
+
+    "warning": Excluded: Students in BCom single or double-degree programs, except for 3155 Actl/Comm and 3521 Comm/Econ, are not allowed to enrol in this course.
     """
-
-    COURSES[code]["exclusions"]["COMM#"] = 1
     
-    return ""
+    return {
+        "original": conditions["original"],
+        "processed": "",
+        "warning": "Excluded: Students in BCom single or double-degree programs, except for 3155 Actl/Comm and 3521 Comm/Econ, are not allowed to enrol in this course."
+    }
 
-# NOT SURE HOW THIS WORKS (FOR THE EXCEPT PART)
-def ECON_1203(code):
+def ECON_1203(code, conditions):
     """
     "original": "Excluded: MATH2841, MATH2801, MATH2901, MATH2099, ACTL2002 & ACTL2131. <br/>Also, must not be enrolled in a BCom single or double degree (except 3155 Actl/Comm or 3521 Comm/Econ), or program 3715 or 3764<br/><br/>"
     
-    "processed": "Must not be Program 3715 || 3764"
+    "processed": ""
+
+    "warning": "Also, must not be enrolled in a BCom single or double degree (except 3155 Actl/Comm or 3521 Comm/Econ)"
     """
     
     COURSES[code]["exclusions"]["MATH2841"] = 1
@@ -91,11 +94,14 @@ def ECON_1203(code):
     COURSES[code]["exclusions"]["MATH2099"] = 1
     COURSES[code]["exclusions"]["ACTL2002"] = 1
     COURSES[code]["exclusions"]["ACTL2131"] = 1
-    COURSES[code]["exclusions"]["COMM#"] = 1
     COURSES[code]["exclusions"]["3715"] = 1
     COURSES[code]["exclusions"]["3764"] = 1
 
-    return ""
+    return {
+        "original": conditions["original"],
+        "processed": "",
+        "warning": "Also, must not be enrolled in a BCom single or double degree (except 3155 Actl/Comm or 3521 Comm/Econ)"
+    }
 
 def ECON_2206():
     """
@@ -112,7 +118,7 @@ def ECON_2403():
     "processed": "PPEC1001 && ECON1202 && in program 3478 || 4797"
     """
 
-    return "PPEC1001 && ECON1202 && 3478 || 4797"
+    return "PPEC1001 && ECON1202 && (3478 || 4797)"
 
 def ECON_3000():
     """
@@ -168,15 +174,20 @@ def ECON_4170_2():
 
     return "ECONEH4502"
 
-# NOT SURE ABOUT THE ASSUMED KNOWLEDGE!
-def ECON_4201():
+def ECON_4201(conditions):
     """
     "original": "Pre-requisite: ECON4103<br/>Assumed knowledge: Familiarity with matrix algebra, introductory statistics and econometrics. Prior Knowledge in Statistical software or programming languages will be useful.<br/><br/>"
     
-    "processed": "ECON4103 Assumed knowledge: Familiarity with matrix algebra && introductory statistics && econometrics. Prior Knowledge in Statistical software || programming languages will be useful"
+    "processed": "ECON4103"
+
+    "warning": "Assumed knowledge: Familiarity with matrix algebra && introductory statistics && econometrics. Prior Knowledge in Statistical software || programming languages will be useful"    
     """
 
-    return "ECON4103"
+    return {
+        "original": conditions["original"],
+        "processed": "ECON4103",
+        "warning": "Assumed knowledge: Familiarity with matrix algebra && introductory statistics && econometrics. Prior Knowledge in Statistical software || programming languages will be useful"
+    }
 
 def ECON_4303():
     """
