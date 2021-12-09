5c4273506562b0e06c4857a652d749bb8d25b716
JamesyJi <49261993+JamesyJi@users.noreply.github.com>
2021-12-09 22:04:35 +1100

    Manual fixes (#150)
    * Create INFSFixes.py with template
    
    * Done with INFS!
    
    * started on TABL
    
    * 2022 TABLfixes done + some improvements in conditions regexing
    
    * Manual fix of MGMT
    
    * Complete MGMT fixes
    
    * Updated INFS manual fixes
    
    * Updated MGMT manual fixes
    
    * Reverted path changes
    
    * missing a single "in" keyword
    
    * math manual fixes (#96)
    
    * MathFixes
    
    * Update MATHFixes.py
    
    Co-authored-by: James Ji <jamesx.ji@gmail.com>
    Co-authored-by: JamesyJi <49261993+JamesyJi@users.noreply.github.com>
    
    * SENG fixes done (#149)
    
    * Psyc manual fixes (#135)
    
    * psyc fixes in progress:
    
    * updated more
    
    * continued on psyc
    
    Co-authored-by: Sanojan Thiyagaraja <sanojan99@gmail.com>
    
    * minor header comment fix changing [CODE] to the actual course code
    
    * Engg manual fixes (#90)
    
    * still need to figure out the pls refer to course overview
    
    * not sure about check course overview
    
    * Update ENGGFixes.py
    
    * edits to engg2600-4600
    
    Co-authored-by: JamesyJi <49261993+JamesyJi@users.noreply.github.com>
    Co-authored-by: James Ji <jamesx.ji@gmail.com>
    
    * Econ manual fixes (#89)
    
    * still need to figure out the pls refer to course overview
    
    * added ECON1203
    
    * fixed the processed, not sure about ECON4201
    
    * added ECON2403
    
    * added ECON1101 and fixed ECON1203, not sure with both
    
    * Delete ENGGFixes.py
    
    Think this is in your other PR for ENGG
    
    * fixed up econ manual fixes
    
    Co-authored-by: JamesyJi <49261993+JamesyJi@users.noreply.github.com>
    Co-authored-by: James Ji <jamesx.ji@gmail.com>
    
    * saving progress on math manual fixes
    
    * finished math manual fixes
    
    * minor fixes to math manual fixes and ran processing on all manual fixes
    
    Co-authored-by: Jennifer Xu <jenniferwen00@gmail.com>
    Co-authored-by: IsaaacYaner <47761372+IsaacYaner@users.noreply.github.com>
    Co-authored-by: Adam Brieger <adam.brieger@gmail.com>
    Co-authored-by: Adam Brieger <77366819+abrieger@users.noreply.github.com>
    Co-authored-by: Sally Sun <sallyysun101@gmail.com>
    Co-authored-by: Sanojan Thiyagaraja <sanojan99@gmail.com>
    Co-authored-by: Gabriella Hartanto <35056564+gabriellahartanto@users.noreply.github.com>

---
+++ backend/data/processors/manualFixes/SENGFixes.py
@@ -0,0 +1,99 @@
+"""
+https://github.com/csesoc/Circles/wiki/Manual-Fixes-to-Course-Prerequisites
+
+Apply manual COMP fixes to processed conditions in conditionsProcessed.json so 
+that they can be fed into algorithms.
+
+If you make a mistake and need to regenerate conditionsProcessed.json, then you
+can run:
+    python3 -m data.processors.conditionsPreprocessing
+
+To then run this file:
+    python3 -m data.processors.manualFixes.SENGFixes
+"""
+
+from data.utility import dataHelpers
+
+CONDITIONS = dataHelpers.read_data("data/finalData/conditionsProcessed.json")
+PROCESSED = "processed"
+
+COURSES = dataHelpers.read_data("data/finalData/coursesProcessed.json")
+
+def fix_conditions():
+    """ Functions to apply manual fixes """
+
+    CONDITIONS["SENG2021"][PROCESSED] = SENG_2021()
+    CONDITIONS["SENG2991"] = SENG_2991(CONDITIONS["SENG2991"])
+    CONDITIONS["SENG3991"] = SENG_3991(CONDITIONS["SENG3991"])
+    CONDITIONS["SENG3992"] = SENG_3992(CONDITIONS["SENG3992"])
+    CONDITIONS["SENG4920"][PROCESSED] = SENG_4920()
+
+    dataHelpers.write_data(
+        CONDITIONS, "data/finalData/conditionsProcessed.json")
+    dataHelpers.write_data(COURSES, "data/finalData/coursesProcessed.json")
+
+
+def SENG_2021():
+    """
+    "original": "Prerequisite:  SENG1031 or COMP1531, and enrolment in a BE or BE(Hons) Software Engineering major.<br/><br/>",
+
+    "processed": "SENG1031 || COMP1531 && (SENG?1 || SENG?H)"
+    """
+
+    return "SENG1031 || COMP1531 && (SENG?1 || SENG?H)"
+
+def SENG_2991(conditions):
+    """ 
+    "original": "Currently enrolled in Program 3707 in the SENGAH stream <br/>and in an approved workplace arrangement <br/>and completed COMP1511, COMP1531 and COMP2521.<br/><br/>"
+    
+    "processed": "COMP1511 && COMP1531 && COMP2521 && 3707 && SENGAH"
+
+    "warning": "Must be in an approved workplace arrangement"
+    """
+
+    return {
+        "original": conditions["original"],
+        "processed": "COMP1511 && COMP1531 && COMP2521 && 3707 && SENGAH",
+        "warning": "Must be in an approved workplace arrangement"
+    }
+
+def SENG_3991(conditions):
+    """
+    "original": "Currently enrolled in Program 3707 in the SENGAH stream and in the Co-op program<br/>and completed SENG2011 and SENG2021 and DESN2000 and COMP2511 and COMP2041.<br/><br/>Successful completion of SEN2991 and attendance of IT Reflection Workshop (facilitated by Co-op Program) are a<br/>prerequisite for this course.<br/><br/>"
+
+    "processed": "3707 && SENGAH && SENG2011 && SENG2021 && DESN2000 && COMP2511 && COMP2041 && SENG2991"
+
+    "warning": "Must currently be enrolled in the Co-op program and completed attendance of IT Reflection Workshop (facilitated by Co-op Program)"
+    """
+
+    return {
+        "original": conditions["original"],
+        "processed": "3707 && SENGAH && SENG2011 && SENG2021 && DESN2000 && COMP2511 && COMP2041 && SENG2991",
+        "warning": "Must currently be enrolled in the Co-op program and completed attendance of IT Reflection Workshop (facilitated by Co-op Program)"
+    }
+
+def SENG_3992(conditions):
+    """
+    "original": "Currently enrolled in Program 3707 in the SENGAH stream and in the Co-op program<br/>and completed SENG3011.<br/><br/>Successful completion of SEN2991 and SENG3991are a prerequisite for this course<br/><br/>",
+    
+    "processed": "3707 && SENGAH && SENG3011 && SENG2991 && SENG3991"
+    
+    "warning": "Must currently be enrolled in the Co-op program"
+    """
+    return {
+        "original": conditions["original"],
+        "processed": "3707 && SENGAH && SENG3011 && SENG2991 && SENG3991",
+        "warning": "Must currently be enrolled in the Co-op program"
+    }
+
+def SENG_4920():
+    """
+    "original": "Prerequisite: COMP2511<br/><br/><br/>Completed more than or equal to 144 UOC in SENGAH, BINFAH or COMPBH<br/><br/>"
+
+    "processed": "COMP2511 && 144UOC in (SENGAH || BINFAH || COMPBH)"
+    """
+
+    return "COMP2511 && 144UOC in (SENGAH || BINFAH || COMPBH)"
+
+if __name__ == "__main__":
+    fix_conditions()
