df7e22851b06e9c02043f355f2f4978e78b01b99
JamesyJi <49261993+JamesyJi@users.noreply.github.com>
2021-12-19 00:50:52 +1100

    Dockerise (#154)
    * docker-compose up works in basic form for backend
    
    * force overwrite
    
    Co-authored-by: Adam Brieger <adam.brieger@gmail.com>

--- backend/server/config.py
+++ backend/server/config.py
@@ -1,4 +1,4 @@
-URI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+URI = "mongodb://mongodb:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
 FINAL_DATA_PATH = "./data/finalData/"
 
