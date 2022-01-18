66b18b07dc322d74e9bdba45092d0a05d04b0a3b
Peter Derias <peter_derias@hotmail.com>
2022-01-19 00:44:38 +1100

    CIRCLES-246: remove extraneous thinkg

--- backend/server/server.py
+++ backend/server/server.py
@@ -30,7 +30,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
