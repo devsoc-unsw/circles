f73d9323539168e2f156eeaf5ef64aa9994d39fa
Sanojan <68191852+Snuzzn@users.noreply.github.com>
2022-01-23 13:41:08 +1100

    Gab/debouncing (#172)
    * added redux store for planner
    
    * redesigning unplanned store
    
    * refactored to work with redux store
    
    * removed need to store sortedUnplanned in redux store
    
    * set up store and logic for warning prereq check:
    
    * start year and degree length added
    
    * can now parse logical prereq expressions
    
    * updated warning style
    
    * added action for updating degree length
    
    * Delete package-lock.json
    
    * updated colours
    
    * refactored checkPrereq to make it more straightforward
    
    * warnings are displayed if a prereq is placed after/during
    
    * Sano/tp/context menu (#59)
    
    * made basic context menu with funcitoning delete
    
    * implemented unschedule
    
    * impelemented info - need route support on course selector
    
    * sidebar icon is sticky
    
    * updated no more courses message
    
    * unschedule and delete now update current courses in term planner, also context menu icons
    
    * info switches to appropirate course in cs
    
    * changed context menu text
    
    * active key in tab is now correct
    
    Authored-by: Sanojan Thiyagaraja <sanojan99@gmail.com>
    
    * Remove package files
    
    * Sano/tp/unplanned column (#62)
    
    * made basic context menu with funcitoning delete
    
    * implemented unschedule
    
    * impelemented info - need route support on course selector
    
    * sidebar icon is sticky
    
    * updated no more courses message
    
    * unschedule and delete now update current courses in term planner, also context menu icons
    
    * functioning summer term toggle
    
    * default checked toggle state
    
    * summer term view is separately styled
    
    * responsive font sizes on courses
    
    * highlight current year
    
    * fixed degree selector to term planner bug
    
    * responsive to smaller screen
    
    * fixed term resizing issue when hovering
    
    * Style changes
    
    Co-authored-by: Sanojan Thiyagaraja <sanojan99@gmail.com>
    Co-authored-by: Jennifer Xu <jenniferwen00@gmail.com>
    
    * Sano/tp/unplanned column (#63)
    
    added unplanned column with summer term toggle
    
    * debouncing functionality works
    
    * Sano/tp/complete term (#86)
    
    terms can now can be marked as complete
    
    * [CF-69] Change the Favicon and Title name #98
    
    * Delete package-lock.json
    
    * Move the selector to search tab
    
    * moved search bar to top and now opens new tab on search
    
    Co-authored-by: Sanojan Thiyagaraja <sanojan99@gmail.com>
    Co-authored-by: JWen00 <33746848+JWen00@users.noreply.github.com>
    Co-authored-by: Jennifer Xu <jenniferwen00@gmail.com>
    Co-authored-by: Jennifer Wen Xu <z5207930@unsw.edu.au>
    Co-authored-by: gabriellahartanto <gabriella.hartanto@icloud.com>

--- frontend/src/pages/CourseSelector/main.jsx
+++ frontend/src/pages/CourseSelector/main.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import CourseMenu from './courseMenu/CourseMenu';
-import CourseDescription from './courseDescription/CourseDescription';
-import { CourseTabs } from './CourseTabs';
-import './main.less';
+import React from "react";
+import { useSelector } from "react-redux";
+import CourseMenu from "./courseMenu/CourseMenu";
+import CourseDescription from "./courseDescription/CourseDescription";
+import { CourseTabs } from "./CourseTabs";
+import "./main.less";
+import SearchCourse from "./SearchCourse";
 
 export default function CourseSelector() {
-  const degree = useSelector(state => state.degree);
+  const degree = useSelector((state) => state.degree);
   return (
-      <div className='cs-root'>
-        <div className='cs-top-cont'>
-          <div className='cs-degree-cont'>
-            <h1 className='text'>{ degree.programCode } - { degree.programName }</h1>
-          </div>
+    <div className="cs-root">
+      <div className="cs-top-cont">
+        <div className="cs-degree-cont">
+          {degree.programCode !== "" && (
+            <h1 className="text">
+              {degree.programCode} - {degree.programName}
+            </h1>
+          )}
         </div>
-          <CourseTabs/>
-        <div className='cs-bottom-cont'>
-          <CourseMenu/>
-          <CourseDescription />
-        </div>
+        <SearchCourse />
       </div>
+      <CourseTabs />
+      <div className="cs-bottom-cont">
+        <CourseMenu />
+        <CourseDescription />
+      </div>
+    </div>
   );
 }
