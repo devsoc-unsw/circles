c212933519c0cbb5105554d07d4c35570eb3e600
Peter Derias <peter_derias@hotmail.com>
2022-02-05 20:38:38 +1100

    CIRCLES-256: seeing what is going on in github

--- backend/server/routers/courses.py
+++ backend/server/routers/courses.py
@@ -18,8 +18,8 @@ def apiIndex():
 def fixUserData(userData: dict):
     ''' updates and returns the userData with the UOC of a course '''
     coursesWithoutUoc = [course for course in userData["courses"] if type(userData["courses"][course]) is int]
-    filledInCourses = {course : [getCourse(course)["UOC"], userData["courses"][course]] for course in coursesWithoutUoc}
     return "got here"
+    filledInCourses = {course : [getCourse(course)["UOC"], userData["courses"][course]] for course in coursesWithoutUoc}
     if any(type(courseValues[1]) is JSONResponse for courseValues in filledInCourses.values()):
         return JSONResponse(status_code=400, content={"message": "a course supplied could not be found"})
     userData["courses"].update(filledInCourses)
