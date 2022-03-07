3a4243f3696f2675227098b5f26a6d0fb90de219
Hussain Nawaz (MBP) <hussain.nwz000@gmail.com>
2022-03-07 22:27:42 +1100

    cleaned load_conditions.py as per pylint / black

--- backend/algorithms/load_conditions.py
+++ backend/algorithms/load_conditions.py
@@ -3,8 +3,9 @@ Will pickle all the conditions into a dictionary. Run this file from the backend
 folder with python3 -m algorithms.load_conditions
 """
 
-import pickle
 import json
+import pickle
+
 from algorithms.create import create_condition
 
 # Load in all the courses and their conditions list
@@ -18,6 +19,12 @@ ALL_OBJECTS = {}
 
 
 def cache_conditions_pkl_file():
+    """
+    Loads all conditions objects and dumps them as a pickled file
+
+    Input: None
+    Returns: None
+    """
     for course, tokens in ALL_CONDITIONS_TOKENS.items():
         ALL_OBJECTS[course] = create_condition(tokens, course)
 
@@ -25,5 +32,5 @@ def cache_conditions_pkl_file():
         pickle.dump(ALL_OBJECTS, outp, pickle.HIGHEST_PROTOCOL)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cache_conditions_pkl_file()
