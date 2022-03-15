e321b7794f482053c274c564b33b2689983de24f
Hussain Nawaz (MBP) <hussain.nwz000@gmail.com>
2022-03-16 00:51:02 +1100

    pls rerun i beg

--- backend/server/routers/programs.py
+++ backend/server/routers/programs.py
@@ -113,6 +113,7 @@ def getMajors(programCode: str):
     },
 )
 
+
 def getMinors(programCode: str):
     """fetch all the minors known for a specific program"""
     result = programsCOL.find_one({"code": programCode})
