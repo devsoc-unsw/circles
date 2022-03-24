48a4b6af5ad835a941a7bb9f18f5666807bed19f
Hussain Nawaz (MBP) <hussain.nwz000@gmail.com>
2022-03-24 14:20:58 +1100

    273 rebase

--- backend/server/tests/test_getStructure.py
+++ backend/server/tests/test_getStructure.py
@@ -2,36 +2,62 @@
 import requests
 from hypothesis import given
 from hypothesis.strategies import sampled_from, composite, DrawFn
-programs = [*requests.get('http://127.0.0.1:8000/programs/getPrograms').json()['programs'].keys()]
+
+programs = [
+    *requests.get("http://127.0.0.1:8000/programs/getPrograms")
+    .json()["programs"]
+    .keys()
+]
+
 
 @composite
 def major_minor_for_program(draw: DrawFn):
     program = draw(sampled_from(programs))
-    major = draw(sampled_from([*requests.get(f'http://127.0.0.1:8000/programs/getMajors/{program}').json()['majors'].keys()]))
-    minor = draw(sampled_from([*requests.get(f'http://127.0.0.1:8000/programs/getMinors/{program}').json()['minors'].keys()]))
+    major = draw(
+        sampled_from(
+            [
+                *requests.get(f"http://127.0.0.1:8000/programs/getMajors/{program}")
+                .json()["majors"]
+                .keys()
+            ]
+        )
+    )
+    minor = draw(
+        sampled_from(
+            [
+                *requests.get(f"http://127.0.0.1:8000/programs/getMinors/{program}")
+                .json()["minors"]
+                .keys()
+            ]
+        )
+    )
     return (program, major, minor)
 
 
 @given(sampled_from(programs))
 def test_all_programs_fetched(program):
-    structure = requests.get(f'http://127.0.0.1:8000/programs/getStructure/{program}')
+    structure = requests.get(f"http://127.0.0.1:8000/programs/getStructure/{program}")
     assert structure != 500
-    structure.json()['structure']['General'] != {}
+    structure.json()["structure"]["General"] != {}
+
 
 @given(major_minor_for_program())
 def test_all_majors_minors_fetched(specifics):
-    structure = requests.get(f'http://127.0.0.1:8000/programs/getStructure/{specifics[0]}/{specifics[1]}/{specifics[2]}')
+    structure = requests.get(
+        f"http://127.0.0.1:8000/programs/getStructure/{specifics[0]}/{specifics[1]}/{specifics[2]}"
+    )
 
-    assert structure.json()['structure']['General'] != {}
-    assert structure.json()['structure']['Major'] != {}
-    assert structure.json()['structure']['Minor'] != {}
+    assert structure.json()["structure"]["General"] != {}
+    assert structure.json()["structure"]["Major"] != {}
+    assert structure.json()["structure"]["Minor"] != {}
+
 
 @given(major_minor_for_program())
 def test_all_majors_fetched(specifics):
-    structure = requests.get(f'http://127.0.0.1:8000/programs/getStructure/{specifics[0]}/{specifics[1]}')
+    structure = requests.get(
+        f"http://127.0.0.1:8000/programs/getStructure/{specifics[0]}/{specifics[1]}"
+    )
 
-    assert structure.json()['structure']['General'] != {}
-    assert structure.json()['structure']['Major'] != {}
-    assert structure.json()['structure'].get('Minor') == None
-
-
+    assert structure.json()["structure"]["General"] != {}
+    assert structure.json()["structure"]["Major"] != {}
+    assert structure.json()["structure"].get("Minor") is None
