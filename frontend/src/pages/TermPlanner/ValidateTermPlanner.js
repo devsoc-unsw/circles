328ce5d5a3686e709271ef6ac01f46d85cf021ee
Leonardo Fan <leonardofan2000@gmail.com>
2022-03-28 23:44:40 +1100

    added getMostRecentPastTerm function

--- frontend/src/pages/TermPlanner/ValidateTermPlanner.js
+++ frontend/src/pages/TermPlanner/ValidateTermPlanner.js
@@ -25,6 +25,31 @@ const validateTermPlanner = (payload) => {
   };
 };
 
+// takes in startYear (int) and gets current date to return the most recent term that has past (week 2)
+// string of year in degree (not array index) and term most recent is returned e.g. 'Y3T0'
+const getMostRecentPastTerm = (startYear) => {
+  let currTime = new Date();
+  let currYear = currTime.getFullYear();
+  let currMonth = currTime.getMonth();
+  let currDay = currTime.getDate();
+
+  const currDegreeYear = currYear - startYear + 1;
+
+  // session dates gathered from: https://www.student.unsw.edu.au/teaching-periods
+  let pastTerm;
+  if ((currDay > 29 && currMonth == 8) || currMonth > 8) {
+    pastTerm = 3;
+  } else if ((currDay > 14 && currMonth == 5) || currMonth > 5) {
+    pastTerm = 2;
+  } else if ((currDay >= 28 && currMonth == 1) || currMonth > 1) {
+    pastTerm = 1;
+  } else {
+    pastTerm = 0;
+  }
+
+  return `Y${currDegreeYear}T${pastTerm}`;
+};
+
 const prepareCoursesForValidation = (plannerInfo, userInfo) => {
   const { years, startYear, _ } = plannerInfo;
   const { programCode, specialisation, minor } = userInfo;
