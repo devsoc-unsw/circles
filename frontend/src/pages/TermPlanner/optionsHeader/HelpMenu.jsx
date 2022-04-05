1451e6314920152998a8f889e0b4f78bd4e91bcb
Leonardo Fan <leonardofan2000@gmail.com>
2022-04-05 16:40:12 +1000

    styling and usage of new css helpermenu class

--- frontend/src/pages/TermPlanner/optionsHeader/HelpMenu.jsx
+++ frontend/src/pages/TermPlanner/optionsHeader/HelpMenu.jsx
@@ -19,65 +19,73 @@ const HelpMenu = () => {
   const { Title } = Typography;
 
   return (
-    <div className="settingsMenu">
-      <div className="settingsTitleContainer">
+    <div className="helpMenu">
+      <div className="helpTitleContainer">
         <Title level={2} class="text" strong className="settingsTitle">
-          Term Planner Tips - Hover to Play
+          Term Planner Tips (Hover to Play)
         </Title>
         <Divider className="settingsDivider" />
       </div>
-      <div className="settingsEntry">
-        <Title level={3} class="text settingsSubtitle">
-          Drag and Drop Courses
-        </Title>
+      <div>
+        <div className="settingsEntry">
+          <Title level={3} class="text settingsSubtitle">
+          1. Drag and Drop Courses
+          </Title>
+        </div>
+        <img 
+          className="helpGif" 
+          onMouseEnter={() => {setDragAndDropPlaying(true)}} 
+          onMouseLeave={() => {setDragAndDropPlaying(false)}}
+          src={dragAndDropPlaying ? dragAndDropGif : dragAndDropPng}
+          alt="drag and drop walkthrough"
+          style={dragAndDropPlaying ? {boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"} : {}}
+        ></img>
       </div>
-      <img 
-        className="helpGif" 
-        onMouseEnter={() => {setDragAndDropPlaying(true)}} 
-        onMouseLeave={() => {setDragAndDropPlaying(false)}}
-        src={dragAndDropPlaying ? dragAndDropGif : dragAndDropPng}
-        alt="drag and drop walkthrough"
-        style={dragAndDropPlaying ? {boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"} : {}}
-      ></img>
-      <div className="settingsEntry">
-        <Title level={3} class="text settingsSubtitle">
-          Right Click Courses for More Actions
-        </Title>
+      <div>
+        <div className="settingsEntry">
+          <Title level={3} class="text settingsSubtitle">
+          2. Right Click Courses for More Actions
+          </Title>
+        </div>
+        <img 
+          className="helpGif" 
+          onMouseEnter={() => {setUnschedulePlaying(true)}} 
+          onMouseLeave={() => {setUnschedulePlaying(false)}}
+          src={unschedulePlaying ? unscheduleGif : unschedulePng}
+          alt="context menu walkthrough"
+          style={unschedulePlaying ? {boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"} : {}}
+        ></img>
       </div>
-      <img 
-        className="helpGif" 
-        onMouseEnter={() => {setUnschedulePlaying(true)}} 
-        onMouseLeave={() => {setUnschedulePlaying(false)}}
-        src={unschedulePlaying ? unscheduleGif : unschedulePng}
-        alt="context menu walkthrough"
-        style={unschedulePlaying ? {boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"} : {}}
-      ></img>
-      <div className="settingsEntry">
-        <Title level={3} class="text settingsSubtitle">
-          Hide Year
-        </Title>
+      <div>
+        <div className="settingsEntry">
+          <Title level={3} class="text settingsSubtitle">
+          3. Hide Year
+          </Title>
+        </div>
+        <img 
+          className="helpGif" 
+          onMouseEnter={() => {setHideYearPlaying(true)}} 
+          onMouseLeave={() => {setHideYearPlaying(false)}}
+          src={hideYearPlaying ? hideYearGif : hideYearPng}
+          alt="hide year walkthrough"
+          style={hideYearPlaying ? {boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"} : {}}
+        ></img>
       </div>
-      <img 
-        className="helpGif" 
-        onMouseEnter={() => {setHideYearPlaying(true)}} 
-        onMouseLeave={() => {setHideYearPlaying(false)}}
-        src={hideYearPlaying ? hideYearGif : hideYearPng}
-        alt="hide year walkthrough"
-        style={hideYearPlaying ? {boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"} : {}}
-      ></img>
-      <div className="settingsEntry">
-        <Title level={3} class="text settingsSubtitle">
-          Additional Features
-        </Title>
+      <div>
+        <div className="settingsEntry">
+          <Title level={3} class="text settingsSubtitle">
+          4. Additional Features
+          </Title>
+        </div>
+        <img 
+          className="helpGif" 
+          onMouseEnter={() => {setAdditionalOptionsPlaying(true)}} 
+          onMouseLeave={() => {setAdditionalOptionsPlaying(false)}}
+          src={additionalOptionsPlaying ? additionalOptionsGif : additionalOptionsPng}
+          alt="additional options walkthrough"
+          style={additionalOptionsPlaying ? {boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"} : {}}
+        ></img>
       </div>
-      <img 
-        className="helpGif" 
-        onMouseEnter={() => {setAdditionalOptionsPlaying(true)}} 
-        onMouseLeave={() => {setAdditionalOptionsPlaying(false)}}
-        src={additionalOptionsPlaying ? additionalOptionsGif : additionalOptionsPng}
-        alt="additional options walkthrough"
-        style={additionalOptionsPlaying ? {boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"} : {}}
-      ></img>
     </div>
   );
 };
