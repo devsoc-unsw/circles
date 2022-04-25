5327290e9883442379361d436afaad2cf6caf147
Gabriel Ting <65492346+gtangelo@users.noreply.github.com>
2022-04-26 09:01:33 +1000

    CF-190 changed back to localhost for axios

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // fallback to localhost port 8000 if env variable is not specified
-axios.defaults.baseURL = process.env.REACT_APP_BACKEND_API_BASE_URL || "https://circlesapi.csesoc.app/";
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND_API_BASE_URL || "http://localhost:8000/";
 
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
