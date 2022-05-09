098ba8088fa07e6e79fd0dc6a922f21a2dff819e
Leonardo Fan <leonardofan2000@gmail.com>
2022-05-09 23:26:15 +1000

    css styling to make unplanned absolute

--- frontend/src/pages/TermPlanner/TermPlanner.jsx
+++ frontend/src/pages/TermPlanner/TermPlanner.jsx
@@ -198,6 +198,7 @@ const TermPlanner = () => {
                         />
                       );
                     })}
+                    <div />
                   </React.Fragment>
                 );
               })}
