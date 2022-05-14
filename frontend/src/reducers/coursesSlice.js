9f75f4f24ecb9aada528a4fd96b9cecc9e84161f
Uzaif Sheikh <50875291+Uzaif-Sheikh@users.noreply.github.com>
2022-05-14 15:20:53 +1000

    Uzaif/cf 207 (#461)
    * replaced 'locked courses' with icon and saving the state in redux
    
    * Added state change in courseSlice.js

--- frontend/src/reducers/coursesSlice.js
+++ frontend/src/reducers/coursesSlice.js
@@ -1,9 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+let initialState = {
   courses: {},
   course: {},
+  isLockedEnabled: false,
 };
+
+const courses = JSON.parse(localStorage.getItem("courses"));
+if (courses && courses.version === initialState.version) {
+  initialState = courses;
+} else if (courses && courses.version !== initialState.version) {
+  localStorage.setItem("isUpdate", true);
+}
 
 const coursesSplice = createSlice({
   name: "courses",
@@ -11,16 +19,25 @@ const coursesSplice = createSlice({
   reducers: {
     setCourses: (state, action) => {
       state.courses = action.payload;
+      localStorage.setItem("courses", JSON.stringify(state));
     },
     setCourse: (state, action) => {
       state.course = action.payload;
+      localStorage.setItem("courses", JSON.stringify(state));
     },
-    resetCourses: () => initialState,
+    toggleCourseLock: (state) => {
+      state.isLockedEnabled = !state.isLockedEnabled;
+      localStorage.setItem("courses", JSON.stringify(state));
+    },
+    resetCourses: (state) => {
+      Object.assign(state, initialState);
+      localStorage.removeItem("courses", JSON.stringify(state));
+    },
   },
 });
 
 export const {
-  setCourses, setCourse, resetCourses,
+  setCourses, setCourse, toggleCourseLock, resetCourses,
 } = coursesSplice.actions;
 
 export default coursesSplice.reducer;
