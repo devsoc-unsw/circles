1493b546e7a760328afb8d84991c0e42c5e7c5a0
hdphuong <63543383+hdphuong@users.noreply.github.com>
2022-05-17 09:08:16 +1000

    modify test_getStructure for the new getstructure()

--- backend/server/tests/programs/test_getStructure.py
+++ backend/server/tests/programs/test_getStructure.py
@@ -2,6 +2,7 @@
 import requests
 from hypothesis import given
 from hypothesis.strategies import DrawFn, composite, sampled_from
+from functools import reduce
 
 programs = [
     *requests.get("http://127.0.0.1:8000/programs/getPrograms")
@@ -13,15 +14,16 @@ programs = [
 @composite
 def major_minor_for_program(draw: DrawFn):
     program = draw(sampled_from(programs))
-    major = draw(
+    majorGroup = draw(
         sampled_from(
             [
-                *requests.get(f"http://127.0.0.1:8000/programs/getMajors/{program}")
-                .json()["majors"]
-                .keys()
+                *requests.get(f"http://127.0.0.1:8000/programs/getMajors/{program}").json()["majors"].values()
             ]
         )
     )
+    major = draw(sampled_from([*majorGroup.keys()]))
+
+
     minor = draw(
         sampled_from(
             [
@@ -40,18 +42,17 @@ def test_all_programs_fetched(program):
     assert structure != 500
     structure.json()["structure"]["General"] != {}
 
-''' 
 @given(major_minor_for_program())
 def test_all_majors_minors_fetched(specifics):
     structure = requests.get(
         f"http://127.0.0.1:8000/programs/getStructure/{specifics[0]}/{specifics[1]}/{specifics[2]}"
     )
+    minorKey = next(k for k in structure.json()["structure"].keys() if "Minor" in k)
 
     assert structure.json()["structure"]["General"] != {}
-    assert structure.json()["structure"]["Major"] != {}
-    assert structure.json()["structure"]["Minor"] != {}
+    assert structure.json()["structure"][minorKey] != {}
+
     course_list = []
-    minorKey = next(k for k in structure.json()["structure"].keys() if "Minor" in k)
     for container_name, container in structure.json()["structure"][minorKey].items():
         if container_name == "Core Courses":
             course_list = container["courses"].keys()
@@ -66,10 +67,10 @@ def test_all_majors_fetched(specifics):
     structure = requests.get(
         f"http://127.0.0.1:8000/programs/getStructure/{specifics[0]}/{specifics[1]}"
     )
-
+    majorKey = next(k for k in structure.json()["structure"].keys() if "Major" in k)
     assert structure.json()["structure"]["General"] != {}
-    assert structure.json()["structure"]["Major"] != {}
-    assert structure.json()["structure"].get("Minor") is None
+    assert structure.json()["structure"][majorKey] != {}
+
     # also assert that there are no duplicates
     course_list = []
     majorKey = next(k for k in structure.json()["structure"].keys() if "Major" in k)
@@ -79,5 +80,3 @@ def test_all_majors_fetched(specifics):
     for container_name, container in structure.json()["structure"][majorKey].items():
         if "courses" in container and container_name != "Core Courses":
             assert all(course not in course_list for course in container["courses"])
-
-'''
