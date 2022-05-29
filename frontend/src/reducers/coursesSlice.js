c72462024ed583a16767a1ebdb3bd5e684291aac
Peedee2002 <64952797+Peedee2002@users.noreply.github.com>
2022-05-29 23:20:16 +1000

    Update coursesSlice.js

--- frontend/src/reducers/coursesSlice.js
+++ frontend/src/reducers/coursesSlice.js
@@ -6,34 +6,20 @@ let initialState = {
   isLockedEnabled: false,
 };
 
-const courses = JSON.parse(localStorage.getItem("courses"));
-if (courses && courses.version === initialState.version) {
-  initialState = courses;
-} else if (courses && courses.version !== initialState.version) {
-  localStorage.setItem("isUpdate", true);
-}
-
 const coursesSplice = createSlice({
   name: "courses",
   initialState,
   reducers: {
     setCourses: (state, action) => {
       state.courses = action.payload;
-      localStorage.setItem("courses", JSON.stringify(state));
     },
     setCourse: (state, action) => {
       state.course = action.payload;
-      localStorage.setItem("courses", JSON.stringify(state));
     },
     toggleCourseLock: (state) => {
       state.isLockedEnabled = !state.isLockedEnabled;
-      localStorage.setItem("courses", JSON.stringify(state));
-    },
-    resetCourses: (state) => {
-      Object.assign(state, initialState);
-      localStorage.removeItem("courses", JSON.stringify(state));
-    },
-  },
+     },
+    resetCourses: (state) => {},
 });
 
 export const {
