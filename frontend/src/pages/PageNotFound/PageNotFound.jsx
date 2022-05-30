6925d087718b75d417388a8a82629748ad3ff6a0
Uzaif <uzaifsheikh2309@gmail.com>
2022-05-30 22:55:08 +1000

    Added go back btn for 404

--- frontend/src/pages/PageNotFound/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound/PageNotFound.jsx
@@ -1,57 +1,63 @@
 import React from "react";
-import { Typography } from "antd";
+import { Button, Typography } from "antd";
 import "./index.less";
+import { useNavigate } from "react-router-dom";
 import circlesLogo from "../../assets/circlesLogo.svg";
 
 const { Title } = Typography;
-const PageNotFound = () => (
-  <>
-    <div className="main-con">
-      <div className="grid-circle">
-        <div className="box">
-          <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
+const PageNotFound = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="main-con">
+        <div className="grid-circle">
+          <div className="box">
+            <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
+          </div>
+          <div className="box-anti">
+            <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
+          </div>
+          <div className="box">
+            <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
+          </div>
+          <div className="box-anti">
+            <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
+          </div>
+          <div className="box-anti">
+            <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
+          </div>
+          <div className="box">
+            <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
+          </div>
+          <div className="box-anti">
+            <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
+          </div>
+          <div className="box">
+            <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
+          </div>
+          <div className="box-anti">
+            <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
+          </div>
+          <div className="box">
+            <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
+          </div>
+          <div className="box-anti">
+            <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
+          </div>
+          <div className="box">
+            <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
+          </div>
         </div>
-        <div className="box-anti">
-          <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
-        </div>
-        <div className="box">
-          <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
-        </div>
-        <div className="box-anti">
-          <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
-        </div>
-        <div className="box-anti">
-          <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
-        </div>
-        <div className="box">
-          <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
-        </div>
-        <div className="box-anti">
-          <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
-        </div>
-        <div className="box">
-          <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
-        </div>
-        <div className="box-anti">
-          <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
-        </div>
-        <div className="box">
-          <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
-        </div>
-        <div className="box-anti">
-          <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
-        </div>
-        <div className="box">
-          <img alt="circles-logo" src={circlesLogo} width="200" height="200" />
-        </div>
+        <div className="box-container" />
       </div>
-      <div className="box-container" />
-    </div>
-    <div className="text404">
-      <Title id="headd">404</Title>
-      <Title level={2} id="page-text">PAGE NOT FOUND</Title>
-    </div>
-  </>
-);
+      <div className="text404">
+        <Title className="not-found">404</Title>
+        <Title level={2} className="page-text">PAGE NOT FOUND</Title>
+        <Button type="primary" size="large" onClick={() => navigate("/degree-wizard")}>Go Back Home</Button>
+      </div>
+    </>
+  );
+};
 
 export default PageNotFound;
