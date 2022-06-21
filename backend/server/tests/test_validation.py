6c7bedd47e7de7716f769b3dc4572b0a93838799
Peter Derias <peter_derias@hotmail.com>
2022-06-21 11:33:39 +1000

    fix validation test - its gonna be fun woot woot

--- backend/server/tests/test_validation.py
+++ backend/server/tests/test_validation.py
@@ -11,17 +11,20 @@ ignored = ['ECON2209', 'ECON2112', 'ECON2101', 'ECON2102']
 def test_validation():
     unlocked = requests.post('http://127.0.0.1:8000/courses/getAllUnlocked', json=USERS["user3"]).json()['courses_state']
     for program in requests.get('http://127.0.0.1:8000/programs/getPrograms').json()['programs']:
+        print(program)
         majorsGroups = requests.get(f'http://127.0.0.1:8000/specialisations/getSpecialisations/{program}/majors')
         minorsGroups = requests.get(f'http://127.0.0.1:8000/specialisations/getSpecialisations/{program}/minors')
         honoursGroups = requests.get(f'http://127.0.0.1:8000/specialisations/getSpecialisations/{program}/honours')
         majorsGroups = majorsGroups.json()['spec'] if majorsGroups.status_code == 200 else {}
         minorsGroups = minorsGroups.json()['spec'] if minorsGroups.status_code == 200 else {}
         honoursGroups = honoursGroups.json()['spec'] if honoursGroups.status_code == 200 else {}
-
+        print(majorsGroups)
         for group in chain(majorsGroups.values(), minorsGroups.values(), honoursGroups.values()):
-            for major in group["specs"].keys():
-                if major not in fake_specs:
-                    assert_possible_structure(unlocked, program, major)
+            print(group)
+            for spec in group["specs"].keys():
+                if spec not in fake_specs:
+                    print(spec)
+                    assert_possible_structure(unlocked, program, spec)
 
 
 def assert_possible_structure(unlocked, program, spec):
@@ -36,7 +39,7 @@ def assert_possible_structure(unlocked, program, spec):
             with suppress(KeyError):
                 del structure[container][container2]['name']
 
-                for course in structure[container][container2]['courses']:
-                    for c in unlocked:
-                        if course in c and c not in ignored:
-                            assert unlocked[c]['is_accurate'], f"{c} is inaccurate"
+            for course in structure[container][container2]['courses']:
+                for c in unlocked:
+                    if course in c and c not in ignored:
+                        assert unlocked[c]['is_accurate'], f"{c} is inaccurate"
