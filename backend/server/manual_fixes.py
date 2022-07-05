ec0d84b6c9106b4811444161ad6c19835e2eacdf
Peter Derias <peter_derias@hotmail.com>
2022-07-05 14:19:49 +1000

    done fixing

--- backend/server/manual_fixes.py
+++ backend/server/manual_fixes.py
@@ -1,31 +1,40 @@
-
-import json
+from contextlib import suppress
 
 
 def remove_course(structure: dict, course: str) -> bool:
     """indescriminantly remove a course from structure"""
     for item, collection in structure.items():
+        if item == "Rules":
+            continue
         for container in collection:
             try:
-                if container == 'name':
-                    raise KeyError
+                if container == "name":
+                    continue
                 del structure[item][container]["courses"][course]
-                print(item, container)
-                return structure[item][container]
+                return structure[item]
             except KeyError:
                 pass
 
 def fix_3784(structure: dict):
     mutated_item = remove_course(structure, "ECON1202")
-    # if mutated_item:
-    #     print(mutated_item)
-    #     mutated_item["Core Courses"]["UOC"] -= 6
-    #     mutated_item["Prescribed Electives"]["UOC"] += 6
+    if mutated_item:
+        core_name = filter(lambda a: "core" in a.lower(), mutated_item.keys()).next()
+        elective_name = filter(lambda a: "prescribed" in a.lower(), mutated_item.keys()).next()
+        mutated_item[core_name]["UOC"] -= 6
+        mutated_item[elective_name]["UOC"] += 6
     return structure
 
+def fix_3785(structure: dict):
+    remove_course(structure, "COMP1911")
+    remove_course(structure, "ENGG1811")
+    remove_course(structure, "MATH1081")
+    mutated_item = remove_course(structure, "COMP4920")
+    mutated_item["Core Courses"]["UOC"] -= 12
+    return structure
 
 def apply_manual_fixes(structure, program_code):
     fixes = {
-        "3784": fix_3784
+        "3784": fix_3784,
+        "3785": fix_3785
     }
     return fixes.get(program_code, lambda a: a)(structure)
