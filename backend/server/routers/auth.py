94c5b3af182ea275c616caff57f87cfa5839a548
imagine-hussain <hussain.nwz000@gmail.com>
2022-07-08 01:34:59 +1000

    auth reset=

--- backend/server/routers/auth.py
+++ backend/server/routers/auth.py
@@ -1,9 +1,29 @@
 """ Routes to deal with user Authentication. """
 
 from fastapi import APIRouter
+import pyjwt
 
 router = APIRouter(
-    prefix="/courses",
-    tags=["courses"],
+    prefix="/auth",
+    tags=["auth"],
 )
+
+# Probably don't want to return a json, just a string
+@router.post(
+    "/login",
+    responses={
+        200: {
+            "description": "take a token, and validate it",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "token": "jwt..."
+                    }
+                }
+            }
+        }
+    }
+)
+def auth_login(token: jwt):
+    pass
 
--- frontend/src/pages/Auth/Auth.jsx
+++ frontend/src/pages/Auth/Auth.jsx
@@ -1,19 +1,43 @@
 /* eslint-disable */
 /* eslint no-console: "error" */
 
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 // import MetaTags from "react-meta-tags";
 import "./index.less";
 import jwt_decode from "jwt-decode";
 
 const Auth = () => {
+
+  const [userObject, setUserObject] = useState({});
+
   const handleCallbackResponse = (response) => {
     const jwt = response.credential;
+    if (requestLogin(jwt).err) {
+      console.log("failed: ", jwt)
+      return;
+    }
     console.log("callBackResponse with jwt: ", jwt);
-    const userObj = jwt_decode(jwt);
-    console.log(userObj);
+    setUserObject(jwt_decode(jwt));
+
     return jwt;
   };
+
+  // const requestLogin = async (c) => {
+  //   const [data, err] = await axiosRequest("get", `/courses/getCourse/${c}`);
+  //   if (!err) {
+  //     console.log("successfull login return", data);
+  //   }
+  // };
+
+  const requestLogin = async (token) => {
+    const [data, err] = await axiosRequest(
+      "post",
+      `/auth/login`,
+      { token },
+    );
+    return { data, err };
+  };
+
 
   useEffect(() => {
     /* global google */
