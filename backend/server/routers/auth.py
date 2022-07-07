402f023cc22ef1e17478b16d54ed7e101b807eb9
imagine-hussain <hussain.nwz000@gmail.com>
2022-07-08 01:50:40 +1000

    ummm, need user

--- backend/server/routers/auth.py
+++ backend/server/routers/auth.py
@@ -1,12 +1,35 @@
 """ Routes to deal with user Authentication. """
 
 from fastapi import APIRouter
-import pyjwt
+import jwt
 
 router = APIRouter(
     prefix="/auth",
     tags=["auth"],
 )
+
+# TODO: Need to validate secret somehow
+
+# TODO: Wrap this as a decorator aroudn proptected apis
+def validate_login(token) -> bool:
+    """
+        Take a token and validate a login off the following criteria
+            - token i
+            - token has not expired (given in unix time)
+            - email verified
+            - email exists in our BE
+    """
+    pass
+
+def validate_token(token):
+    """
+        Take a token and validate that:
+            - token not tampered
+            - Token is not expired
+            - email valid
+        This does not check if the user is valid
+    """
+    return token
 
 # Probably don't want to return a json, just a string
 @router.post(
@@ -25,5 +48,9 @@ router = APIRouter(
     }
 )
 def auth_login(token: jwt):
-    pass
-
+    if not validate_token(token):
+        return False
+    if not validate_login():
+        # Create new user
+        pass
+    return token
