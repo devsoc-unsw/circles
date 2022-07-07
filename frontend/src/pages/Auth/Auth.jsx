ae93bf03788279a99445b82b4bdf11cd1698bbe0
imagine-hussain <hussain.nwz000@gmail.com>
2022-07-07 22:34:38 +1000

    o

--- frontend/src/pages/Auth/Auth.jsx
+++ frontend/src/pages/Auth/Auth.jsx
@@ -1,21 +1,18 @@
 import React from "react";
-import MetaTags from 'react-meta-tags';
+import MetaTags from "react-meta-tags";
+import "./index.less";
 
-import "./index.less"
-
-const Auth = () => {
-    return (
-        <div>
-            <MetaTags>
-                <meta
-                    name="google-signin-client_id"
-                    content="696494854262-hlrt28hbdmq1imp99dafbkvcgo9b5ld0.apps.googleusercontent.com"
-                />
-            </MetaTags>
-            <h1>SIGN IN </h1>
-            <script src="https://apis.google.com/js/platform.js" async defer></script>
-        </div>
-    );
-};
+const Auth = () => (
+  <div>
+    <MetaTags>
+      <meta
+        name="google-signin-client_id"
+        content="696494854262-hlrt28hbdmq1imp99dafbkvcgo9b5ld0.apps.googleusercontent.com"
+      />
+    </MetaTags>
+    <h1>SIGN IN </h1>
+    <script src="https://apis.google.com/js/platform.js" async defer />
+  </div>
+);
 
 export default Auth;
