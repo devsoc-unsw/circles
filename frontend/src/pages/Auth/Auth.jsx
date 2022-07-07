b3c8c858523876bc50aa01738cedd6636f8d67f5
imagine-hussain <hussain.nwz000@gmail.com>
2022-07-08 00:31:30 +1000

    workign loging with hussain.nwz000@gmail.com's client

--- frontend/src/pages/Auth/Auth.jsx
+++ frontend/src/pages/Auth/Auth.jsx
@@ -4,18 +4,21 @@
 import React, { useEffect } from "react";
 // import MetaTags from "react-meta-tags";
 import "./index.less";
+import jwt_decode from "jwt-decode";
 
 const Auth = () => {
   const handleCallbackResponse = (response) => {
     const jwt = response.credential;
-    console.log(jwt);
+    console.log("callBackResponse with jwt: ", jwt);
+    const userObj = jwt_decode(jwt);
+    console.log(userObj);
     return jwt;
   };
 
   useEffect(() => {
     /* global google */
     google.accounts.id.initialize({
-      clientId: "696494854262-hlrt28hbdmq1imp99dafbkvcgo9b5ld0.apps.googleusercontent.com",
+      client_id: "1017197944285-i4ov50aak72667j31tuieffd8o2vd5md.apps.googleusercontent.com",
       callback: handleCallbackResponse,
     });
 
@@ -23,6 +26,9 @@ const Auth = () => {
       document.getElementById("signInDiv"),
       { theme: "outline", size: "large" },
     );
+
+    // recent acc prompt
+    google.accounts.id.prompt();
   }, []);
 
   return (
@@ -31,29 +37,5 @@ const Auth = () => {
     </div>
   );
 };
-  // const [reload, setReload] = useState(false);
-  //
-  // const refreshPage = () => {
-  //   setReload(reload);
-  // };
-  //
-  // return (
-  //   <div>
-  //     <MetaTags>
-  //       <title>
-  //         Circles Login
-  //       </title>
-  //       <meta
-  //         name="google-signin-client_id"
-  //         content="696494854262-hlrt28hbdmq1imp99dafbkvcgo9b5ld0.apps.googleusercontent.com"
-  //       />
-  //     </MetaTags>
-  //     <h1> IN </h1>
-  //     <div className="g-signin2" data-onsuccess="onSignIn" />
-  //
-  //     {/* Google Library */}
-  //     <script src="https://apis.google.com/js/platform.js" async defer />
-  //   </div>
-  // );
 
 export default Auth;
