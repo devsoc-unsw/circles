7540ed8f7f1dc5d2e76f86806035effbf3dcb84d
imagine-hussain <hussain.nwz000@gmail.com>
2022-07-09 09:05:59 +1000

    move clientID

--- backend/server/config.py
+++ backend/server/config.py
@@ -2,6 +2,8 @@
 
 URI = "mongodb://mongodb:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
+CLIENT_ID = "1017197944285-i4ov50aak72667j31tuieffd8o2vd5md.apps.googleusercontent.com"
+
 FINAL_DATA_PATH = "./data/final_data/"
 
 ARCHIVED_DATA_PATH = "./data/final_data/archive/processed/"
