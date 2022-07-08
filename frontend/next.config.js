50968bb30e10e222cfc4a3473488dbaf40f6cd1b
Gabriel Ting <65492346+gtangelo@users.noreply.github.com>
2022-07-08 21:47:28 +1000

    added dummy persist gate for ssr

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,21 +1,12 @@
 const withAntdLess = require("next-plugin-antd-less");
 
 module.exports = withAntdLess({
-  // optional
   modifyVars: {
     "@primary-color": "#9254de", // purple-5
     // Since we've disabled the toggling of themes, I've changed heading colour to black
     "@heading-color": "black",
     "@text-color": "black",
   },
-  // optional
-  // lessVarsFilePath: "./src/styles/variables.less",
-  // // optional
-  // lessVarsFilePathAppendToEndOfContent: false,
-  // optional https://github.com/webpack-contrib/css-loader#object
-  cssLoaderOptions: {},
-
-  // Other Config Here...
 
   webpack(config) {
     return config;
--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -8,11 +8,25 @@ import "./App.css";
 
 const persistor = persistStore(store);
 
+// TODO: Is there a need for SSR? Remove this once we dont need redux-persist
+// and saving storage in BE
+const PersistGateSSR = ({ children }) => {
+  if (typeof window === "undefined") {
+    // Not on browser, nothing to persist here
+    return children;
+  }
+  return (
+    <PersistGate loading={null} persistor={persistor}>
+      {children}
+    </PersistGate>
+  );
+};
+
 const MyApp = ({ Component, pageProps }) => (
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+    <PersistGateSSR loading={null} persistor={persistor}>
       <Component {...pageProps} />
-    </PersistGate>
+    </PersistGateSSR>
   </Provider>
 );
 
