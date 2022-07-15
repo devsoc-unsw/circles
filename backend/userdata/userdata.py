fcfe6acfe67a65d19b7528c2dff5dad183378ea3
imagine-hussain <hussain.nwz000@gmail.com>
2022-07-15 18:57:14 +1000

    start user schema

---
+++ backend/userdata/userdata.py
@@ -0,0 +1,45 @@
+"""
+Stores the data for a user.
+
+Not to be confused with `user.py` which only stores the user data relevant
+to the backend algorithms
+TODO: eventual merge of those two
+"""
+
+from time import time
+from typing import Dict
+
+from algorithms.objects.user import User
+
+class UserData(object):
+    """
+        A complete data class for a user.
+        Combines course data with metadata
+    """
+
+    def __init__(self, uid: str, data=None) -> None:
+        self.uid: str = uid
+        self.time_created: int = int(time())
+        self.user: User = User(data)
+        self.login_instances: Dict[int, int] = {}
+
+        self.add_login_instance()
+
+    def add_login_instance(self, ts: int=None):
+        day = unix_time_to_day(ts)
+        try:
+            self.login_instances[day] += 1
+        except KeyError:
+            self.login_instances[day] = 1
+        return ts
+
+
+def unix_time_to_day(ts: int = None) -> int:
+    """
+        Returns the number of days since unix epoch for the given timestamp
+        - If no timestamp given then, the current time will be used
+    """
+    ts = time() if ts is None else ts
+    SECONDS_PER_DAY: int = 86400
+    return ts // SECONDS_PER_DAY
+
