a2800b0d4667fec6365b1914830cf40d2d241085
xincczn <105340084+xincczn@users.noreply.github.com>
2022-07-22 18:53:27 +1000

    feat(graphical-selector): CF-284/added functionality to graph, hide/ unhide (#625)
    * stuff
    
    * styling issues
    
    Co-authored-by: Xing <xing.he@csesoc.org.au>
    Co-authored-by: Xing He <xincczn@Xings-MacBook-Air.local>
    Co-authored-by: Peedee2002 <64952797+Peedee2002@users.noreply.github.com>

--- frontend/src/pages/GraphicalSelector/GraphicalSelector.jsx
+++ frontend/src/pages/GraphicalSelector/GraphicalSelector.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-import G6 from "@antv/g6";
+import G6, { Algorithm } from "@antv/g6";
 import { Button } from "antd";
 import axios from "axios";
 import PageTemplate from "components/PageTemplate";
@@ -43,6 +43,8 @@ const GraphicalSelector = () => {
       },
       // animate: true,
       defaultNode: GRAPH_STYLE.defaultNode,
+      defaultEdge: GRAPH_STYLE.defaultEdge,
+      nodeStateStyles: GRAPH_STYLE.nodeStateStyles,
     });
 
     setGraph(graphInstance);
@@ -61,6 +63,43 @@ const GraphicalSelector = () => {
       const { _cfg: { id } } = node;
       const [courseData, err] = await axiosRequest("get", `/courses/getCourse/${id}`);
       if (!err) setCourse(courseData);
+
+      // hides/ unhides dependent nodes
+      const { breadthFirstSearch } = Algorithm;
+      if (node.hasState("click")) {
+        graphInstance.clearItemStates(node, "click");
+        breadthFirstSearch(data, id, {
+          enter: ({ current }) => {
+            if (id !== current) {
+              const currentNode = graphInstance.findById(current);
+              // Unhiding node won't unhide other hidden nodes
+              currentNode.getEdges().forEach((e) => e.show());
+              currentNode.show();
+            }
+          },
+        });
+      } else if (node.getOutEdges().length) {
+        graphInstance.setItemState(node, "click", true);
+        breadthFirstSearch(data, id, {
+          enter: ({ current }) => {
+            if (id !== current) {
+              const currentNode = graphInstance.findById(current);
+              currentNode.getEdges().forEach((e) => e.hide());
+              currentNode.hide();
+            }
+          },
+        });
+      }
+    });
+
+    graphInstance.on("node:mouseenter", async (ev) => {
+      const node = ev.item;
+      graphInstance.setItemState(node, "hover", true);
+    });
+
+    graphInstance.on("node:mouseleave", async (ev) => {
+      const node = ev.item;
+      graphInstance.clearItemStates(node, "hover");
     });
   };
 
@@ -81,7 +120,7 @@ const GraphicalSelector = () => {
     const edges = children
       .flatMap((courseObject) => courseObject.courses
         .filter((c) => courseList.includes(c))
-        .map((c) => ({ source: courseObject.original, target: c })));
+        .map((c) => ({ source: c, target: courseObject.original })));
     if (courseList.length !== 0 && edges.length !== 0) initialiseGraph(courseList, edges);
     setLoading(false);
   };
