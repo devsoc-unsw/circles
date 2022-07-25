ea6595f2dd9b498b3183e9c3fae0fa0098a13d8e
Gabriel T <65492346+gtangelo@users.noreply.github.com>
2022-07-26 09:16:29 +1000

    feat: Added error boundary component (#631)

---
+++ frontend/src/components/ErrorBoundary/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 2rem;
+  max-width: 1500px;
+  margin: auto;
+`;
+
+const TextBody = styled.div`
+  font-size: 16px;
+  padding-bottom: 10px;
+`;
+
+export default { Container, TextBody };
