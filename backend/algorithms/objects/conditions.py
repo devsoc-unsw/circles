24ceb11b9a6dd3b891bc3e3ac6b46f7a5d6185c2
sixohtoo <67740921+sixohtoo@users.noreply.github.com>
2022-08-01 14:18:52 +1000

    feat(backend): JSONified version of conditions objects for prerequisite tree graph

--- backend/algorithms/objects/conditions.py
+++ backend/algorithms/objects/conditions.py
@@ -4,7 +4,7 @@ Contains the Conditions classes
 
 import json
 from abc import ABC, abstractmethod
-from typing import Optional, Tuple
+from typing import Optional, Tuple, TypedDict
 
 from algorithms.objects.categories import Category, AnyCategory, ClassCategory, CompositeCategory
 from algorithms.objects.user import User
@@ -19,6 +19,13 @@ with open(CACHED_CONDITIONS_TOKENS_PATH, "r", encoding="utf8") as f:
 CACHED_PROGRAM_MAPPINGS_FILE = "./algorithms/cache/programMappings.json"
 with open(CACHED_PROGRAM_MAPPINGS_FILE, "r", encoding="utf8") as f:
     CACHED_PROGRAM_MAPPINGS = json.load(f)
+
+
+
+class CompositeJsonData(TypedDict):
+    id: str
+    logic: str
+    children: list[dict]
 
 
 class Condition(ABC):
@@ -77,7 +84,9 @@ class CourseCondition(Condition):
         return user.has_taken_course(self.course), []
 
     def __str__(self) -> str:
-        return f"CourseCondition({self.course})"
+        return json.dumps({
+            'id': self.course
+        })
 
 
 class CoreqCoursesCondition(Condition):
@@ -126,9 +135,12 @@ class CoreqCoursesCondition(Condition):
             return False
         return any(c in course.keys() for c in self.courses)
 
-
     def __str__(self) -> str:
-        return f"CoreqCoursesCondition(courses={self.courses}, logic={self.logic})"
+        logic = "and" if self.logic == Logic.AND else "or"
+        return json.dumps({
+            'logic': logic,
+            'children': [{'id': course} for course in self.courses],
+        })
 
 class UOCCondition(Condition):
     """ UOC conditions such as '24UOC in COMP' """
@@ -157,7 +169,10 @@ class UOCCondition(Condition):
         return user.uoc(self.category) >= self.uoc, []
 
     def __str__(self) -> str:
-        return f"{self.uoc}UOC in {self.category}"
+        return json.dumps({
+            'UOC': self.uoc,
+            'category': str(self.category)
+        })
 
 
 class WAMCondition(Condition):
@@ -196,7 +211,10 @@ class WAMCondition(Condition):
         return f"Requires {self.wam} WAM in {self.category}. Your WAM in {self.category} is currently {applicable_wam:.3f}"
 
     def __str__(self) -> str:
-        return f"{self.wam}WAM in {self.category}"
+        return json.dumps({
+            'wam': self.wam,
+            'category': str(self.category)
+        })
 
 
 class GradeCondition(Condition):
@@ -252,7 +270,10 @@ class GradeCondition(Condition):
         return f"Requires {self.grade} mark in {self.category}. Your mark has not been recorded"
 
     def __str__(self) -> str:
-        return f"{self.grade}GRADE in {self.category}"
+        return json.dumps({
+            'grade': self.grade,
+            'category': str(self.category)
+        })
 
 
 class ProgramCondition(Condition):
@@ -268,7 +289,9 @@ class ProgramCondition(Condition):
         return user.in_program(self.program), []
 
     def __str__(self) -> str:
-        return f"Program {self.program}"
+        return json.dumps({
+            'program': self.program,
+        })
 
 
 class ProgramTypeCondition(Condition):
@@ -289,7 +312,9 @@ class ProgramTypeCondition(Condition):
         return user.program in CACHED_PROGRAM_MAPPINGS[self.programType], []
 
     def __str__(self) -> str:
-        return f"ProgramTypeCondition: {self.programType}"
+        return json.dumps({
+            'programType': self.programType,
+        })
 
 
 class SpecialisationCondition(Condition):
@@ -305,7 +330,9 @@ class SpecialisationCondition(Condition):
         return False
 
     def __str__(self) -> str:
-        return f"SpecialisationCondition: {self.specialisation}"
+        return json.dumps({
+            'specialisation': self.specialisation,
+        })
 
 
 class CourseExclusionCondition(Condition):
@@ -322,7 +349,9 @@ class CourseExclusionCondition(Condition):
         return False
 
     def __str__(self) -> str:
-        return f"Exclusion: {self.exclusion}"
+        return json.dumps({
+            'exclusion': self.exclusion,
+        })
 
 
 class ProgramExclusionCondition(Condition):
@@ -341,7 +370,9 @@ class ProgramExclusionCondition(Condition):
         return False
 
     def __str__(self) -> str:
-        return f"ProgramExclusionCondition: {self.exclusion}"
+        return json.dumps({
+            'programExclusion': self.exclusion,
+        })
 
 
 class CompositeCondition(Condition):
@@ -383,6 +414,24 @@ class CompositeCondition(Condition):
             return False
         return any(condition.beneficial(user, course) for condition in self.conditions)
 
-    def __str__(self) -> str:
-        logic_op = "&&" if self.logic == Logic.AND else "||"
-        return f"({f' {logic_op} '.join(str(cond) for cond in self.conditions)})"
+    def __str__(self, id='start') -> str:
+        data: CompositeJsonData = {
+            'logic': "and" if self.logic == Logic.AND else "or",
+            'id': id,
+            'children': []
+        }
+
+        for index, cond in enumerate(self.conditions):
+            if id == 'start':
+                child_index = 'root'
+            elif id == 'root':
+                child_index = f'subtree.{index}'
+            else:
+                child_index = f'{id}.{index}'
+            if isinstance(cond, CompositeCondition):
+                data['children'].append(json.loads(cond.__str__(child_index)))
+            else:
+                data['children'].append(json.loads(str(cond)))
+        if id == 'start':
+            return json.dumps(data['children'][0])
+        return json.dumps(data)
