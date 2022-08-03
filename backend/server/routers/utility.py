9a7703d05c35edea971158d0130f45a948dba5f4
imagine-hussain <hussain.nwz000@gmail.com>
2022-08-04 09:05:07 +1000

    comply 'any' with mypy

--- backend/server/routers/utility.py
+++ backend/server/routers/utility.py
@@ -4,10 +4,10 @@ specifically in any one function
 """
 
 
-from typing import Any
+from typing import Any, Callable, List
 
 
-def map_suppressed_errors(func: callable, errors_log: list[Any], *args, **kwargs) -> Any:
+def map_suppressed_errors(func: Callable, errors_log: List[Any], *args, **kwargs) -> Any:
     """
     Map a function to a list of arguments, and return the result of the function
     if no error is raised. If an error is raised, log the error and return None.
