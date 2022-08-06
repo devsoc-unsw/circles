f08aaa3fbb4718a0be05ff232cb717e4093aff1b
Gabriel Ting <65492346+gtangelo@users.noreply.github.com>
2022-08-07 00:31:38 +1000

    reverted back to original be api url

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // fallback to localhost port 8000 if env variable is not specified
-axios.defaults.baseURL = "https://circlesapi.csesoc.app";
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_API_BASE_URL || "http://localhost:8000/";
 
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
