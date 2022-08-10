e2d171de04524c8f639ae17877f88b65f7da9cc3
Peter Derias <peter_derias@hotmail.com>
2022-08-11 02:12:53 +1000

    fix indentation

--- backend/server/tests/courses/test_get_all_unlocked.py
+++ backend/server/tests/courses/test_get_all_unlocked.py
@@ -11,7 +11,7 @@ with open(PATH, encoding="utf8") as f:
 def test_fix_wam_only_unlock_given_course():
     x = requests.post(
         "http://127.0.0.1:8000/courses/getAllUnlocked", json=USERS["user5"]
-    )    
+    )
     assert x.status_code != 500
     assert x.json()["courses_state"]["COMP1521"]["unlocked"] is True
     assert x.json()["courses_state"]["COMP1521"]["is_accurate"] is True
