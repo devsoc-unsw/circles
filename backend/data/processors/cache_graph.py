77302ff1adfb3eb67a86a6364dbce46422c0279f
imagine-hussain <hussain.nwz000@gmail.com>
2022-08-13 00:43:04 +1000

    stub global conditions

--- backend/data/processors/cache_graph.py
+++ backend/data/processors/cache_graph.py
@@ -15,6 +15,8 @@ from data.processors.load_conditions import construct_conditions_objects
 from data.utility.data_helpers import write_data
 import json
 
+CONDITONS = None
+
 def cache_graph():
     graph = construct_full_graph()
     write_data(graph, GRAPH_CACHE_FILE)
@@ -24,20 +26,43 @@ def cache_graph():
 def construct_full_graph():
     conditions_objects = construct_conditions_objects()
     graph = {}
+    fails = []
     for k, v in conditions_objects.items():
-        print("k: ", k)
-        with suppress(AttributeError):
-            try:
-                v = json.loads(str(v))
-                with suppress(KeyError):
-                    print(v["children"])
-            except json.JSONDecodeError:
-                print("ERRORRRRR\n"*5)
-                print(v)
-                print("\n"*5)
-    print(graph)
+        try:
+            pass
+            # this is such a dumb hack; TODO: make actual __dict__ method
+            real_v = json.loads(str(v))
 
+            children = real_v.get("children", [])
+            graph[k] = children
+            # print(
+            #         "k: ", k,
+            #         json.loads(str(v))
+            #     )
+        except json.JSONDecodeError as e:
+            if v is None:
+                fails.append((v, e))
+    # for f in fails:
+    #     print(f)
+    # print("len", len(fails))
+    return graph
+
+def initialisee_conditions():
+    global CONDITIONS
+    CONDITIONS = construct_conditions_objects()
 
 if __name__ == "__main__":
+    initialisee_conditions()
     cache_graph()
 
+def get_paths_to():
+    course_condition = CONDITIONS.get(course)
+    if not course_condition:
+        raise HTTPException(400, f"no course by name {course}")
+    return {
+        "original" : course,
+        "courses" : [
+            coursename for coursename, _ in CONDITIONS.items()
+            if course_condition.is_path_to(coursename)
+        ]
+    }
