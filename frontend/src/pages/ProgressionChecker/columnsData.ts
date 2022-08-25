06c1735b9516299b698ed3c40cb9f2658716eb19
Gabriel T <65492346+gtangelo@users.noreply.github.com>
2022-08-25 18:06:14 +1000

    feat(progression-checker): improvements for table view (#698)
    * flesh out table view
    
    * update
    
    * removed in dev flag

---
+++ frontend/src/pages/ProgressionChecker/columnsData.ts
@@ -0,0 +1,29 @@
+const columnsData = [
+  {
+    title: 'Course Code',
+    dataIndex: 'courseCode',
+    key: 'courseCode',
+    width: '15%',
+  },
+  {
+    title: 'Course Name',
+    dataIndex: 'title',
+    key: 'title',
+    width: '45%',
+    ellipsis: true,
+  },
+  {
+    title: 'UOC Count',
+    dataIndex: 'UOC',
+    key: 'UOC',
+    width: '10%',
+  },
+  {
+    title: 'Term Planned',
+    dataIndex: 'plannedFor',
+    key: 'plannedFor',
+    width: '15%',
+  },
+];
+
+export default columnsData;
