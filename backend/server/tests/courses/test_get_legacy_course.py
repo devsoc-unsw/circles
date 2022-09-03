837aa0ee67f2b79976fe285f69e4bdd5024f28e3
imagine-hussain <hussain.nwz000@gmail.com>
2022-09-03 20:33:30 +1000

    refactor test

--- backend/server/tests/courses/test_get_legacy_course.py
+++ backend/server/tests/courses/test_get_legacy_course.py
@@ -1,4 +1,5 @@
 import json
+from typing import Optional
 import requests
 
 from server.routers.model import CourseDetails
@@ -17,18 +18,24 @@ def get_legacy_course_wrapper(year: int, course: str):
         f"http://127.0.0.1:8000/courses/getLegacyCourse/{year}/{course}"
     )
 
-def compare_legacy_course(output: CourseDetails, expected: CourseDetails, compare_desc: bool=False):
+def compare_course_details(output: CourseDetails, expected: CourseDetails, compare_desc: bool=False) -> Optional[str]:
+    """
+    Used to compare two course-detail objects.
+    Returns the key (str) that had a differing value, otherwise None.
+    By default, this skips the description value. Add the `compare_desc` flag
+    to toggle this behaviour.
+    """
     assert set(output.keys()) == set(expected.keys())
     if compare_desc:
         if output["description"] != expected["description"]:
-            return False
+            return "description"
 
     del output["description"]
     del expected["description"]
     for key, val in expected.items():
         if output[key] != val:
-            return False
-    return True
+            return key
+    return None
     
 
 def test_legacy_comp1511():
@@ -38,7 +45,5 @@ def test_legacy_comp1511():
     res_2019 = get_legacy_course_wrapper(2019, "COMP1511")
     assert res_2019.status_code == 200
     data_2019 = res_2019.json()
-    assert compare_legacy_course(data_2019, TEST_OBJECTS["comp1511_2019"])
-
-
+    assert not compare_course_details(data_2019, TEST_OBJECTS["comp1511_2019"])
 
