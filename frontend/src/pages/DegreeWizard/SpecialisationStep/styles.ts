248ce64a9e58fe793cfc97e74c9ecf13e4b1e503
Gabriel <65492346+gtangelo@users.noreply.github.com>
2022-09-07 00:40:28 +1000

    dark mode stylings

--- frontend/src/pages/DegreeWizard/SpecialisationStep/styles.ts
+++ frontend/src/pages/DegreeWizard/SpecialisationStep/styles.ts
@@ -5,16 +5,25 @@ import styled, { css } from 'styled-components';
 const Menu = styled(antdMenu)`
   border-right: none;
   background-color: inherit;
+  color: ${({ theme }) => theme.text};
+
+  .ant-menu-submenu-title {
+    background-color: inherit;
+  }
+
+  ${({ theme }) => theme.courseSidebar && css`
+    .ant-menu-sub {
+      background-color: ${theme.courseSidebar.menuSubColor} !important;
+    }
+  `}
+
+  .ant-menu-title-content, .ant-menu-item-group-title, .ant-menu-submenu-arrow {
+    color: ${({ theme }) => theme.text};
+  }
 
   .ant-menu-submenu {
     border: 1px solid #a86fed;
   }
-
-  ${({ theme }) => theme.specialsationStep && css`
-    .ant-menu-submenu {
-      background: ${theme.specialsationStep.background};
-    }
-  `};
 
 `;
 
