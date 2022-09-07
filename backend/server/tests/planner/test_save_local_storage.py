b8ffd27ca5219d8464185a7354f01c6117b7bb80
vuong <vuong@Phuongs-MacBook-Air.local>
2022-09-07 14:49:00 +0700

    database tests with auth errors

--- backend/server/tests/planner/test_save_local_storage.py
+++ backend/server/tests/planner/test_save_local_storage.py
@@ -1,7 +1,6 @@
 import json
 import requests
-
-
+from server.database import usersDB
 PATH = "server/example_input/example_local_storage_data.json"
 
 with open(PATH, encoding="utf8") as f:
@@ -18,3 +17,10 @@ def test_saveLocalStorage_simple():
     x = requests.post(
         'http://127.0.0.1:8000/planner/saveLocalStorage', json=DATA["simple_year"])
     assert x.status_code == 200
+    data = usersDB['tokens'].find_one({'token': ''})
+    assert data is not None
+    data = usersDB['users'].find_one({'_id': data['objectId']})
+    print(data)
+    assert data['degree']['programCode'] == "COMP2521"
+
+
