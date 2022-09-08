b561cecd513ce6b336f221f242f2f87cfe95200d
vuong <vuong@Phuongs-MacBook-Air.local>
2022-09-08 11:23:09 +0700

    test data for save_local_storage

--- backend/server/tests/planner/test_save_local_storage.py
+++ backend/server/tests/planner/test_save_local_storage.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from server.database import usersDB
+from server.config import DUMMY_TOKEN
 PATH = "server/example_input/example_local_storage_data.json"
 
 with open(PATH, encoding="utf8") as f:
@@ -17,10 +17,10 @@ def test_saveLocalStorage_simple():
     x = requests.post(
         'http://127.0.0.1:8000/planner/saveLocalStorage', json=DATA["simple_year"])
     assert x.status_code == 200
-    data = usersDB['tokens'].find_one({'token': ''})
-    assert data is not None
-    data = usersDB['users'].find_one({'_id': data['objectId']})
-    print(data)
-    assert data['degree']['programCode'] == "COMP2521"
+
+    data = requests.get(f'http://127.0.0.1:8000/user/data/{DUMMY_TOKEN}')
+    assert data.status_code == 200
+    data = data.json()
+    assert "COMP6447" in data['planner']['unplanned']
 
 
