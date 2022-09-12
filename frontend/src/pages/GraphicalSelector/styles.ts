afa401a0eddfbc562b08b77129d9920b6f34ad32
Kellie Yau <97064025+kellieyau@users.noreply.github.com>
2022-09-12 15:56:10 +1000

    added align-items and gap

--- frontend/src/pages/GraphicalSelector/styles.ts
+++ frontend/src/pages/GraphicalSelector/styles.ts
@@ -33,8 +33,11 @@ const SearchBarWrapper = styled.div`
 `;
 
 const ToolsWrapper = styled.div`
+  display: flex;
   align-items: center;
-  gap: 10px;
+  gap:10px;
+  margin-bottom: 10px;
+  margin-left: 10px;
 `;
 export default {
   Wrapper,
