37728b66928d76ab52df00f2b0579e402a06286b
Peter Derias <peter_derias@hotmail.com>
2022-09-25 15:14:07 +1000

    small fix in planner to avoid trailing

--- backend/server/routers/planner.py
+++ backend/server/routers/planner.py
@@ -9,16 +9,16 @@ from server.routers.model import ValidCoursesState, PlannerData, ValidPlannerDat
 
 def fix_planner_data(plannerData: PlannerData) -> ValidPlannerData:
     """ fixes the planner data to add missing UOC info """
-    plan: list[list[dict[str, Tuple[int, int | None]]]] = [[{}]]
+    plan: list[list[dict[str, Tuple[int, int | None]]]] = []
     for year_index, year in enumerate(plannerData.plan):
+        plan.append([])
         for term_index, term in enumerate(year):
+            plan[year_index].append({})
             for courseName, course in term.items():
                 if not isinstance(course, list):
                     plan[year_index][term_index][courseName] = (get_course(courseName)["UOC"], course)
                 elif course[0] is not None:
                     plan[year_index][term_index][courseName] = (course[0], course[1])
-            plan[year_index].append({})
-        plan.append([])
     return ValidPlannerData(
         program=plannerData.program,
         specialisations=plannerData.specialisations,
