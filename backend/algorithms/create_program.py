13c10a03b11f4ce65a55ed273948d4db3665d5f3
imagine-hussain <hussain.nwz000@gmail.com>
2022-09-27 19:26:46 +1000

    going to fix mipy

--- backend/algorithms/create_program.py
+++ backend/algorithms/create_program.py
@@ -10,19 +10,30 @@ from data.processors.program_conditions_pre_processing import PROGRAMS_PROCESSED
 from data.processors.program_conditions_tokenising import FINAL_TOKENS_PATH
 
 
-
-
 def create_all_program_conditions():
     """
     """
     programs_list: List[str] = read_data(PROGRAMS_PROCESSED_PATH)
+    tokens_data: Dict[str, List[Dict[str, List[str]]]] = read_data(FINAL_TOKENS_PATH)
     for program in programs_list:
+        conditons = [
+            create_program_condition(tokens)
+            for tokens in tokens_data.get(program, [])
+        ]
+        print(conditons)
+
         create_program_condition(program)
 
     raise NotImplementedError
 
 # TODO: Make this use a real condition
-def create_program_condition() -> Condition:
+def create_program_condition(tokens: Dict[str, List[str]]) -> Condition:
+    """
+    Creates a condition from the tokens.
+    Currently supports:
+        - Maturity Conditions
+    """
+    print(tokens)
     raise NotImplementedError
 
 if __name__ == "__main__":
