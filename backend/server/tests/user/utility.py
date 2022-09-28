e2dbb6ba30c428612b3853beb27d1222499f32e3
Peter Derias <peter_derias@hotmail.com>
2022-09-28 12:47:16 +1000

    fix for docker

--- backend/server/tests/user/utility.py
+++ backend/server/tests/user/utility.py
@@ -1,14 +1,8 @@
 import os
-import docker # type: ignore
 from dotenv import load_dotenv
 load_dotenv("../env/backend.env")
 
-docker_client = docker.from_env()
-# check if you are running in docker
-try:
-    docker_client.containers.get("backend")
-except docker.errors.NotFound:
-    os.environ["MONGODB_SERVICE_HOSTNAME"] = "localhost"
+os.environ["MONGODB_SERVICE_HOSTNAME"] = "localhost"
 
 from server.database import usersDB, create_dynamic_db
 
