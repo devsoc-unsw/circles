a1868847f93eeb6872f342efff4769ce6fd3241a
Kellie Yau <97064025+kellieyau@users.noreply.github.com>
2022-10-07 10:43:41 +1100

    uncentered

--- frontend/src/pages/GraphicalSelector/GraphicalSelector.tsx
+++ frontend/src/pages/GraphicalSelector/GraphicalSelector.tsx
@@ -259,7 +259,7 @@ const GraphicalSelector = () => {
         </S.GraphPlaygroundWrapper>
         {sidebar && (
           <S.SidebarWrapper>
-            <Tabs centered items={items} />
+            <Tabs items={items} />
             {course ? <div>{course.code} - {course.title}</div> : 'No course selected'}
           </S.SidebarWrapper>
         )}
