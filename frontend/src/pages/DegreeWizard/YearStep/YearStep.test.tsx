62458e9992d6e77297ef35eaf406494923df08f9
Gabriel Ting <65492346+gtangelo@users.noreply.github.com>
2022-10-08 18:43:09 +1100

    add redux tests for YearStep component

--- frontend/src/pages/DegreeWizard/YearStep/YearStep.test.tsx
+++ frontend/src/pages/DegreeWizard/YearStep/YearStep.test.tsx
@@ -1,29 +1,49 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithProviders } from 'test/testUtil';
 import { vi } from 'vitest';
+import * as hooks from 'hooks';
+import Steps from '../common/steps';
 import YearStep from './YearStep';
 
-const incrementStep = vi.fn();
-
-vi.mock('../../../components/Datepicker.tsx', () => {
-  const Datepicker = () => <div data-testid="ant-rangepicker" />;
-  return {
-    __esModule: true,
-    default: {
-      RangePicker: Datepicker,
-    },
-  };
-});
+const incrementStepMock = vi.fn();
 
 describe('YearStep', () => {
+  const useSelectorMock = vi.spyOn(hooks, 'useAppSelector');
+  const useDispatchMock = vi.spyOn(hooks, 'useAppDispatch');
+
   beforeEach(() => {
-    vi.resetAllMocks();
+    useSelectorMock.mockClear();
+    useDispatchMock.mockClear();
+    vi.clearAllMocks();
   });
 
   it('should render', async () => {
-    renderWithProviders(<YearStep incrementStep={incrementStep} />);
+    renderWithProviders(<YearStep incrementStep={incrementStepMock} />);
     expect(screen.getByText('What years do you start and finish?')).toBeInTheDocument();
-    expect(await screen.findByTestId('ant-rangepicker')).toBeInTheDocument();
+    await waitFor(() => expect(screen.getByTestId('antd-rangepicker')).toBeInTheDocument());
+  });
+
+  it('should dispatch correct props and call incrementStep after selecting years', async () => {
+    const dummyDispatch = vi.fn();
+    useDispatchMock.mockReturnValue(dummyDispatch);
+
+    renderWithProviders(<YearStep incrementStep={incrementStepMock} />);
+    await waitFor(() => expect(screen.getByTestId('antd-rangepicker')).toBeInTheDocument());
+    await userEvent.click(screen.getByTestId('antd-rangepicker'));
+    await userEvent.click(screen.getByText('2020'));
+    await userEvent.click(screen.getByText('2022'));
+    expect(dummyDispatch.mock.calls).toEqual([
+      [{
+        payload: 3,
+        type: 'planner/updateDegreeLength',
+      }],
+      [{
+        payload: 2020,
+        type: 'planner/updateStartYear',
+      }],
+    ]);
+    expect(incrementStepMock).toHaveBeenCalledWith(Steps.DEGREE);
   });
 });
