dd9d63dd0231551a4cfcc73c4b22b21ecb42d2c1
Gabriel Ting <65492346+gtangelo@users.noreply.github.com>
2022-10-09 17:56:08 +1100

    steamline tests

--- frontend/src/reducers/courseTabsSlice.test.ts
+++ frontend/src/reducers/courseTabsSlice.test.ts
@@ -1,5 +1,4 @@
-import { setupStore } from 'config/store';
-import {
+import reducer, {
   addTab,
   initialCourseTabsState,
   removeTab,
@@ -11,34 +10,25 @@ import {
 describe('courseTabsSlice reducer tests', () => {
   describe('addTab reducer', () => {
     it('adds a new tab', () => {
-      const store = setupStore();
-      store.dispatch(addTab('COMP1511'));
-      expect(store.getState().courseTabs).toEqual({
+      expect(reducer(undefined, addTab('COMP1511'))).toEqual({
         tabs: ['COMP1511'],
         active: 0
       });
     });
 
     it('adds a new tab and set it as active', () => {
-      const store = setupStore();
-      store.dispatch(addTab('COMP1511'));
-      store.dispatch(addTab('COMP1521'));
-      store.dispatch(addTab('COMP1531'));
+      const previousState = { tabs: ['COMP1511', 'COMP1521'], active: 0 };
 
-      expect(store.getState().courseTabs).toEqual({
+      expect(reducer(previousState, addTab('COMP1531'))).toEqual({
         tabs: ['COMP1511', 'COMP1521', 'COMP1531'],
         active: 2
       });
     });
 
     it('set the active tab if the added tab already exists', () => {
-      const store = setupStore();
-      store.dispatch(addTab('COMP1511'));
-      store.dispatch(addTab('COMP1521'));
-      store.dispatch(addTab('COMP1531'));
-      store.dispatch(addTab('COMP1521'));
+      const previousState = { tabs: ['COMP1511', 'COMP1521', 'COMP1531'], active: 2 };
 
-      expect(store.getState().courseTabs).toEqual({
+      expect(reducer(previousState, addTab('COMP1521'))).toEqual({
         tabs: ['COMP1511', 'COMP1521', 'COMP1531'],
         active: 1
       });
@@ -47,71 +37,54 @@ describe('courseTabsSlice reducer tests', () => {
 
   describe('removeTab reducer', () => {
     it('remains in the initial state when there are no tabs to be removed', () => {
-      const store = setupStore();
-      store.dispatch(removeTab(0));
-      expect(store.getState().courseTabs).toEqual(initialCourseTabsState);
+      expect(reducer(undefined, removeTab(0))).toEqual(initialCourseTabsState);
     });
 
     it('does not remove any tabs if the index is out of scope', () => {
-      const store = setupStore({ courseTabs: { tabs: ['COMP1511'], active: 0 } });
-      store.dispatch(removeTab(1));
+      const previousState = { tabs: ['COMP1511'], active: 0 };
 
-      expect(store.getState().courseTabs).toEqual({
+      expect(reducer(previousState, removeTab(1))).toEqual({
         tabs: ['COMP1511'],
         active: 0
       });
     });
 
     it('removes an existing tab', () => {
-      const store = setupStore({ courseTabs: { tabs: ['COMP1511'], active: 0 } });
-      store.dispatch(removeTab(0));
-
-      expect(store.getState().courseTabs).toEqual(initialCourseTabsState);
+      const previousState = { tabs: ['COMP1511'], active: 0 };
+      expect(reducer(previousState, removeTab(0))).toEqual(initialCourseTabsState);
     });
 
     it('removes a non active tab with active index positioned on the correct tab', () => {
-      const store = setupStore({
-        courseTabs: { tabs: ['COMP1511', 'COMP1521', 'COMP1531'], active: 2 }
-      });
-      store.dispatch(removeTab(0));
+      const previousState = { tabs: ['COMP1511', 'COMP1521', 'COMP1531'], active: 2 };
 
-      expect(store.getState().courseTabs).toEqual({
+      expect(reducer(previousState, removeTab(0))).toEqual({
         tabs: ['COMP1521', 'COMP1531'],
         active: 1
       });
     });
 
     it('removes the first tab (active tab) and set correct active index', () => {
-      const store = setupStore({
-        courseTabs: { tabs: ['COMP1511', 'COMP1521', 'COMP1531'], active: 0 }
-      });
-      store.dispatch(removeTab(0));
+      const previousState = { tabs: ['COMP1511', 'COMP1521', 'COMP1531'], active: 0 };
 
-      expect(store.getState().courseTabs).toEqual({
+      expect(reducer(previousState, removeTab(0))).toEqual({
         tabs: ['COMP1521', 'COMP1531'],
         active: 0
       });
     });
 
     it('removes the middle tab (active tab) and set correct active index', () => {
-      const store = setupStore({
-        courseTabs: { tabs: ['COMP1511', 'COMP1521', 'COMP1531'], active: 1 }
-      });
-      store.dispatch(removeTab(1));
+      const previousState = { tabs: ['COMP1511', 'COMP1521', 'COMP1531'], active: 1 };
 
-      expect(store.getState().courseTabs).toEqual({
+      expect(reducer(previousState, removeTab(1))).toEqual({
         tabs: ['COMP1511', 'COMP1531'],
         active: 1
       });
     });
 
     it('removes the last tab (active tab) and set correct active index', () => {
-      const store = setupStore({
-        courseTabs: { tabs: ['COMP1511', 'COMP1521', 'COMP1531'], active: 2 }
-      });
-      store.dispatch(removeTab(2));
+      const previousState = { tabs: ['COMP1511', 'COMP1521', 'COMP1531'], active: 2 };
 
-      expect(store.getState().courseTabs).toEqual({
+      expect(reducer(previousState, removeTab(2))).toEqual({
         tabs: ['COMP1511', 'COMP1521'],
         active: 1
       });
@@ -119,35 +92,26 @@ describe('courseTabsSlice reducer tests', () => {
   });
 
   it('tests setActiveTab reducer', () => {
-    const store = setupStore({
-      courseTabs: { tabs: ['COMP1511', 'COMP1521', 'COMP1531'], active: 2 }
-    });
-    store.dispatch(setActiveTab(1));
+    const previousState = { tabs: ['COMP1511', 'COMP1521', 'COMP1531'], active: 2 };
 
-    expect(store.getState().courseTabs).toEqual({
+    expect(reducer(previousState, setActiveTab(1))).toEqual({
       tabs: ['COMP1511', 'COMP1521', 'COMP1531'],
       active: 1
     });
   });
 
   it('tests reorderTabs reducer', () => {
-    const store = setupStore({
-      courseTabs: { tabs: ['COMP1511', 'COMP1521', 'COMP1531'], active: 2 }
-    });
-    store.dispatch(reorderTabs(['COMP1531', 'COMP1511', 'COMP1521']));
+    const previousState = { tabs: ['COMP1511', 'COMP1521', 'COMP1531'], active: 2 };
 
-    expect(store.getState().courseTabs).toEqual({
+    expect(reducer(previousState, reorderTabs(['COMP1531', 'COMP1511', 'COMP1521']))).toEqual({
       tabs: ['COMP1531', 'COMP1511', 'COMP1521'],
       active: 2
     });
   });
 
   it('tests resetTabs reducer', () => {
-    const store = setupStore({
-      courseTabs: { tabs: ['COMP1511', 'COMP1521', 'COMP1531'], active: 2 }
-    });
-    store.dispatch(resetTabs());
+    const previousState = { tabs: ['COMP1511', 'COMP1521', 'COMP1531'], active: 2 };
 
-    expect(store.getState().courseTabs).toEqual(initialCourseTabsState);
+    expect(reducer(previousState, resetTabs())).toEqual(initialCourseTabsState);
   });
 });
--- frontend/src/reducers/degreeSlice.test.ts
+++ frontend/src/reducers/degreeSlice.test.ts
@@ -1,5 +1,4 @@
-import { setupStore } from 'config/store';
-import {
+import reducer, {
   addSpecialisation,
   initialDegreeState,
   removeSpecialisation,
@@ -10,9 +9,9 @@ import {
 
 describe('degreeSlice reducer tests', () => {
   it('tests setProgram reducer', () => {
-    const store = setupStore();
-    store.dispatch(setProgram({ programCode: '3778', programName: 'Computer Science' }));
-    expect(store.getState().degree).toEqual({
+    expect(
+      reducer(undefined, setProgram({ programCode: '3778', programName: 'Computer Science' }))
+    ).toEqual({
       ...initialDegreeState,
       programCode: '3778',
       programName: 'Computer Science'
@@ -20,55 +19,48 @@ describe('degreeSlice reducer tests', () => {
   });
 
   it('tests addSpecialisation reducer', () => {
-    const store = setupStore();
-    store.dispatch(addSpecialisation('COMPA1'));
-    expect(store.getState().degree).toEqual({
+    expect(reducer(undefined, addSpecialisation('COMPA1'))).toEqual({
       ...initialDegreeState,
       specs: ['COMPA1']
     });
 
-    store.dispatch(addSpecialisation('INFSA2'));
-    expect(store.getState().degree).toEqual({
+    const previousState = { ...initialDegreeState, specs: ['COMPA1'] };
+    expect(reducer(previousState, addSpecialisation('INFSA2'))).toEqual({
       ...initialDegreeState,
       specs: ['COMPA1', 'INFSA2']
     });
   });
 
   it('tests removeSpecialisation reducer', () => {
-    const store = setupStore({ degree: { ...initialDegreeState, specs: ['COMPA1', 'INFSA2'] } });
-    store.dispatch(removeSpecialisation('COMPA1'));
-    expect(store.getState().degree).toEqual({
+    const previousState = { ...initialDegreeState, specs: ['COMPA1', 'INFSA2'] };
+    expect(reducer(previousState, removeSpecialisation('COMPA1'))).toEqual({
       ...initialDegreeState,
       specs: ['INFSA2']
     });
   });
 
   it('tests removeSpecialisation reducer for non existent specialisation', () => {
-    const store = setupStore({ degree: { ...initialDegreeState, specs: ['COMPA1'] } });
-
-    store.dispatch(removeSpecialisation('NOTASPEC'));
-    expect(store.getState().degree).toEqual({
+    const previousState = { ...initialDegreeState, specs: ['COMPA1'] };
+    expect(reducer(previousState, removeSpecialisation('NOTASPEC'))).toEqual({
       ...initialDegreeState,
       specs: ['COMPA1']
     });
   });
 
   it('tests setIsComplete reducer', () => {
-    const store = setupStore();
-    store.dispatch(setIsComplete(true));
-    expect(store.getState().degree.isComplete).toEqual(true);
+    expect(reducer(undefined, setIsComplete(true))).toEqual({
+      ...initialDegreeState,
+      isComplete: true
+    });
   });
 
   it('tests resetDegree reducer', () => {
-    const store = setupStore({
-      degree: {
-        programCode: '3778',
-        programName: 'Computer Science',
-        specs: ['COMPA1'],
-        isComplete: true
-      }
-    });
-    store.dispatch(resetDegree());
-    expect(store.getState().degree).toEqual(initialDegreeState);
+    const previousState = {
+      programCode: '3778',
+      programName: 'Computer Science',
+      specs: ['COMPA1'],
+      isComplete: true
+    };
+    expect(reducer(previousState, resetDegree())).toEqual(initialDegreeState);
   });
 });
