91a95cd5451f493c1b66940cf402ae6508ca219c
Gabriel <65492346+gtangelo@users.noreply.github.com>
2022-10-17 19:09:40 +1100

    clarified course unlocks wording

--- frontend/src/components/CourseDescriptionPanel/CourseInfoDrawers.tsx
+++ frontend/src/components/CourseDescriptionPanel/CourseInfoDrawers.tsx
@@ -18,10 +18,19 @@ type CourseInfoDrawersProps = {
 const CourseInfoDrawers = ({
   course,
   onCourseClick,
-  pathFrom,
+  pathFrom = [],
   unlocked
 }: CourseInfoDrawersProps) => {
-  const planner = useSelector((state: RootState) => state.planner);
+  const { planner, courses } = useSelector((state: RootState) => state);
+
+  const pathFromInPlanner = pathFrom.filter((courseCode) =>
+    Object.keys(planner.courses).includes(courseCode)
+  );
+  const pathFromNotInPlanner = pathFrom.filter(
+    (courseCode) => !Object.keys(planner.courses).includes(courseCode)
+  );
+  const isUnlocked = courses.courses[course.code]?.unlocked;
+  const inPlanner = planner.courses[course.code];
 
   return (
     <div>
@@ -29,41 +38,79 @@ const CourseInfoDrawers = ({
         {/* eslint-disable-next-line react/no-danger */}
         <p dangerouslySetInnerHTML={{ __html: course?.description || 'None' }} />
       </Collapsible>
-      <Collapsible title="Requirements" initiallyCollapsed>
+      <Collapsible title="Requirements">
         {/* eslint-disable-next-line react/no-danger */}
         <p dangerouslySetInnerHTML={{ __html: course?.raw_requirements || 'None' }} />
       </Collapsible>
-      <Collapsible title="Courses you have done to unlock this course" initiallyCollapsed>
-        <p>
-          {pathFrom && pathFrom.length > 0
-            ? pathFrom
-                .filter((courseCode) => Object.keys(planner.courses).includes(courseCode))
-                .map((courseCode) => (
-                  <CourseTag key={courseCode} onCourseClick={onCourseClick} name={courseCode} />
-                ))
-            : 'None'}
-        </p>
+      <Collapsible title="Course Prerequisites">
+        {!!pathFrom.length && !isUnlocked ? (
+          <>
+            <p>
+              These courses in your planner have helped unlocked this course:
+              {!pathFromInPlanner.length && ' None'}
+            </p>
+            {!!pathFromInPlanner.length && (
+              <p>
+                {pathFromInPlanner.map((code) => (
+                  <CourseTag key={code} onCourseClick={onCourseClick} name={code} />
+                ))}
+              </p>
+            )}
+            <p>
+              You may need to complete these courses to unlock this course:
+              {!pathFromNotInPlanner.length && ' None'}
+            </p>
+            {!!pathFromNotInPlanner.length && (
+              <p>
+                {pathFromNotInPlanner.map((code) => (
+                  <CourseTag key={code} onCourseClick={onCourseClick} name={code} />
+                ))}
+              </p>
+            )}
+          </>
+        ) : (
+          <p>
+            {isUnlocked
+              ? 'You have already unlocked this course.'
+              : 'This course does not have any prerequisite that needs to be met.'}
+          </p>
+        )}
       </Collapsible>
-      <Collapsible title="Doing this course will directly unlock these courses" initiallyCollapsed>
-        <p>
-          {unlocked?.direct_unlock && unlocked.direct_unlock.length > 0
-            ? unlocked.direct_unlock.map((courseCode) => (
-                <CourseTag key={courseCode} onCourseClick={onCourseClick} name={courseCode} />
-              ))
-            : 'None'}
-        </p>
-      </Collapsible>
-      <Collapsible
-        title="Doing this course will indirectly unlock these courses"
-        initiallyCollapsed
-      >
-        <p>
-          {unlocked?.indirect_unlock && unlocked.indirect_unlock.length > 0
-            ? unlocked.indirect_unlock.map((code) => (
+      <Collapsible title="Courses unlocked after doing this course">
+        {!!unlocked?.direct_unlock?.length && !inPlanner ? (
+          <>
+            <p>These courses are directly unlocked after completing this course:</p>
+            <p>
+              {unlocked.direct_unlock.map((code) => (
                 <CourseTag key={code} name={code} onCourseClick={onCourseClick} />
-              ))
-            : 'None'}
-        </p>
+              ))}
+            </p>
+          </>
+        ) : (
+          <p>
+            {inPlanner
+              ? "This course has already been added to your planner and hence can't unlock anymore courses."
+              : 'No courses will be unlocked after completing this course.'}
+          </p>
+        )}
+      </Collapsible>
+      <Collapsible title="Courses indirectly unlocked after doing this course" initiallyCollapsed>
+        {!!unlocked?.indirect_unlock?.length && !inPlanner ? (
+          <>
+            <p>These courses are indirectly unlocked after completing this course:</p>
+            <p>
+              {unlocked.indirect_unlock.map((code) => (
+                <CourseTag key={code} name={code} onCourseClick={onCourseClick} />
+              ))}
+            </p>
+          </>
+        ) : (
+          <p>
+            {inPlanner
+              ? "This course has already been added to your planner and hence can't unlock anymore courses."
+              : 'No courses will be indirectly unlocked after completing this course.'}
+          </p>
+        )}
       </Collapsible>
       {inDev && (
         <Collapsible title="Prerequisite Visualisation">
