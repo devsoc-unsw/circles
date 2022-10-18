6d9d59dc890a466234e19bc660440c2830201447
Peter Derias <peter_derias@hotmail.com>
2022-10-18 20:27:09 +1100

    fix style

--- frontend/src/utils/migrateLocalStorageData.ts
+++ frontend/src/utils/migrateLocalStorageData.ts
@@ -4,12 +4,14 @@ import { PlannerSliceState } from 'reducers/plannerSlice';
 import prepareLocalStorageData from './prepareLocalStorageData';
 
 const migrateLocalStorageData = (token: string): void => {
-  const data = JSON.parse(localStorage.getItem('persist:root') ?? '{}') as { [key: string]: string };
+  const data = JSON.parse(localStorage.getItem('persist:root') ?? '{}') as {
+    [key: string]: string;
+  };
   const degree = JSON.parse(data.degree ?? {}) as DegreeSliceState;
   const planner = JSON.parse(data.planner ?? {}) as PlannerSliceState;
   axios.post(
     '/planner/saveLocalStorage/',
-    JSON.stringify(prepareLocalStorageData(planner, degree, token)),
+    JSON.stringify(prepareLocalStorageData(planner, degree, token))
   );
 };
 export default migrateLocalStorageData;
