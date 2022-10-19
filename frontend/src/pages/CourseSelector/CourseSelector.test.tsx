68883463dad0f802abed67874261106643397966
Gabriel <65492346+gtangelo@users.noreply.github.com>
2022-10-20 10:45:20 +1100

    setup stubs for course selector tests

---
+++ frontend/src/pages/CourseSelector/CourseSelector.test.tsx
@@ -0,0 +1,37 @@
+describe('CourseSelector', () => {
+  it('should render', () => {
+    fail();
+  });
+
+  it('should toggle locked courses', () => {
+    fail();
+  });
+
+  it('should show course description when click the course menu item', () => {
+    fail();
+  });
+
+  it('should be able to quick add a course from the course menu', () => {
+    fail();
+  });
+
+  it('should be able to quick remove a course from the course menu', () => {
+    fail();
+  });
+
+  it('should show rerender when user switch tabs', () => {
+    fail();
+  });
+
+  it('should adds the course from the "Add To Planner" button in the course description', () => {
+    fail();
+  });
+
+  it('should render course description based on the active tab', () => {
+    fail();
+  });
+
+  it('should open a course tab when the user searches for a course', () => {
+    fail();
+  });
+});
