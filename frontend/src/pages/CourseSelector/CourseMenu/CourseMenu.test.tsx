f852e0f8818e1271a2f36c0eafca5ab157f0c7d2
Gabriel <65492346+gtangelo@users.noreply.github.com>
2022-10-21 20:33:41 +1100

    init course menu tests

--- frontend/src/pages/CourseSelector/CourseMenu/CourseMenu.test.tsx
+++ frontend/src/pages/CourseSelector/CourseMenu/CourseMenu.test.tsx
@@ -1,13 +1,73 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { renderWithProviders } from 'test/testUtil';
+import { vi } from 'vitest';
+import * as hooks from 'hooks';
+import CourseMenu from './CourseMenu';
+
+const axiosMock = new MockAdapter(axios);
+axiosMock.onPost('/courses/getAllUnlocked/').reply(200, {
+  courses_state: {
+    COMP1511: {
+      is_accurate: true,
+      handbook_note: '',
+      unlocked: true,
+      warnings: []
+    },
+    COMP2521: {
+      is_accurate: true,
+      handbook_note: '',
+      unlocked: false,
+      warnings: []
+    }
+  }
+});
+
+const structure = {
+  'Major - COMPA1': {
+    name: 'Computer Science',
+    content: {
+      'Core Courses': {
+        UOC: 66,
+        courses: {
+          COMP1511: 'Programming Fundamentals'
+        },
+        type: '',
+        notes: ''
+      }
+    }
+  }
+};
+
 describe('CourseMenu', () => {
-  it('should render', () => {
-    fail();
+  const useDispatchMock = vi.spyOn(hooks, 'useAppDispatch');
+
+  beforeEach(() => {
+    useDispatchMock.mockClear();
+    vi.clearAllMocks();
   });
 
-  it('should show loading skeleton when still fetching course menu data', () => {
-    fail();
+  it('should render', async () => {
+    renderWithProviders(<CourseMenu structure={structure} />);
+    expect(await screen.findByText('Major - COMPA1 - Computer Science')).toBeInTheDocument();
+    expect(screen.getByText('Core Courses')).toBeInTheDocument();
+    expect(screen.getByText('0 / 66')).toBeInTheDocument();
+    expect(screen.getByText('COMP1511: Programming Fundamentals')).toBeInTheDocument();
   });
 
-  it('should dispatch addTab when menu item is clicked', () => {
-    fail();
+  it('should dispatch addTab when menu item is clicked', async () => {
+    const dummyDispatch = vi.fn();
+    useDispatchMock.mockReturnValue(dummyDispatch);
+
+    renderWithProviders(<CourseMenu structure={structure} />);
+    expect(await screen.findByText('Major - COMPA1 - Computer Science'));
+    await userEvent.click(await screen.findByText('COMP1511: Programming Fundamentals'));
+    expect(dummyDispatch).toBeCalledWith({
+      payload: 'COMP1511',
+      type: 'courseTabs/addTab'
+    });
   });
 });
