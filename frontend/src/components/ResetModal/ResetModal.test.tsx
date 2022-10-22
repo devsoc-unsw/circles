8d5165227a8e59cdd03030bcd631cb493e960cf8
ollibowers <80164276+ollibowers@users.noreply.github.com>
2022-10-22 12:37:07 +0000

    fix: oop a broken test im dumb

--- frontend/src/components/ResetModal/ResetModal.test.tsx
+++ frontend/src/components/ResetModal/ResetModal.test.tsx
@@ -31,7 +31,7 @@ describe('ResetModal', () => {
   });
 
   it('should render', () => {
-    renderWithProviders(<ResetModal open />);
+    renderWithProviders(<ResetModal open={false} />);
     expect(screen.queryByText('Reset Planner?')).not.toBeInTheDocument();
   });
 
