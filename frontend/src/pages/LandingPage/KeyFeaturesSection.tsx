4c6f19b47b799e990ed0e2a93f6803fb6325de46
Kellie Yau <97064025+kellieyau@users.noreply.github.com>
2022-11-19 17:57:19 +1100

    umm purple blobs dont like being mobile responsive

--- frontend/src/pages/LandingPage/KeyFeaturesSection.tsx
+++ frontend/src/pages/LandingPage/KeyFeaturesSection.tsx
@@ -9,34 +9,6 @@ const KeyFeaturesSection = () => {
     <div>
       <div>
         <svg
-          width="60%"
-          height="90%"
-          viewBox="0 0 1386 546"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          style={{ position: 'absolute' }}
-          transform="translate(300 50)"
-        >
-          <path
-            d="M367.575 56.6426C-95.7607 17.8601 -48.9425 377.275 117.508 498.025C375.556 685.224 607.177 254.691 1033.21 391.582C1459.25 528.473 1466.06 3.24546e-05 1230.8 0C995.552 -3.24546e-05 819.324 94.4553 367.575 56.6426Z"
-            fill="#EFDBFE"
-          />
-        </svg>
-        <svg
-          width="60%"
-          height="90%"
-          viewBox="0 0 1462 546"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          style={{ position: 'absolute' }}
-          transform="translate(180 0)"
-        >
-          <path
-            d="M387.493 57.0908C-100.95 18.3438 -51.5946 377.43 123.875 498.069C395.908 685.097 640.079 254.958 1089.2 391.724C1538.33 528.49 1545.5 0.500032 1297.5 0.5C1049.5 0.499968 863.722 94.8689 387.493 57.0908Z"
-            fill="#E5C2FF"
-          />
-        </svg>
-        {/* <svg
           width="850"
           height="546"
           viewBox="0 0 1386 546"
@@ -63,7 +35,7 @@ const KeyFeaturesSection = () => {
             d="M387.493 57.0908C-100.95 18.3438 -51.5946 377.43 123.875 498.069C395.908 685.097 640.079 254.958 1089.2 391.724C1538.33 528.49 1545.5 0.500032 1297.5 0.5C1049.5 0.499968 863.722 94.8689 387.493 57.0908Z"
             fill="#E5C2FF"
           />
-        </svg> */}
+        </svg>
         <S.LandingPageTitle>Introducing our features</S.LandingPageTitle>
         <S.CardContainer>
           <S.Card>
