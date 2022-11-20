49fd0f56e87a51f1db6ca2b43b84f3e77c37778a
Kellie Yau <97064025+kellieyau@users.noreply.github.com>
2022-11-20 21:58:52 +1100

    footer almost done, just need to make it mobile repsosnive with background colour of footer

--- frontend/src/pages/LandingPage/Footer.tsx
+++ frontend/src/pages/LandingPage/Footer.tsx
@@ -1,40 +1,82 @@
 import React from 'react';
-import circlesLogo from './circlesLogo.svg';
-import CSESocLogo from './CSESocLogo.png';
-import S from './Footer/styles';
+import S from './styles';
 
 const Footer = () => {
   return (
-    <div>
-      <div className="left">
-        <b>circles</b>
-        <img src={CSESocLogo} alt="CSESoc Logo" />
-        <img src={circlesLogo} alt="Circles Logo" />
-        <p>
-          CSESoc is the constituent student society of UNSW’s School of Computer Science and
-          Engineering. We do not represent the School, Faculty, or University. This website seeks to
-          be a general guide, but its information has not been officially endorsed and is subject to
-          change or correction. This is not official advice, and you should confirm any statements
-          are correct before relying on it. Any opinions expressed are those of the authors, and may
-          not necessarily represent those of the University, Faculty, School, or Society. You are
-          responsible for any content provided and should only post content which you are
-          comfortable with sharing. We reserve the right to remove content from the website if it is
-          deemed to be abusive, offensive or otherwise inappropriate. UNSW policies apply and if a
-          breach is detected, further action may be taken with the University.
-        </p>
-        <p>© 2022 — CSESoc UNSW</p>
-      </div>
-      <div className="right">
-        <b>Leave Feedback</b>
-        <p>Any comments, suggestions or feedback for improvement? We want to hear it all! </p>
-        <div className="info">
-          <input className="landing-page-input" placeholder="Name" />
-          <input placeholder="Email address" />
-        </div>
-        <S.styledInput placeholder="Comment" />
-        <p>Checkout our github repo!</p>
-      </div>
-    </div>
+    <S.FooterContainer>
+      <img
+        width="100%"
+        style={{ position: 'absolute' }}
+        src="./src/assets/LandingPage/Footer/backgroundShape.svg"
+        alt="CSESoc Logo"
+      />
+      <S.FooterContentContainer>
+        <S.FooterLeftContent>
+          <S.CirclesLogoContainer>
+            <img
+              width="61"
+              height="92"
+              src="./src/assets/LandingPage/Footer/circlesLogo.svg"
+              alt="Circles Logo"
+            />
+            <S.CirclesTitle>circles</S.CirclesTitle>
+          </S.CirclesLogoContainer>
+          <img
+            width="290px"
+            height="66px"
+            src="./src/assets/LandingPage/Footer/CSESocLogo.svg"
+            alt="CSESoc Logo"
+          />
+          <S.FooterTextContainer>
+            <S.FooterMainLeftText>
+              CSESoc is the constituent student society of UNSW’s School of Computer Science and
+              Engineering. We do not represent the School, Faculty, or University.
+            </S.FooterMainLeftText>
+            <S.FooterMainLeftText>
+              This website seeks to be a general guide, but its information has not been officially
+              endorsed and is subject to change or correction. This is not official advice, and you
+              should confirm any statements are correct before relying on it. Any opinions expressed
+              are those of the authors, and may not necessarily represent those of the University,
+              Faculty, School, or Society.
+            </S.FooterMainLeftText>
+            <S.FooterMainLeftText>
+              You are responsible for any content provided and should only post content which you
+              are comfortable with sharing. We reserve the right to remove content from the website
+              if it is deemed to be abusive, offensive or otherwise inappropriate. UNSW policies
+              apply and if a breach is detected, further action may be taken with the University.
+            </S.FooterMainLeftText>
+            <b>© 2022 — CSESoc UNSW</b>
+          </S.FooterTextContainer>
+        </S.FooterLeftContent>
+        <S.FooterRightContent>
+          <S.FooterTextContainer>
+            <S.LeaveFeedbackText>Leave Feedback</S.LeaveFeedbackText>
+            <p> Any comments, suggestions or feedback for improvement? We want to hear it all!</p>
+          </S.FooterTextContainer>
+          <S.FormInput>
+            <S.LeftFormInput>
+              <S.LeftStyledInput placeholder="Name" />
+              <S.LeftStyledInput placeholder="Email address" />
+            </S.LeftFormInput>
+            <S.RightFormInput>
+              <S.RightStyledInput placeholder="Comment" />
+            </S.RightFormInput>
+          </S.FormInput>
+          <S.FooterSubmitButton type="submit">
+            <b>SUBMIT</b>
+          </S.FooterSubmitButton>
+          <S.GithubText>
+            <b>Checkout our github repo!</b>
+            <img
+              width="61"
+              height="92"
+              src="./src/assets/LandingPage/Footer/githubLogo.svg"
+              alt="Github Logo"
+            />
+          </S.GithubText>
+        </S.FooterRightContent>
+      </S.FooterContentContainer>
+    </S.FooterContainer>
   );
 };
 
--- frontend/src/pages/LandingPage/styles.ts
+++ frontend/src/pages/LandingPage/styles.ts
@@ -110,6 +110,135 @@ const GradientBox = styled.div`
   background: -webkit-linear-gradient(#eeddff, #faeef5, #ffffff);
 `;
 
+const FooterContainer = styled.div`
+  margin-bottom: 0;
+  width: 100%;
+`;
+
+const FooterContentContainer = styled.div`
+  position: 'absolute';
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  justify-content: space-between;
+  padding: 150px 50px 0px 50px;
+  gap: 50px;
+  flex-wrap: wrap;
+  background-color: #9154de;
+`;
+
+const FooterLeftContent = styled.div`
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const CirclesLogoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 30px;
+`;
+
+const CirclesTitle = styled.b`
+  color: white;
+  font-size: 50px;
+`;
+
+const FooterTextContainer = styled.p`
+  color: white;
+`;
+
+const FooterMainLeftText = styled.p`
+  font-size: 8px;
+`;
+
+const FooterRightContent = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  padding-top: 30px;
+`;
+
+const LeaveFeedbackText = styled.b`
+  font-size: 30px;
+`;
+
+const FormInput = styled.div`
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  justify-content: space-between;
+  gap: 10px;
+  height: 20%;
+`;
+
+const StyledInput = styled.input`
+  font-size: 15px;
+  background-color: #d3b4eb;
+  color: #b768c3;
+  border-radius: 30px;
+  border-style: none;
+`;
+
+const LeftFormInput = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const LeftStyledInput = styled.input`
+  font-size: 15px;
+  background-color: #d3b4eb;
+  font-color: #b768c3;
+  border-color: #fff4f4;
+  border-radius: 10px;
+  border-style: none;
+  padding: 0px 5%;
+  height: 50%;
+  ::placeholder {
+    color: #b768c3;
+  }
+`;
+
+const RightFormInput = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const RightStyledInput = styled.input`
+  font-size: 15px;
+  background-color: #d3b4eb;
+  border-radius: 10px;
+  border-style: none;
+  padding: 0px 5%;
+  height: 100%;
+  ::placeholder {
+    color: #b768c3;
+  }
+`;
+
+const FooterSubmitButton = styled.button`
+  margin-top: 30px;
+  width: 187px;
+  height: 50px;
+  border-radius: 30px;
+  color: #9453e6;
+  border: none;
+  cursor: pointer;
+`;
+
+const GithubText = styled.p`
+  color: white;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  justify-content: end;
+`;
+
 export default {
   LandingPageContainer,
   LandingPageTitle,
@@ -123,5 +252,22 @@ export default {
   InteractiveViewTextContainer,
   InteractiveViewTitle,
   InteractiveViewText,
-  GradientBox
+  GradientBox,
+  FooterContainer,
+  FooterContentContainer,
+  FooterLeftContent,
+  CirclesLogoContainer,
+  CirclesTitle,
+  FooterMainLeftText,
+  FooterRightContent,
+  LeaveFeedbackText,
+  FooterTextContainer,
+  LeftFormInput,
+  LeftStyledInput,
+  RightFormInput,
+  RightStyledInput,
+  FormInput,
+  StyledInput,
+  FooterSubmitButton,
+  GithubText
 };
