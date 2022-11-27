c9a8284baf29bffa7efe9712b7a607546f57f220
Kellie Yau <97064025+kellieyau@users.noreply.github.com>
2022-11-27 22:51:11 +1100

    fixed up footer and styling, works between 360-1200px width

--- frontend/src/pages/LandingPage/FooterSection/Footer.tsx
+++ frontend/src/pages/LandingPage/FooterSection/Footer.tsx
@@ -4,77 +4,33 @@ import S from './styles';
 const Footer = () => {
   return (
     <S.FooterContainer>
-      <img
-        width="100%"
-        style={{ position: 'absolute' }}
-        src="./src/assets/LandingPage/Footer/backgroundShape.svg"
-        alt="CSESoc Logo"
-      />
+      <S.StyledImg src="./src/assets/LandingPage/Footer/backgroundTop.svg" alt="CSESoc Logo" />
       <S.FooterContentContainer>
-        <S.FooterLeftContent>
-          <S.CirclesLogoContainer>
-            <img
-              width="61"
-              height="92"
-              src="./src/assets/LandingPage/Footer/circlesLogo.svg"
-              alt="Circles Logo"
-            />
-            <S.CirclesTitle>circles</S.CirclesTitle>
-          </S.CirclesLogoContainer>
-          <img
-            width="290px"
-            height="66px"
-            src="./src/assets/LandingPage/Footer/CSESocLogo.svg"
-            alt="CSESoc Logo"
-          />
-          <S.FooterTextContainer>
-            <S.FooterMainLeftText>
-              CSESoc is the constituent student society of UNSW’s School of Computer Science and
-              Engineering. We do not represent the School, Faculty, or University.
-            </S.FooterMainLeftText>
-            <S.FooterMainLeftText>
-              This website seeks to be a general guide, but its information has not been officially
-              endorsed and is subject to change or correction. This is not official advice, and you
-              should confirm any statements are correct before relying on it. Any opinions expressed
-              are those of the authors, and may not necessarily represent those of the University,
-              Faculty, School, or Society.
-            </S.FooterMainLeftText>
-            <S.FooterMainLeftText>
-              You are responsible for any content provided and should only post content which you
-              are comfortable with sharing. We reserve the right to remove content from the website
-              if it is deemed to be abusive, offensive or otherwise inappropriate. UNSW policies
-              apply and if a breach is detected, further action may be taken with the University.
-            </S.FooterMainLeftText>
-            <b>© 2022 — CSESoc UNSW</b>
-          </S.FooterTextContainer>
-        </S.FooterLeftContent>
-        <S.FooterRightContent>
-          <S.FooterTextContainer>
-            <S.LeaveFeedbackText>Leave Feedback</S.LeaveFeedbackText>
-            <p> Any comments, suggestions or feedback for improvement? We want to hear it all!</p>
-          </S.FooterTextContainer>
-          <S.FormInputContainer>
-            <S.LeftFormInput>
-              <S.LeftStyledInput placeholder="Name" />
-              <S.LeftStyledInput placeholder="Email address" />
-            </S.LeftFormInput>
-            <S.RightFormInput>
-              <S.RightStyledInput placeholder="Comment" />
-            </S.RightFormInput>
-          </S.FormInputContainer>
-          <S.FooterSubmitButton type="submit">
-            <b>SUBMIT</b>
-          </S.FooterSubmitButton>
-          <S.GithubText>
-            <b>Checkout our github repo!</b>
-            <img
-              width="61"
-              height="92"
-              src="./src/assets/LandingPage/Footer/githubLogo.svg"
-              alt="Github Logo"
-            />
-          </S.GithubText>
-        </S.FooterRightContent>
+        <S.FooterContent>
+          <S.StyledCSELogo src="./src/assets/LandingPage/Footer/CSESocLogo.svg" alt="CSESoc Logo" />
+          <S.BottomLeft>© 2022 — CSESoc UNSW</S.BottomLeft>
+        </S.FooterContent>
+        <S.FooterContent>
+          <S.FooterText>
+            CSESoc is the constituent student society of UNSW’s School of Computer Science and
+            Engineering. We do not represent the School, Faculty, or University.
+            <br />
+            <br />
+            This website seeks to be a general guide for degree planning and course selection, but
+            its information has not been officially endorsed and is subject to change or correction.
+            This is not official advice, and you should confirm any statements are correct before
+            relying on it. You should confirm with official resources endorsed by UNSW and any
+            information found here may not necessarily represent those of the University, Faculty,
+            School, or the Computer Science and Engineering Society.
+            <br />
+            <br />
+            You are responsible for planning your degree and the Computer Science and Engineering
+            Society have no responsibility on whether the information shown is accurate.
+            <br />
+            <br />
+            Circles was made with love by CSE students for CSE students.
+          </S.FooterText>
+        </S.FooterContent>
       </S.FooterContentContainer>
     </S.FooterContainer>
   );
--- frontend/src/pages/LandingPage/FooterSection/styles.ts
+++ frontend/src/pages/LandingPage/FooterSection/styles.ts
@@ -2,158 +2,60 @@ import styled from 'styled-components';
 
 const FooterContainer = styled.div`
   margin-bottom: 0;
-  width: 100%;
-  background: -webkit-linear-gradient(rgba(145, 84, 222, 0) 160px, rgba(145, 84, 222, 1) 10px);
-  @media all and (max-width: 450px) {
-    background: -webkit-linear-gradient(rgba(145, 84, 222, 0) 5%, rgba(145, 84, 222, 1) 10px);
+  background: -webkit-linear-gradient(rgba(145, 84, 222, 0) 11vh, rgba(145, 84, 222, 1) 11vh);
+  @media all and (max-width: 730px) {
+    background: -webkit-linear-gradient(rgba(145, 84, 222, 0) 5vh, rgba(145, 84, 222, 1) 5vh);
   }
 `;
 
 const FooterContentContainer = styled.div`
-  position: absolute;
   display: flex;
   flex-direction: row;
   position: relative;
   justify-content: space-between;
-  padding: 150px 50px 0px 50px;
+  padding: 0px 100px;
   gap: 80px;
-  flex-wrap: wrap;
+  @media all and (max-width: 900px) {
+    flex-wrap: wrap;
+    gap: 30px;
+  }
 `;
 
-const FooterLeftContent = styled.div`
-  width: 40%;
+const FooterContent = styled.div`
   display: flex;
-  flex-direction: column;
-  gap: 20px;
+`;
+
+const StyledImg = styled.img`
+  width: 100%;
+`;
+
+const StyledCSELogo = styled.img`
+  width: 290px;
+  height: 66px;
   @media all and (max-width: 900px) {
     width: 100%;
   }
 `;
 
-const CirclesLogoContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 30px;
-`;
-
-const CirclesTitle = styled.b`
+const FooterText = styled.p`
   color: white;
-  font-size: 50px;
-`;
-
-const FooterTextContainer = styled.p`
-  color: white;
-`;
-
-const FooterMainLeftText = styled.p`
   font-size: 12px;
+  padding-bottom: 100px;
+  white-space: pre-line;
 `;
 
-const FooterRightContent = styled.div`
-  width: 50%;
-  display: flex;
-  flex-direction: column;
-  padding-top: 30px;
-  @media all and (max-width: 900px) {
-    width: 100%;
-  }
-`;
-
-const LeaveFeedbackText = styled.b`
-  font-size: 30px;
-`;
-
-const FormInputContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  position: relative;
-  justify-content: space-between;
-  gap: 10px;
-  height: 126px;
-`;
-
-const StyledInput = styled.input`
-  font-size: 15px;
-  background-color: #d3b4eb;
-  color: #b768c3;
-  border-radius: 30px;
-  border-style: none;
-`;
-
-const LeftFormInput = styled.div`
-  width: 50%;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-`;
-
-const LeftStyledInput = styled.input`
-  font-size: 15px;
-  background-color: #d3b4eb;
-  font-color: #b768c3;
-  border-color: #fff4f4;
-  border-radius: 10px;
-  border-style: none;
-  padding: 0px 5%;
-  height: 50%;
-  ::placeholder {
-    color: #b768c3;
-  }
-`;
-
-const RightFormInput = styled.div`
-  width: 50%;
-  display: flex;
-  flex-direction: column;
-`;
-
-const RightStyledInput = styled.input`
-  font-size: 15px;
-  background-color: #d3b4eb;
-  border-radius: 10px;
-  border-style: none;
-  padding: 0px 5%;
-  height: 100%;
-  ::placeholder {
-    color: #b768c3;
-  }
-`;
-
-const FooterSubmitButton = styled.button`
-  margin-top: 30px;
-  width: 187px;
-  height: 50px;
-  border-radius: 30px;
-  color: #9453e6;
-  border: none;
-  cursor: pointer;
-`;
-
-const GithubText = styled.p`
+const BottomLeft = styled.b`
+  position: absolute;
+  bottom: 50px;
   color: white;
-  display: flex;
-  align-items: center;
-  gap: 20px;
-  justify-content: end;
 `;
 
 export default {
   FooterContainer,
+  StyledImg,
   FooterContentContainer,
-  FooterLeftContent,
-  CirclesLogoContainer,
-  CirclesTitle,
-  FooterMainLeftText,
-  FooterRightContent,
-  LeaveFeedbackText,
-  FooterTextContainer,
-  LeftFormInput,
-  LeftStyledInput,
-  RightFormInput,
-  RightStyledInput,
-  FormInputContainer,
-  StyledInput,
-  FooterSubmitButton,
-  GithubText
+  FooterContent,
+  StyledCSELogo,
+  BottomLeft,
+  FooterText
 };
