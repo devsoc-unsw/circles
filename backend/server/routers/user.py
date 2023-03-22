4cad3f19a41aceafc359b2c27be19c8eba196a03
Christian Lam <66155270+ui-Auxilary@users.noreply.github.com>
2023-03-22 13:59:33 +1100

    Christian/circles 347 create endpoints for degree slice (#1007)
    * feat: Added pseudo routes for backend based on FE degree slices
    
    * refactor: Refactor based on PR comments
    
    * feat: Remove comment
    
    * fix: Compliant to mypy (didn't see this before oops)
    
    * feat: More pylint compliant ie. fix trailing white spaces
    
    * fix: Remove unnecesary comment
    
    ---------
    
    Co-authored-by: Christian Lam <christian.lam@csesoc.org.au>

--- backend/server/routers/user.py
+++ backend/server/routers/user.py
@@ -1,13 +1,14 @@
 from itertools import chain
-from typing import List, cast
-from fastapi import APIRouter, HTTPException
+from typing import cast
+from fastapi import APIRouter
 from data.config import LIVE_YEAR
 from server.database import usersDB
 from bson.objectid import ObjectId
-from server.routers.model import CourseMark, Courses, LocalStorage, PlannerLocalStorage, Storage
 import pydantic
 from server.config import DUMMY_TOKEN
-pydantic.json.ENCODERS_BY_TYPE[ObjectId]=str
+from server.routers.model import CourseMark, LocalStorage, PlannerLocalStorage, Storage
+
+pydantic.json.ENCODERS_BY_TYPE[ObjectId] = str
 
 router = APIRouter(
     prefix="/user",
@@ -15,42 +16,48 @@ router = APIRouter(
 )
 
 # keep this private
+
+
 def set_user(token: str, item: Storage, overwrite: bool = False):
     data = usersDB['tokens'].find_one({'token': token})
     if data:
         if not overwrite:
             return
         objectID = data['objectId']
-        usersDB['users'].update_one({'_id': ObjectId(objectID)}, {'$set': item})
+        usersDB['users'].update_one(
+            {'_id': ObjectId(objectID)}, {'$set': item})
     else:
         objectID = usersDB['users'].insert_one(dict(item)).inserted_id
         usersDB['tokens'].insert_one({'token': token, 'objectId': objectID})
 
 
 # Ideally not used often.
-# 
+#
 @router.post("/saveLocalStorage/")
 def save_local_storage(localStorage: LocalStorage, token: str = DUMMY_TOKEN):
     # TODO: turn giving no token into an error
-    planned: list[str] = sum((sum(year.values(), []) for year in localStorage.planner['years']), [])
+    planned: list[str] = sum((sum(year.values(), [])
+                             for year in localStorage.planner['years']), [])
     unplanned: list[str] = localStorage.planner['unplanned']
     courses = {
         course: {
             'code': course,
-            'suppressed': localStorage.planner['courses'][course]['supressed'], # this is peter's fault for sucking at spelling
+            # this is peter's fault for sucking at spelling
+            'suppressed': localStorage.planner['courses'][course]['supressed'],
             'mark': localStorage.planner['courses'][course].get('mark', None)
         }
         for course in chain(planned, unplanned)
     }
     # cancer, but the FE inspired this cancer
     real_planner = localStorage.planner.copy()
-    real_planner.pop('courses') # type: ignore
+    real_planner.pop('courses')  # type: ignore
     item = {
         'degree': localStorage.degree,
         'planner': real_planner,
         'courses': courses
     }
     set_user(token, cast(Storage, item))
+
 
 @router.get("/data/{token}")
 def get_user(token: str) -> Storage:
@@ -60,8 +67,8 @@ def get_user(token: str) -> Storage:
         # should error and prompt a registration - at some point ;)
         return default_cs_user()
         # raise HTTPException(400,f"Invalid token: {token}")
-    else:
-        return cast(Storage, usersDB['users'].find_one({'_id': ObjectId(data['objectId'])}))
+    return cast(Storage, usersDB['users'].find_one(
+        {'_id': ObjectId(data['objectId'])}))
 
 
 # this is super jank - should never see prod
@@ -71,6 +78,8 @@ def register_user(token: str):
     set_user(token, user)
 
 # makes an empty CS Student
+
+
 def default_cs_user() -> Storage:
     planner: PlannerLocalStorage = {
         'mostRecentPastTerm': {
@@ -78,7 +87,7 @@ def default_cs_user() -> Storage:
             'T': 0
         },
         'unplanned': [],
-        'isSummerEnabled': True, # shoudl default be false? who knows
+        'isSummerEnabled': True, 
         'startYear': LIVE_YEAR,
         'years': [],
         'courses': {},
@@ -101,6 +110,7 @@ def toggle_summer_term(token: str = DUMMY_TOKEN):
     user['planner']['isSummerEnabled'] = not user['planner']['isSummerEnabled']
     set_user(token, user, True)
 
+
 @router.put("/toggleWarnings")
 def toggle_warnings(courses: list[str], token: str = DUMMY_TOKEN):
     user = get_user(token)
@@ -108,17 +118,20 @@ def toggle_warnings(courses: list[str], token: str = DUMMY_TOKEN):
         user['courses'][course]['suppressed'] = not user['courses'][course]['suppressed']
     set_user(token, user, True)
 
+
 @router.put("/updateCourseMark")
 def update_course_mark(courseMark: CourseMark, token: str = DUMMY_TOKEN):
     user = get_user(token)
     user['courses'][courseMark.course]['mark'] = courseMark.mark
     set_user(token, user, True)
 
+
 @router.put("/updateStartYear")
 def update_start_year(startYear: int, token: str = DUMMY_TOKEN):
     """
-        update the start year the user is taking. We assume that the number of years will stay the same,
-        and that the user will want their courses to be matched *by year taken*.
+        update the start year the user is taking. We assume that the number of years
+        will stay the same, and that the user will want their courses to be matched
+        *by year taken*.
     """
     user = get_user(token)
     if startYear == user['planner']['startYear']:
@@ -133,8 +146,9 @@ def update_start_year(startYear: int, token: str = DUMMY_TOKEN):
         # pad the beginning and yeet the end
         user['planner']['years'] = \
             ([{"T0": [], "T1": [], "T2": [], "T3": []}] * diff) + \
-            user['planner']['years'][:-diff] # type: ignore
+            user['planner']['years'][:-diff]  # type: ignore
     set_user(token, user, True)
+
 
 @router.put("/updateDegreeLength")
 def update_degree_length(numYears: int, token: str = DUMMY_TOKEN):
@@ -143,53 +157,59 @@ def update_degree_length(numYears: int, token: str = DUMMY_TOKEN):
         return
     diff = numYears - len(user['planner']['years'])
     if diff > 0:
-        user['planner']['years'] += ([{"T0": [], "T1": [], "T2": [], "T3": []}] * diff)
+        user['planner']['years'] += ([{"T0": [],
+                                     "T1": [], "T2": [], "T3": []}] * diff)
     else:
         user['planner']['years'] = user['planner']['years'][:diff]
     set_user(token, user, True)
 
-@router.put("/setProgram") 
+
+@router.put("/setProgram")
 def setProgram(programCode: str, token: str = DUMMY_TOKEN):
     user = get_user(token)
-    user['user']['degree']['programCode'] = programCode
+    user['degree']['programCode'] = programCode
     set_user(token, user, True)
 
-@router.put("/addSpecialisation") 
+
+@router.put("/addSpecialisation")
 def addSpecialisation(specialisation: str, token: str = DUMMY_TOKEN):
     user = get_user(token)
-    user['user']['degree']['specs'].append(specialisation)
+    user['degree']['specs'].append(specialisation)
     set_user(token, user, True)
 
-@router.put("/removeSpecialisation") 
+
+@router.put("/removeSpecialisation")
 def removeSpecialisation(specialisation: str, token: str = DUMMY_TOKEN):
     user = get_user(token)
-    specs = user['user']['degree']['specs']
+    specs = user['degree']['specs']
     if specialisation in specs:
         specs.remove(specialisation)
     set_user(token, user, True)
 
-@router.put("/setIsComplete") 
+
+@router.put("/setIsComplete")
 def setIsComplete(isComplete: bool, token: str = DUMMY_TOKEN):
-    user = get_user(token);
-    user['user']['degree']['isComplete'] = isComplete
+    user = get_user(token)
+    user['degree']['isComplete'] = isComplete
     set_user(token, user, True)
+
 
 @router.put("/reset")
 def reset(token: str = DUMMY_TOKEN):
-    user = get_user(token)
+    """Resets user data of a parsed token"""
     planner: PlannerLocalStorage = {
         'mostRecentPastTerm': {
             'Y': 0,
             'T': 0
         },
         'unplanned': [],
-        'isSummerEnabled': True, 
+        'isSummerEnabled': True,
         'startYear': LIVE_YEAR,
         'years': [],
         'courses': {},
     }
-    
-    user['user'] = {
+
+    user: Storage = {
         'degree': {
             'programCode': '',
             'specs': [],
@@ -199,4 +219,3 @@ def reset(token: str = DUMMY_TOKEN):
         'courses': {}
     }
     set_user(token, user, True)
-    
