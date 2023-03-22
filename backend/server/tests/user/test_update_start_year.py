3a9642ed1cbb196830858cdce16830146e09862f
Lucas <me@lhvy.dev>
2023-03-22 15:08:23 +1100

    Update backend logic

--- backend/server/tests/user/test_update_start_year.py
+++ backend/server/tests/user/test_update_start_year.py
@@ -16,9 +16,8 @@ def test_updateCourseMark_back_time_2019():
         f'http://127.0.0.1:8000/user/updateStartYear?startYear=2019')
     user_after = requests.get(f'http://127.0.0.1:8000/user/data/{DUMMY_TOKEN}').json()
     assert len(user_before['planner']['years']) == len(user_after['planner']['years'])
-    assert user_after['planner']['years'][0] == {"T0": [], "T1": [], "T2": [], "T3": []}
-    assert user_after['planner']['years'][1] == { "T0": [], "T1": [ "COMP1511", "MATH1141", "MATH1081"], "T2": [ "COMP1521", "COMP1531", "COMP2521"], "T3": []}
-    assert user_after['planner']['years'][2] == {"T0": [], "T1": ["ENGG2600"], "T2": ["ENGG2600"], "T3": ["ENGG2600"]}
+    assert user_before['planner']['years'] == user_after['planner']['years']
+    assert user_after['planner']['startYear'] == 2019
     
 
 def test_updateCourseMark_back_time_2018():
@@ -30,9 +29,8 @@ def test_updateCourseMark_back_time_2018():
         f'http://127.0.0.1:8000/user/updateStartYear?startYear=2018')
     user_after = requests.get(f'http://127.0.0.1:8000/user/data/{DUMMY_TOKEN}').json()
     assert len(user_before['planner']['years']) == len(user_after['planner']['years'])
-    assert user_after['planner']['years'][0] == {"T0": [], "T1": [], "T2": [], "T3": []}
-    assert user_after['planner']['years'][1] == {"T0": [], "T1": [], "T2": [], "T3": []}
-    assert user_after['planner']['years'][2] == { "T0": [], "T1": [ "COMP1511", "MATH1141", "MATH1081"], "T2": [ "COMP1521", "COMP1531", "COMP2521"], "T3": []}
+    assert user_before['planner']['years'] == user_after['planner']['years']
+    assert user_after['planner']['startYear'] == 2018
 
 def test_updateCourseMark_forward_time_2021():
     clear()
@@ -43,6 +41,5 @@ def test_updateCourseMark_forward_time_2021():
         f'http://127.0.0.1:8000/user/updateStartYear?startYear=2021')
     user_after = requests.get(f'http://127.0.0.1:8000/user/data/{DUMMY_TOKEN}').json()
     assert len(user_before['planner']['years']) == len(user_after['planner']['years'])
-    assert user_after['planner']['years'][0] == {"T0": [], "T1": ["ENGG2600"], "T2": ["ENGG2600"], "T3": ["ENGG2600"]}
-    assert user_after['planner']['years'][1] == {"T0": [], "T1": [], "T2": [], "T3": []}
-    assert user_after['planner']['years'][2] == {"T0": [], "T1": [], "T2": [], "T3": []}
+    assert user_before['planner']['years'] == user_after['planner']['years']
+    assert user_after['planner']['startYear'] == 2021
