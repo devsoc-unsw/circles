197dfdbc7579b2fb6064eab5c453a6eb21cefe77
Christian Lam <christian.lam@csesoc.org.au>
2023-04-18 10:54:51 +1000

    fix: Fix spacing

--- frontend/src/components/ResetModal/ResetModal.tsx
+++ frontend/src/components/ResetModal/ResetModal.tsx
@@ -9,46 +9,43 @@ import { useSelector } from 'react-redux';
 import { RootState } from 'config/store';
 
 type Props = {
-    open?: boolean;
-    onOk?: () => void; // runs after resetting the data
-    onCancel?: () => void;
+  open?: boolean;
+  onOk?: () => void; // runs after resetting the data
+  onCancel?: () => void;
 };
 
 // has no internal "open" state since it becomes difficult to juggle with external buttons
 const ResetModal = ({ open, onOk, onCancel }: Props) => {
-    const dispatch = useAppDispatch();
-    const { token } = useSelector((state: RootState) => state.settings);
-
-    const handleOk = async () => {
-        dispatch(resetPlanner());
-        try {
-            await axios.post('user/reset', {}, { params: { token } });
-        } catch (err) {
-            // eslint-disable-next-line no-console
-            console.error('Error resetting degree at handleDegreeChange: ' + err);
-        }
-        dispatch(resetTabs());
-        dispatch(resetCourses());
-
-        onOk?.();
-    };
-
-    return (
-        <Modal
-            title="Reset Planner?"
-            open={open ?? false}
-            closable={false}
-            onOk={handleOk}
-            okText="Reset"
-            okButtonProps={{ type: 'primary', danger: true }}
-            onCancel={onCancel}
-            cancelText="Go back"
-        >
-            <div>
-                Are you sure want to reset your planner? Your existing data will be permanently removed.
-            </div>
-        </Modal>
-    );
+  const dispatch = useAppDispatch();
+  const { token } = useSelector((state: RootState) => state.settings);
+  const handleOk = async () => {
+    dispatch(resetPlanner());
+    try {
+      await axios.post('user/reset', {}, { params: { token } });
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error('Error resetting degree at handleDegreeChange: ' + err);
+    }
+    dispatch(resetTabs());
+    dispatch(resetCourses());
+    onOk?.();
+  };
+  return (
+    <Modal
+      title="Reset Planner?"
+      open={open ?? false}
+      closable={false}
+      onOk={handleOk}
+      okText="Reset"
+      okButtonProps={{ type: 'primary', danger: true }}
+      onCancel={onCancel}
+      cancelText="Go back"
+    >
+      <div>
+        Are you sure want to reset your planner? Your existing data will be permanently removed.
+      </div>
+    </Modal>
+  );
 };
 
 export default ResetModal;
--- frontend/src/pages/DegreeWizard/StartBrowsingStep/StartBrowsingStep.tsx
+++ frontend/src/pages/DegreeWizard/StartBrowsingStep/StartBrowsingStep.tsx
@@ -10,41 +10,41 @@ import { useSelector } from 'react-redux';
 import axios from 'axios';
 
 const StartBrowsingStep = () => {
-	const navigate = useNavigate();
-	const { programCode, specs } = useAppSelector((state: RootState) => state.degree);
-	const { token } = useSelector((state: RootState) => state.settings);
+  const navigate = useNavigate();
+  const { programCode, specs } = useAppSelector((state: RootState) => state.degree);
+  const { token } = useSelector((state: RootState) => state.settings);
 
-	const handleSaveUserSettings = async () => {
-		if (!programCode) {
-			openNotification({
-				type: 'error',
-				message: 'Please select a degree'
-			});
-		} else if (!specs.length) {
-			openNotification({
-				type: 'error',
-				message: 'Please select a specialisation'
-			});
-		} else {
-			try {
-				await axios.post('user/reset', true, { params: { token } });
-			} catch (err) {
-				// eslint-disable-next-line no-console
-				console.error('Error resetting degree at handleDegreeChange: ' + err);
-			}
-			navigate('/course-selector');
-		}
-	};
+  const handleSaveUserSettings = async () => {
+    if (!programCode) {
+      openNotification({
+        type: 'error',
+        message: 'Please select a degree'
+      });
+    } else if (!specs.length) {
+      openNotification({
+        type: 'error',
+        message: 'Please select a specialisation'
+      });
+    } else {
+      try {
+        await axios.post('user/reset', true, { params: { token } });
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error('Error resetting degree at handleDegreeChange: ' + err);
+      }
+      navigate('/course-selector');
+    }
+  };
 
-	return (
-		<CS.StepContentWrapper id="start browsing">
-			<S.StartBrowsingWrapper>
-				<Button type="primary" onClick={handleSaveUserSettings}>
-					Start browsing courses!
-				</Button>
-			</S.StartBrowsingWrapper>
-		</CS.StepContentWrapper>
-	);
+  return (
+    <CS.StepContentWrapper id="start browsing">
+      <S.StartBrowsingWrapper>
+        <Button type="primary" onClick={handleSaveUserSettings}>
+          Start browsing courses!
+        </Button>
+      </S.StartBrowsingWrapper>
+    </CS.StepContentWrapper>
+  );
 };
 
 export default StartBrowsingStep;
