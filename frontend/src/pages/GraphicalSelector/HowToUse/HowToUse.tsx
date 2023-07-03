453ffb0b8e0f0581d99c84efb8ab6a5107f32d1e
Sandeep Das <65163571+Dasyure@users.noreply.github.com>
2023-07-03 19:08:34 +1000

    feat: CF-356: unlocked course nodes added to graphical selector (#1035)
    Different course node stylings added for the following states:
    - course node is added to the planner
    - course node is unlocked (i.e. meets the prereqs for that course)
    - course node that is locked
    https://csesoc.atlassian.net/browse/CF-356
    ---------
    
    Co-authored-by: Daysure <sandeep.das@student.unsw.edu.au>
    Co-authored-by: Leonardo Fan <leonardofan2000@gmail.com>

--- frontend/src/pages/GraphicalSelector/HowToUse/HowToUse.tsx
+++ frontend/src/pages/GraphicalSelector/HowToUse/HowToUse.tsx
@@ -9,6 +9,8 @@ import step3Dark from 'assets/GraphicalSelectorHelp/step3-dark.jpg';
 import step3Light from 'assets/GraphicalSelectorHelp/step3-light.jpg';
 import step4Dark from 'assets/GraphicalSelectorHelp/step4-dark.jpg';
 import step4Light from 'assets/GraphicalSelectorHelp/step4-light.jpg';
+import step5Dark from 'assets/GraphicalSelectorHelp/step5-dark.jpg';
+import step5Light from 'assets/GraphicalSelectorHelp/step5-light.jpg';
 import type { RootState } from 'config/store';
 import CS from '../common/styles';
 import S from './styles';
@@ -19,12 +21,11 @@ const HowToUse = () => {
   const { theme } = useSelector((state: RootState) => state.settings);
 
   const step = (num: number) => {
-    let url = '';
-    if (num === 1) url = theme === 'light' ? step1Light : step1Dark;
-    if (num === 2) url = theme === 'light' ? step2Light : step2Dark;
-    if (num === 3) url = theme === 'light' ? step3Light : step3Dark;
-    if (num === 4) url = theme === 'light' ? step4Light : step4Dark;
-    return url;
+    const pics = {
+      light: [step1Light, step2Light, step3Light, step4Light, step5Light],
+      dark: [step1Dark, step2Dark, step3Dark, step4Dark, step5Dark]
+    };
+    return theme === 'light' ? pics.light[num - 1] : pics.dark[num - 1];
   };
 
   return (
@@ -41,17 +42,21 @@ const HowToUse = () => {
         </div>
         <S.ImageStep src={step(1)} alt="How to find a course infographic." />
         <div>
-          <CS.TextWrapper> 2. Use the search bar to bring up the courses. </CS.TextWrapper>
+          <CS.TextWrapper> 2. Courses can be planned, unlocked or locked. </CS.TextWrapper>
         </div>
-        <S.ImageStep src={step(2)} alt="How to use the search bar infographic." />
+        <S.ImageStep src={step(2)} alt="Icons for planned, unlocked or locked courses." />
         <div>
-          <CS.TextWrapper> 3. Hover over a course to see related courses. </CS.TextWrapper>
+          <CS.TextWrapper> 3. Use the search bar to bring up the courses. </CS.TextWrapper>
         </div>
-        <S.ImageStep src={step(3)} alt="How to quickly view related courses." />
+        <S.ImageStep src={step(3)} alt="How to use the search bar infographic." />
         <div>
-          <CS.TextWrapper> 4. Click the course to view the course information! </CS.TextWrapper>
+          <CS.TextWrapper> 4. Hover over a course to see related courses. </CS.TextWrapper>
         </div>
-        <S.ImageStep src={step(4)} alt="Where to click to view course information infographic." />
+        <S.ImageStep src={step(4)} alt="How to quickly view related courses." />
+        <div>
+          <CS.TextWrapper> 5. Click the course to view the course information! </CS.TextWrapper>
+        </div>
+        <S.ImageStep src={step(5)} alt="Where to click to view course information infographic." />
       </S.ContentsWrapper>
     </S.Wrapper>
   );
