d4c9156b4fab1c09bc169c01d106ee428b7f02ee
Martin <martin.knezevic1@gmail.com>
2023-09-05 14:29:10 +1000

    return quickaddcartbutton functionality

--- frontend/src/components/QuickAddCartButton/QuickAddCartButton.tsx
+++ frontend/src/components/QuickAddCartButton/QuickAddCartButton.tsx
@@ -26,6 +26,7 @@ const QuickAddCartButton = ({ courseCode, runMutate, planned }: Props) => {
   const handleClick = (e: React.MouseEvent<HTMLElement>) => {
     e.stopPropagation();
     if (runMutate) runMutate(courseCode);
+    else mutation.mutate(courseCode);
   };
 
   return !planned ? (
