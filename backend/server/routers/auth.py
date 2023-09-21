4d589cb370e00d43c3212a38d982dca17885f08e
ollibowers <80164276+ollibowers@users.noreply.github.com>
2023-09-21 18:16:35 +0000

    wip: started working on session middleware

--- backend/server/routers/auth.py
+++ backend/server/routers/auth.py
@@ -14,14 +14,18 @@ from typing import Annotated, Dict, List, Literal, Optional, Tuple, TypedDict, c
 from fastapi import APIRouter, Depends, HTTPException, Header, Request, Security
 from pydantic import BaseModel
 
-from .auth_utility.middleware import HTTPBearerTokenVerifier, ValidatedToken
-from .auth_utility.oidc_requests import exchange_auth_code_for_tokens, get_user_info
+from backend.server.routers.auth_utility.session_token import SessionStorage
+
+from .auth_utility.middleware import HTTPBearer401, SessionTokenToValidUserID
+from .auth_utility.oidc_requests import UserInfoResponse, exchange_auth_code_for_tokens, get_user_info
 
 router = APIRouter(
     prefix="/auth",
     tags=["auth"],
 )
-require_token = HTTPBearerTokenVerifier()
+sessions = SessionStorage()
+require_token = HTTPBearer401()
+validated_uid = SessionTokenToValidUserID(session_store=sessions)
 
 @router.post('/token')
 def create_user_token(token: str):
@@ -94,13 +98,17 @@ def exchange_authorization_code(data: ExchangeCodePayload) -> str:
         )
     
     # TODO: do some stuff with the id token here like user setup
-    return res[0]
+    return res[0]["access_token"]
 
 @router.get("/info")
 def user_info(token: str = Security(require_token)):
     return get_user_info(token, True)
 
+@router.get("/validatedUserID")
+def get_validated_user_id(user_id: Annotated[str, Security(validated_uid)]) -> str:
+    return user_id
+
 @router.get("/checkToken")
-def check_token(token: str = Security(require_token)):
+def check_token(user_id: str = Security(validated_uid)):
     # TODO: check it is in database
     return
