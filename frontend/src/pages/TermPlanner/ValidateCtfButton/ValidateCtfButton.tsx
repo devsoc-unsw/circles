d9b290d6dd684da5d833931a4f6967999de1ab80
imagine-hussain <93496985+imagine-hussain@users.noreply.github.com>
2023-09-28 00:57:55 +1000

    in the arena trying stuff (#1082)

--- frontend/src/pages/TermPlanner/ValidateCtfButton/ValidateCtfButton.tsx
+++ frontend/src/pages/TermPlanner/ValidateCtfButton/ValidateCtfButton.tsx
@@ -1,13 +1,12 @@
-/* eslint-disable */
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Typography } from 'antd';
 import axios from 'axios';
+import styled from 'styled-components';
 import prepareCoursesForValidationPayload from 'utils/prepareCoursesForValidationPayload';
 import { RootState } from 'config/store';
 import CS from '../common/styles';
 import S from './styles';
-import { Typography } from 'antd';
-import styled from 'styled-components';
 
 type CtfResult = {
   valid: boolean;
@@ -30,14 +29,13 @@ const loadingResult: CtfResult = {
   failed: 0,
   passed: [],
   message: 'Loading...',
-  flags: [],
+  flags: []
 };
 
 const ModalTitle = styled(Title)`
   margin: 0 !important;
   color: ${({ theme }) => theme.text} !important;
 `;
-
 
 const ValidateCtfButton = () => {
   const planner = useSelector((state: RootState) => state.planner);
@@ -46,14 +44,12 @@ const ValidateCtfButton = () => {
   const [result, setResult] = React.useState(loadingResult);
 
   const validateCtf = async () => {
-    // TODO: Call this async and disaplay output
     setOpen(true);
     const res = await axios.post<CtfResult>(
       '/ctf/validateCtf/',
       prepareCoursesForValidationPayload(planner, degree, false)
     );
     setResult(res.data);
-    console.log(res.data);
   };
 
   return (
@@ -68,38 +64,33 @@ const ValidateCtfButton = () => {
       >
         <ModalTitle level={4}>Passed Challenges</ModalTitle>
         <TextBlock>
-          {
-            <ol>
-              {result.passed.map((challenge, index) => (
-                <>
-                  <li>
-                    <TextBlock key={index}>{challenge}</TextBlock>
-                  </li>
-                </>
-              ))}
-            </ol>
-          }
-        </TextBlock>
-        <ModalTitle level={4}>Unlocked Flags</ModalTitle>
-        <TextBlock>
           <ol>
-            {(result.flags.length > 0) && result.flags.map((flag, index) => (
-              <>
-                <li>
-                  <TextBlock key={index}>{flag}</TextBlock>
-                </li>
-              </>
+            {result.passed.map((challenge) => (
+              <li key={challenge}>
+                <TextBlock>{challenge}</TextBlock>
+              </li>
             ))}
           </ol>
         </TextBlock>
+
+        <ModalTitle level={4}>Unlocked Flags</ModalTitle>
+        <TextBlock>
+          <ol>
+            {result.flags.length > 0 &&
+              result.flags.map((flag) => (
+                <li key={flag}>
+                  <TextBlock>{flag}</TextBlock>
+                </li>
+              ))}
+          </ol>
+        </TextBlock>
+
         <ModalTitle level={4}>
-          {
-            result.valid ? (
-              <Text type="success">{result.message}</Text>
-            ) : (
-              <Text type="warning">Next: {result.message}</Text>
-            )
-          }
+          {result.valid ? (
+            <Text type="success">{result.message}</Text>
+          ) : (
+            <Text type="warning">Next: {result.message}</Text>
+          )}
         </ModalTitle>
       </S.Modal>
     </>
