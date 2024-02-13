c412d382c56c4474150711725545c5d19b643a96
ollibowers <80164276+ollibowers@users.noreply.github.com>
2024-02-13 10:18:27 +0000

    fix: add more gap between the sponsors

--- frontend/src/pages/LandingPage/SponsorSection/styles.ts
+++ frontend/src/pages/LandingPage/SponsorSection/styles.ts
@@ -1,21 +1,20 @@
 import styled from 'styled-components';
 
 const SponsorsText = styled.h1`
-  font-size: 30px;
+  font-size: 2rem;
   background: -webkit-linear-gradient(30deg, #9f62de, #b77eff);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   text-align: center;
   position: relative;
-  margin-bottom: 15px;
+  margin-bottom: 1rem;
   font-weight: 650;
-  padding-top: 15px;
 `;
 
 const LogosWrapper = styled.div`
   display: flex;
   justify-content: center;
-  gap: 30px;
+  gap: 4rem;
 `;
 
 const LogoImg = styled.img`
