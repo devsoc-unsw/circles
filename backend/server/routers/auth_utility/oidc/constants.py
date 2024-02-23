ffbc1a485e66b7e1aab10e5aede6ebc28ebeffdc
ollibowers <80164276+ollibowers@users.noreply.github.com>
2024-02-23 06:33:12 +0000

    wip: started refactor for new auth flow

--- backend/server/routers/auth_utility/oidc/constants.py
+++ backend/server/routers/auth_utility/oidc/constants.py
@@ -3,6 +3,5 @@ from dotenv import load_dotenv
 
 
 load_dotenv("./env/backend.env")
-JWT_SECRET = os.getenv("AUTH_JWT_SECRET") or exit(1)
 CLIENT_ID = os.getenv("AUTH_CSE_CLIENT_ID") or exit(1)
 CLIENT_SECRET = os.getenv("AUTH_CSE_CLIENT_SECRET") or exit(1)
