a80308462e1b2d41d3b8124196f46583d4494f88
Lucas <me@lhvy.dev>
2024-04-07 14:03:00 +1000

    Refactor Unilectives API helper

--- frontend/src/utils/api/unilectivesApi.ts
+++ frontend/src/utils/api/unilectivesApi.ts
@@ -4,8 +4,10 @@ import { UnilectivesCourse } from 'types/unilectives';
 // eslint-disable-next-line import/prefer-default-export
 export const getCourseRating = async (code: string) => {
   try {
-    const res = await axios.get(`https://unilectives.devsoc.app/api/v1/course/${code}`);
-    return (res.data as { course: UnilectivesCourse }).course;
+    const res = await axios.get<{ course: UnilectivesCourse }>(
+      `https://unilectives.devsoc.app/api/v1/course/${code}`
+    );
+    return res.data.course;
   } catch (err) {
     // eslint-disable-next-line no-console
     console.error('Error at getCourseRating: ', err);
