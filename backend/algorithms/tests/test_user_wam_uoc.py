577b95413d140e9700ed5631adc838c70fedcff7
Lucas <me@lhvy.dev>
2024-05-27 01:19:15 +1000

    Update tests after refactoring WAM method

--- backend/algorithms/tests/test_user_wam_uoc.py
+++ backend/algorithms/tests/test_user_wam_uoc.py
@@ -19,11 +19,18 @@ def test_category_instantiation_causes_error():
 def test_course_category_produces_correct_wam():
     user = User(USERS["user4"])
     comp = CourseCategory("COMP")
-    assert user.wam(CourseCategory("FOOD")) is None
-    assert user.wam(comp) == 66
+    wam, marks_complete = user.wam(CourseCategory("FOOD"))
+    assert wam == None
+    assert marks_complete
+
+    wam, marks_complete = user.wam(comp)
+    assert wam == 66
+    assert marks_complete
 
     user.add_courses({"COMP1069": (2, 69)})
-    assert user.wam(comp) == 66.3
+    wam, marks_complete = user.wam(comp)
+    assert wam == 66.3
+    assert marks_complete
 
 
 def test_course_category_produces_correct_uoc():
