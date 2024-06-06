67845ef5455cb9ae1797e824374111b47faadf4e
Deki Lhamu <z5268870@student.unsw.edu.au>
2024-06-06 21:01:08 +1000

    downsized tab loading status

--- frontend/src/components/Spinner/Spinner.tsx
+++ frontend/src/components/Spinner/Spinner.tsx
@@ -5,13 +5,25 @@ import S from './styles';
 
 type Props = {
   text: string;
+  component?: string;
 };
 
-const Spinner = ({ text }: Props) => (
-  <S.Wrapper className="loading-spinner">
-    <Spin indicator={<LoadingOutlined style={{ fontSize: '2.5rem' }} spin />} />
-    <p>{text}</p>
-  </S.Wrapper>
-);
+const Spinner = ({ text, component = '' }: Props) => {
+  // modify spinner css for course tab loads
+  if (component === 'course-tab') {
+    return (
+      <S.Wrapper className="loading-spinner" style={{ gap: '1px' }}>
+        <Spin indicator={<LoadingOutlined style={{ fontSize: '1rem' }} spin />} />
+        <p style={{ fontSize: '0.5rem' }}>{text}</p>
+      </S.Wrapper>
+    );
+  }
+  return (
+    <S.Wrapper className="loading-spinner">
+      <Spin indicator={<LoadingOutlined style={{ fontSize: '2.5rem' }} spin />} />
+      <p>{text}</p>
+    </S.Wrapper>
+  );
+};
 
 export default Spinner;
