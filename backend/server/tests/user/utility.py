d0dc4a4079330b1cb844f0c57c49cebb153a1371
ollibowers <80164276+ollibowers@users.noreply.github.com>
2024-06-26 16:43:07 +0000

    fix: added redis reset to test clear helper

--- backend/server/tests/user/utility.py
+++ backend/server/tests/user/utility.py
@@ -6,8 +6,10 @@ load_dotenv("../env/backend.env")
 os.environ["MONGODB_SERVICE_HOSTNAME"] = "localhost"
 
 from server.db.mongo.setup import setup_user_related_collections
+from server.db.redis.setup import setup_redis_sessionsdb
 
 
 def clear():
     """drop users in database. Used before every test is run."""
     setup_user_related_collections(drop=True)
+    setup_redis_sessionsdb()
