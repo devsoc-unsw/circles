f40acd6ae7eea61f97ef423af125594fd6c213ff
ollibowers <80164276+ollibowers@users.noreply.github.com>
2024-06-26 17:24:20 +0000

    fix: tests can run locally now given you have env files

--- backend/server/tests/user/utility.py
+++ backend/server/tests/user/utility.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv("../env/backend.env")
 os.environ["MONGODB_SERVICE_HOSTNAME"] = "localhost"
+os.environ["SESSIONSDB_SERVICE_HOSTNAME"] = "localhost"
 
 from server.db.mongo.setup import setup_user_related_collections
 from server.db.redis.setup import setup_redis_sessionsdb
