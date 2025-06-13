71a97166351f4e02c510f6a38eb01abfd72f4d26
keatsm <keatsm@zoho.com>
2025-06-13 18:15:23 +1000

    feat(frontend): add upload academic statement option to fill courses
    Users can now upload a pdf of their academic statement which will load
    courses into Circles. This option is in the menu where you can
    upload exported planners.
    
    Further testing is needed on different academic statements as there
    is no formal specification for what an academic statement consits
    of.

--- frontend/src/pages/TermPlanner/ImportPlannerMenu/ImportPlannerMenu.tsx
+++ frontend/src/pages/TermPlanner/ImportPlannerMenu/ImportPlannerMenu.tsx
@@ -16,16 +16,91 @@ import {
   updateStartYear
 } from 'reducers/plannerSlice';
 import CS from '../common/styles';
+import parseAcademicStatement from './inputParsers';
 import S from './styles';
 
 const ImportPlannerMenu = () => {
-  const planner = useSelector((state: RootState) => state.planner);
-  const inputRef = useRef<HTMLInputElement>(null);
+  const plannerState = useSelector((state: RootState) => state.planner);
+  const inputJSONRef = useRef<HTMLInputElement>(null);
+  const inputPDFRef = useRef<HTMLInputElement>(null);
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
 
-  const upload = () => {
-    inputRef.current?.click();
+  const uploadJSON = () => {
+    inputJSONRef.current?.click();
+  };
+
+  const uploadPDF = () => {
+    inputPDFRef.current?.click();
+  };
+
+  const updatePlanner = (planner: JSONPlanner) => {
+    const plannedCourses: string[] = [];
+    plannerState.years.forEach((year) => {
+      Object.values(year).forEach((termKey) => {
+        termKey.forEach((code) => {
+          plannedCourses.push(code);
+        });
+      });
+    });
+
+    dispatch(updateDegreeLength(planner.numYears));
+    dispatch(updateStartYear(planner.startYear));
+    if (plannerState.isSummerEnabled !== planner.isSummerEnabled) {
+      dispatch(toggleSummer());
+    }
+
+    planner.years.forEach((year, yearIndex) => {
+      Object.entries(year).forEach(([term, termCourses]) => {
+        termCourses.forEach(async (code, index) => {
+          const { data: course } = await axios.get<Course>(`/courses/getCourse/${code}`);
+          const courseData: PlannerCourse = {
+            title: course.title,
+            termsOffered: course.terms,
+            UOC: course.UOC,
+            plannedFor: null,
+            prereqs: course.raw_requirements,
+            isLegacy: course.is_legacy,
+            isUnlocked: true,
+            warnings: [],
+            handbookNote: course.handbook_note,
+            isAccurate: course.is_accurate,
+            isMultiterm: course.is_multiterm,
+            supressed: false,
+            // TODO: should actually hydrate people's mark
+            // and also their suppressions and ignoreFromProgression
+            // Maybe we don't need to worry about this as
+            // exporting / importing becomes redundant with accounts
+            ignoreFromProgression: false,
+            mark: undefined // TODO: WTF?
+          };
+
+          if (plannedCourses.indexOf(course.code) === -1) {
+            plannedCourses.push(course.code);
+            dispatch(addToUnplanned({ courseCode: course.code, courseData }));
+            const destYear = Number(yearIndex) + Number(plannerState.startYear);
+            const destTerm = term as Term;
+            const destRow = destYear - plannerState.startYear;
+            const destIndex = index;
+            dispatch(
+              moveCourse({
+                course: code,
+                destTerm: `${destYear}${destTerm}`,
+                srcTerm: 'unplanned'
+              })
+            );
+            dispatch(
+              setUnplannedCourseToTerm({
+                destRow,
+                destTerm,
+                destIndex,
+                course: code
+              })
+            );
+          }
+        });
+      });
+    });
   };
 
   const uploadedJSONFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -42,15 +117,6 @@ const ImportPlannerMenu = () => {
       e.target.value = '';
       return;
     }
-
-    const plannedCourses: string[] = [];
-    planner.years.forEach((year) => {
-      Object.values(year).forEach((termKey) => {
-        termKey.forEach((code) => {
-          plannedCourses.push(code);
-        });
-      });
-    });
 
     setLoading(true);
     const reader = new FileReader();
@@ -76,62 +142,7 @@ const ImportPlannerMenu = () => {
             });
             return;
           }
-          dispatch(updateDegreeLength(fileInJson.numYears));
-          dispatch(updateStartYear(fileInJson.startYear));
-          if (planner.isSummerEnabled !== fileInJson.isSummerEnabled) {
-            dispatch(toggleSummer());
-          }
-          fileInJson.years.forEach((year, yearIndex) => {
-            Object.entries(year).forEach(([term, termCourses]) => {
-              termCourses.forEach(async (code, index) => {
-                const { data: course } = await axios.get<Course>(`/courses/getCourse/${code}`);
-                const courseData: PlannerCourse = {
-                  title: course.title,
-                  termsOffered: course.terms,
-                  UOC: course.UOC,
-                  plannedFor: null,
-                  prereqs: course.raw_requirements,
-                  isLegacy: course.is_legacy,
-                  isUnlocked: true,
-                  warnings: [],
-                  handbookNote: course.handbook_note,
-                  isAccurate: course.is_accurate,
-                  isMultiterm: course.is_multiterm,
-                  supressed: false,
-                  // TODO: should actually hydrate people's mark
-                  // and also their suppressions and ignoreFromProgression
-                  // Maybe we don't need to worry about this as
-                  // exporting / importing becomes redundant with accounts
-                  ignoreFromProgression: false,
-                  mark: undefined // TODO: WTF?
-                };
-
-                if (plannedCourses.indexOf(course.code) === -1) {
-                  plannedCourses.push(course.code);
-                  dispatch(addToUnplanned({ courseCode: course.code, courseData }));
-                  const destYear = Number(yearIndex) + Number(planner.startYear);
-                  const destTerm = term as Term;
-                  const destRow = destYear - planner.startYear;
-                  const destIndex = index;
-                  dispatch(
-                    moveCourse({
-                      course: code,
-                      destTerm: `${destYear}${destTerm}`,
-                      srcTerm: 'unplanned'
-                    })
-                  );
-                  dispatch(
-                    setUnplannedCourseToTerm({
-                      destRow,
-                      destTerm,
-                      destIndex,
-                      course: code
-                    })
-                  );
-                }
-              });
-            });
-          });
+          updatePlanner(fileInJson);
           setLoading(false);
         } catch (err) {
           setLoading(false);
@@ -154,6 +165,42 @@ const ImportPlannerMenu = () => {
     };
   };
 
+  const uploadedPDFFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files === null) {
+      return;
+    }
+
+    if (e.target.files[0].type !== 'application/pdf') {
+      openNotification({
+        type: 'error',
+        message: 'Import file needs to be PDF.',
+        description: 'The uploaded file is not of type PDF.'
+      });
+      e.target.value = '';
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const plannerFromStatement: JSONPlanner = await parseAcademicStatement(e.target.files[0]);
+      updatePlanner(plannerFromStatement);
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      openNotification({
+        type: 'error',
+        message: 'Invalid academic statement format',
+        description: 'An error occured when processing your academic statement'
+      });
+      return;
+    }
+    openNotification({
+      type: 'success',
+      message: 'Academic Statement Imported',
+      description: 'Your academic statement has been successfully imported.'
+    });
+  };
+
   const spinIcon = <LoadingOutlined style={{ fontSize: 28 }} spin />;
 
   return (
@@ -162,14 +209,33 @@ const ImportPlannerMenu = () => {
       <CS.MenuDivider />
       <div>Import an existing planner if you have exported it previously as a JSON file.</div>
       <div>If you currently have courses planned, it may be merged with the imported planner.</div>
+      <div>
+        You can also upload an academic statement that can be used to populate your planner.
+      </div>
       <>
-        <div style={{ display: 'flex' }}>
-          <CS.Button style={{ width: '150px', margin: '5px' }} onClick={upload}>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <CS.Button style={{ width: '100%', margin: '5px' }} onClick={uploadJSON}>
             Upload a planner
+          </CS.Button>
+          <CS.Button style={{ width: '100%', margin: '5px' }} onClick={uploadPDF}>
+            Upload academic statement
           </CS.Button>
           {loading && <Spin indicator={spinIcon} />}
         </div>
-        <input type="file" style={{ display: 'none' }} ref={inputRef} onChange={uploadedJSONFile} />
+        <input
+          type="file"
+          accept=".json"
+          style={{ display: 'none' }}
+          ref={inputJSONRef}
+          onChange={uploadedJSONFile}
+        />
+        <input
+          type="file"
+          accept=".pdf"
+          style={{ display: 'none' }}
+          ref={inputPDFRef}
+          onChange={uploadedPDFFile}
+        />
       </>
     </S.Wrapper>
   );
