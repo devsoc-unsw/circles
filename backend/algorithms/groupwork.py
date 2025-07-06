7c1f47c3cbc5bfd8a5079155c784ada5279d8de9
Jenny <liu1jenny@gmail.com>
2025-07-06 20:11:31 +1000

    More parsing progress

--- backend/algorithms/groupwork.py
+++ backend/algorithms/groupwork.py
@@ -1,5 +1,7 @@
+# This is a basic checker, if this doesn't work, your groupwork parsing won't work either.
+
 # Don't be alarmed when selenium opens Chrome, this is needed for fetching data, as the data is
-# loaded dynamically based on the fragments, and cannot be parsed via a standard
+# loaded dynamically based on the fragments, and cannot be parsed via standard Beautiful Soup
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
@@ -14,7 +16,12 @@ driver.quit()
 # Puts the HTML in nicer beautiful soup format
 soup = BeautifulSoup(html, 'html.parser')
 
-# All group
+# Gets whether it's group work or not, all group work specifications is stored under a tag called description details,
+# and it conveniently enough can be parsed out. There's a bunch of unrelated description details tags, so
+# we get all the ones that contain the exact text "Group", since that's a standard way of formatting
+# group projects or tasks worth a percentage of the mark for the course. The below is approximately the
+# format from the webpage:
+# <dt>Assessment Format: <dt> <dd>Group<dd>
 potential_group_tags = soup.find_all('dd')
 groupwork = False
 for potential_group_tag in potential_group_tags:
@@ -24,4 +31,8 @@ for potential_group_tag in potential_group_tags:
 if groupwork:
     print("Success: Groupwork found in COMP6080!")
 else:
-    print("I swear groupwork was part of this course")
+    # If you are getting this error, either change the link at driver.get() to a course you
+    # are certain has groupwork (At present it is COMP6080), and if it works you're good to go.
+    # If it still doesn't work, you're going to have to find a different source for your links
+    # because the website doesn't work
+    print("I swear groupwork was part of this course, either it's been reworked, or the website's changed formats")
