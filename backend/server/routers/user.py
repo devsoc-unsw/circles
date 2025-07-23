87496a60011b3530d9a83d3766459b676548ba10
Lucas <me@lhvy.dev>
2025-07-23 21:55:15 +1000

    Fix removing multiterm courses from planner

--- backend/server/routers/user.py
+++ backend/server/routers/user.py
@@ -99,8 +99,11 @@ def get_user_p(uid: Annotated[str, Security(require_uid)]) -> Dict[str, CourseSt
     for index, year in enumerate(planner['years']):
         for termIndex, term in year.items():
             for course in term:
-                assert course not in flattened  # makes sure its not double storred
-                flattened[course] = f"{index + planner['startYear']} {termIndex}"
+                # TODO: Investigate if we need this. Needs to be switched off for multiterm courses
+                # assert course not in flattened  # makes sure its not double storred
+                if course not in flattened:
+                    # This if statement was added when the above assert was commented out
+                    flattened[course] = f"{index + planner['startYear']} {termIndex}"
 
     res: Dict[str, CourseStorageWithExtra] = {}
 
