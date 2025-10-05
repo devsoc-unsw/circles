5d58438c2e93e07c6fd295cdea4862c7db89fe90
Eugene Briones <eugenebriones7135@gmail.com>
2025-10-05 21:41:40 +1100

    initial functionalities

--- frontend/src/pages/TermPlanner/TermBox/TermBox.tsx
+++ frontend/src/pages/TermPlanner/TermBox/TermBox.tsx
@@ -12,8 +12,6 @@ import Spinner from 'components/Spinner';
 import useMediaQuery from 'hooks/useMediaQuery';
 import DraggableCourse from '../DraggableCourse';
 import S from './styles';
-// import useAverageDifficulty from 'hooks/useAverageDifficulty';
-
 import { useQueries } from '@tanstack/react-query';
 import { getCourseRating } from 'utils/api/unilectivesApi';
 import getDifficultyRating from 'utils/getDifficultyRating';
@@ -50,25 +48,24 @@ const TermBox = ({
   const coursesQuery = useUserCourses();
   const isSmall = useMediaQuery('(max-width: 1400px)');
 
-  // const difficulty = useAverageDifficulty(termCourseCodes);
-  // console.log(`${name}: ${difficulty}`);
-
   const courseRatings = useQueries({
     queries: termCourseCodes.map((courseCode) => ({
       queryKey: [`${courseCode}`],
       queryFn: () => getCourseRating(courseCode)
     }))
   });
+
   const manageabilities = courseRatings
     .map((courseRating) => courseRating.data?.manageability)
     .filter((rating) => typeof rating === 'number');
-  let avgManageability = 0;
+
+  let difficulty = 'Unknown';
   if (manageabilities.length !== 0) {
-    avgManageability =
-      manageabilities.reduce((sum, currRating) => sum + currRating, 0) / manageabilities.length;
+    const totalManageability = manageabilities.reduce((sum, currRating) => sum + currRating, 0);
+    difficulty = getDifficultyRating(totalManageability);
   }
-  const difficulty = getDifficultyRating(avgManageability);
-  console.log(`${name}: ${difficulty}`);
+
+  console.log(`${name}: ${manageabilities.length}`);
 
   if (!coursesQuery.data || !plannerQuery.data) {
     return <div>loading page...</div>;
@@ -149,6 +146,7 @@ const TermBox = ({
                 />
               </S.UOCBadgeWrapper>
               {/* place difficulty div here */}
+              {difficulty}
             </S.TermBoxWrapper>
           </Badge>
         )}
