85d766e23b9b49cea9d3fe2ff5ff5d107658f485
jason <111541945+jcongc@users.noreply.github.com>
2026-04-10 15:55:05 +1000

    fix: remove equivalent and exclusion courses from being added by default (#1275)
    * Feat/autoplan courses (#1272)
    
    * bump backend dependencies to latest
    
    * add guest auth for development environments
    
    * hook up autoplanning to the frontend
    
    * hook up autoplanning to the frontend
    
    * don't try to go to csesoc api during testing
    
    * don't try to go to csesoc api during testing
    
    * give typeguard for oidc config
    
    * add test flag
    
    * address nits
    
    * improve some waffling in autoplan.py
    
    * fix formatting
    
    * remove unnessesary complex logic around what the input should be - we always only plan unplanned courses
    
    * fix variable names around the use of channeled constraints
    
    ---------
    
    Co-authored-by: Peter Derias <peter.derias@gmail.com>
    
    * fix: adding core courses automatically (#1274)
    
    * bump backend dependencies to latest
    
    * add guest auth for development environments
    
    * hook up autoplanning to the frontend
    
    * hook up autoplanning to the frontend
    
    * don't try to go to csesoc api during testing
    
    * don't try to go to csesoc api during testing
    
    * give typeguard for oidc config
    
    * add test flag
    
    * address nits
    
    * improve some waffling in autoplan.py
    
    * fix formatting
    
    * remove unnessesary complex logic around what the input should be - we always only plan unplanned courses
    
    * fix variable names around the use of channeled constraints
    
    * fix: allow toggling of adding default courses and fix adding of mutex courses
    
    ---------
    
    Co-authored-by: Peter Derias <peter.derias@gmail.com>
    Co-authored-by: jason <jasoncchen@bytedance.com>
    
    ---------
    
    Co-authored-by: Peter Derias <peter.derias@gmail.com>
    Co-authored-by: jason <jasoncchen@bytedance.com>

--- frontend/src/types/degreeWizard.ts
+++ frontend/src/types/degreeWizard.ts
@@ -1,6 +1,9 @@
+export type CoreCoursesPreference = 'none' | 'lower' | 'higher';
+
 export type DegreeWizardPayload = {
   programCode: string;
   startYear?: number;
   endYear?: number;
   specs: string[];
+  addCoreCourses: CoreCoursesPreference;
 };
