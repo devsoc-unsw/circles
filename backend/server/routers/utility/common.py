3736af9f3b960666f7e5b668c2c5bc1e18c70b60
jason <jasoncongcchen@gmail.com>
2026-04-10 16:10:15 +1000

    fix: repeated function definitions

--- backend/server/routers/utility/common.py
+++ backend/server/routers/utility/common.py
@@ -94,36 +94,6 @@ def get_multiterm_instance_count(course_details: dict, is_summer_enabled: bool) 
     return (lcm(uoc, min_completed_course_uoc) // uoc) if uoc != 0 else 1
 
 
-def get_multiterm_instance_count(course_details: dict, is_summer_enabled: bool) -> int:
-    """Return required multiterm instances from a get_course_details-style object."""
-    if not course_details['is_multiterm']:
-        return 1
-
-    terms_offered = course_details['terms']
-    allowed_terms = ['T0', 'T1', 'T2', 'T3'] if is_summer_enabled else ['T1', 'T2', 'T3']
-    if not any(term in terms_offered for term in allowed_terms):
-        return 1
-
-    uoc = course_details['UOC']
-    min_completed_course_uoc = 6
-    return (lcm(uoc, min_completed_course_uoc) // uoc) if uoc != 0 else 1
-
-
-def get_multiterm_instance_count(course_details: dict, is_summer_enabled: bool) -> int:
-    """Return required multiterm instances from a get_course_details-style object."""
-    if not course_details['is_multiterm']:
-        return 1
-
-    terms_offered = course_details['terms']
-    allowed_terms = ['T0', 'T1', 'T2', 'T3'] if is_summer_enabled else ['T1', 'T2', 'T3']
-    if not any(term in terms_offered for term in allowed_terms):
-        return 1
-
-    uoc = course_details['UOC']
-    min_completed_course_uoc = 6
-    return (lcm(uoc, min_completed_course_uoc) // uoc) if uoc != 0 else 1
-
-
 def get_course_object(code: str, prog_time: ProgramTime, locked_offering: Optional[tuple[int, int]] = None, mark: Optional[int] = 100) -> Course:
     '''
     This return the Course object for the given course code.
